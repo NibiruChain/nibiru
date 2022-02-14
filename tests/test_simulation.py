9f0d0b92a4adfd1183ed839939e9d4d0cdfd1424
VimDiesel-Cosmos <vimdiesel@matrixsystems.co>
2022-02-14 11:42:11 -0700

    tests; bull market example

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -46,3 +46,21 @@ def test_get_leveraged_position():
         c_LA * (la_position.leverage_mult * la_position.price_pct_change + 1))
 
     assert la_position.value - position_value == pytest.approx(0, 0.0001)
+
+def test_bull_example():
+    amt_osmo = 10e3
+    spec_init = types.SpeculativeAssetState(amt=amt_osmo, price_usd=10)
+    matrix_exposure = sim.exposure_in_spec(spec=spec_init)
+    assert matrix_exposure == 1e5
+
+    la_exposure = 1e4
+    la_amt = la_exposure / spec_init.price_usd
+    assert la_amt == 1000
+    leverage_mult = matrix_exposure / la_exposure
+    assert leverage_mult == 10
+
+    # Price of OSMO increases 5 bucks
+    spec_bull = types.SpeculativeAssetState(amt=amt_osmo, price_usd=15)
+    protocol_pnl = sim.exposure_delta(spec_init=spec_init, spec_final=spec_bull)
+    assert protocol_pnl == 5e4
+
