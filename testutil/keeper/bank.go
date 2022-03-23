1ec3d21e921e4369c7507562877bc96891ea9c1b
Walter White <heisenberg@matrixsystems.co>
2022-03-23 11:58:22 +0100

    Add test bank keeper

---
+++ testutil/keeper/bank.go
@@ -0,0 +1,54 @@
+package keeper
+
+import (
+	"testing"
+
+	"github.com/MatrixDao/matrix/x/dex/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmdb "github.com/tendermint/tm-db"
+)
+
+func BankKeeper(t testing.TB) (keeper.Keeper, sdk.Context) {
+	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
+
+	db := tmdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
+	// stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	registry := codectypes.NewInterfaceRegistry()
+	cdc := codec.NewProtoCodec(registry)
+
+	paramsSubspace := paramstypes.NewSubspace(
+		cdc,
+		codec.NewLegacyAmino(),
+		storeKey,
+		memStoreKey,
+		"AccountParams",
+	)
+
+	accountKeeper, _ := AccountKeeper(t)
+
+	k := keeper.NewBaseKeeper(
+		cdc,
+		storeKey,
+		accountKeeper,
+		paramsSubspace,
+		map[string]bool{},
+	)
+
+	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+
+	return k, ctx
+}
