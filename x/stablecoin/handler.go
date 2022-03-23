df7e0f09f1ef75526f5d4fbf03db0761215ef2c0
VimDiesel-Cosmos <vimdiesel@matrixsystems.co>
2022-03-23 09:41:58 +0100

    test, feature: All tests passing in stablecoin

---
+++ x/stablecoin/handler.go
@@ -0,0 +1,31 @@
+package stablecoin
+
+import (
+	"fmt"
+
+	"github.com/MatrixDao/matrix/x/stablecoin/keeper"
+	"github.com/MatrixDao/matrix/x/stablecoin/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewHandler ...
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
+
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		case *types.MsgMint:
+			res, err := msgServer.Mint(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		// case *types.MsgDeposit:
+		// 	res, err := msgServer.Deposit(sdk.WrapSDKContext(ctx), msg)
+		// 	return sdk.WrapServiceResult(ctx, res, err)
+		default:
+			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		}
+	}
+}
