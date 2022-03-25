35ff55bff5b3526155393c854d333be6bda0694b
VimDiesel-Cosmos <vimdiesel@matrixsystems.co>
2022-03-25 13:01:01 +0100

    fix, tests: all x/* tests running

--- x/testutil/keeper/stablecoin.go
+++ x/testutil/keeper/stablecoin.go
@@ -43,7 +43,6 @@ func StablecoinKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		paramsSubspace,
 		nil,
 		nil,
-		nil,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
