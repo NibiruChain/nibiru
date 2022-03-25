4b20bb44502d80c3f9e7a315790f8aa32288be8c
Walter White <heisenberg@matrixsystems.co>
2022-03-25 19:16:29 +0100

    Change test keeper factory return types

--- x/dex/testutil/factory.go
+++ x/dex/testutil/factory.go
@@ -4,17 +4,18 @@ import (
 	"testing"
 
 	"github.com/MatrixDao/matrix/x/dex/keeper"
-	"github.com/MatrixDao/matrix/x/dex/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
 )
 
-func CreateKeepers(t *testing.T, storeKey sdk.StoreKey) (keeper.Keeper, types.AccountKeeper, types.BankKeeper, sdk.Context, codec.ProtoCodec) {
+func CreateKeepers(t *testing.T, storeKey sdk.StoreKey) (keeper.Keeper, authkeeper.AccountKeeper, bankkeeper.Keeper, sdk.Context, codec.ProtoCodec) {
 	db := tmdb.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db)
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
