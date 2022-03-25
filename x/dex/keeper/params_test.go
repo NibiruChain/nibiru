36eb03bc5ded557cbcd01fdd78694965e25c2199
Walter White <heisenberg@matrixsystems.co>
2022-03-25 16:43:00 +0100

    Update tests to use testutil keepers

--- x/dex/keeper/params_test.go
+++ x/dex/keeper/params_test.go
@@ -3,7 +3,7 @@ package keeper_test
 import (
 	"testing"
 
-	testkeeper "github.com/MatrixDao/matrix/testutil/keeper"
+	"github.com/MatrixDao/matrix/x/dex/testutil"
 	"github.com/MatrixDao/matrix/x/dex/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/stretchr/testify/require"
@@ -11,9 +11,9 @@ import (
 
 func TestGetParams(t *testing.T) {
 	storeKey := storetypes.NewKVStoreKey(types.ModuleName)
-	k, ctx, _ := testkeeper.NewDexKeeper(t, storeKey)
-	params := types.DefaultParams()
+	k, _, _, ctx, _ := testutil.CreateKeepers(t, storeKey)
 
+	params := types.DefaultParams()
 	k.SetParams(ctx, params)
 
 	require.EqualValues(t, params, k.GetParams(ctx))
