abdd794ccb18aabfa818791622afa5ad6d4d2dd7
Unique Divine <realuniquedivine@gmail.com>
2022-03-25 14:29:13 +0100

    refactor: Remove internal testutil module from x/stablecoin

--- x/stablecoin/keeper/params_test.go
+++ x/stablecoin/keeper/params_test.go
@@ -3,13 +3,13 @@ package keeper_test
 import (
 	"testing"
 
-	"github.com/MatrixDao/matrix/x/stablecoin/testutil"
 	"github.com/MatrixDao/matrix/x/stablecoin/types"
+	testkeeper "github.com/MatrixDao/matrix/x/testutil/keeper"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetParams(t *testing.T) {
-	k, ctx := testutil.StablecoinKeeper(t)
+	k, ctx := testkeeper.StablecoinKeeper(t)
 	params := types.DefaultParams()
 
 	k.SetParams(ctx, params)
