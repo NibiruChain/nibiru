04a5d56d0113cfffb019f61debc3ad220218efd2
Walter White <heisenberg@matrixsystems.co>
2022-03-25 13:34:26 +0100

    Return poolId in msg create pool response

--- x/dex/keeper/msg_server.go
+++ x/dex/keeper/msg_server.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/MatrixDao/matrix/x/dex/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -28,12 +27,12 @@ func (k msgServer) CreatePool(goCtx context.Context, msg *types.MsgCreatePool) (
 		return nil, err
 	}
 
-	k.Logger(ctx).Info(fmt.Sprintf("Sender address is %s", sender))
-
-	_, err = k.NewPool(ctx, sender, *msg.PoolParams, msg.PoolAssets)
+	poolId, err := k.NewPool(ctx, sender, *msg.PoolParams, msg.PoolAssets)
 	if err != nil {
 		return nil, err
 	}
 
-	return &types.MsgCreatePoolResponse{}, nil
+	return &types.MsgCreatePoolResponse{
+		PoolId: poolId,
+	}, nil
 }
