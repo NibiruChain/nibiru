1269af34db02992ce3dd8742f5ed8bb953f02fc3
Walter White <heisenberg@matrixsystems.co>
2022-03-26 13:23:24 +0100

    Fix x/testutil import

--- x/dex/types/message_create_pool_test.go
+++ x/dex/types/message_create_pool_test.go
@@ -3,7 +3,7 @@ package types
 import (
 	"testing"
 
-	"github.com/MatrixDao/matrix/testutil/sample"
+	"github.com/MatrixDao/matrix/x/testutil/sample"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 )
