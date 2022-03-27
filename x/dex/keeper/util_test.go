ba40bece56f7347811d917375dae94764fd52173
Walter White <heisenberg@matrixsystems.co>
2022-03-27 19:03:13 +0200

    Conversion from poolAssets to sdkCoins

---
+++ x/dex/keeper/util_test.go
@@ -0,0 +1,26 @@
+package keeper
+
+import (
+	"testing"
+
+	"github.com/MatrixDao/matrix/x/dex/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPoolAssetsCoins(t *testing.T) {
+	poolAssets := []types.PoolAsset{
+		{
+			Token: sdk.NewCoin("atom", sdk.NewInt(100)),
+		},
+		{
+			Token: sdk.NewCoin("mtrx", sdk.NewInt(200)),
+		},
+	}
+	coins := PoolAssetsCoins(poolAssets)
+
+	require.Equal(t, coins, sdk.NewCoins(
+		sdk.NewCoin("atom", sdk.NewInt(100)),
+		sdk.NewCoin("mtrx", sdk.NewInt(100))),
+	)
+}
