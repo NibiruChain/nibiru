6a80db5cf76e13a8d724186b1619774b75f36577
Walter White <heisenberg@matrixsystems.co>
2022-03-27 03:18:13 +0200

    Add comments to keeper functions

--- x/dex/keeper/keeper.go
+++ x/dex/keeper/keeper.go
@@ -23,6 +23,19 @@ type (
 	}
 )
 
+/*
+Creates a new keeper for the dex module.
+
+args
+  cdc: a codec
+  storeKey: the key-value store key that this keeper uses
+  ps: the param subspace for this keeper
+  accountKeeper: the auth module\'s keeper for accounts
+  bankKeeper: the bank module\'s keeper for bank transfers
+
+ret
+  Keeper: a keeper for the dex module
+*/
 func NewKeeper(
 	cdc codec.BinaryCodec,
 	storeKey sdk.StoreKey,
@@ -48,13 +61,30 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
 
-// SetNextPoolNumber sets next pool number
+/*
+Sets the next pool id that should be chosen when a new pool is created.
+This function changes the state.
+
+args
+  ctx: the cosmos-sdk context
+  poolNumber: the numeric id of the next pool number to use
+*/
 func (k Keeper) SetNextPoolNumber(ctx sdk.Context, poolNumber uint64) {
 	store := ctx.KVStore(k.storeKey)
 	bz := k.cdc.MustMarshal(&gogotypes.UInt64Value{Value: poolNumber})
 	store.Set(types.KeyNextGlobalPoolNumber, bz)
 }
 
+/*
+Retrieves the next pool id number to use when creating a new pool.
+This function is idempotent (does not change state).
+
+args
+  ctx: the cosmos-sdk context
+
+ret
+  uint64: a pool id number
+*/
 func (k Keeper) GetNextPoolNumber(ctx sdk.Context) uint64 {
 	var poolNumber uint64
 	store := ctx.KVStore(k.storeKey)
@@ -76,13 +106,34 @@ func (k Keeper) GetNextPoolNumber(ctx sdk.Context) uint64 {
 	return poolNumber
 }
 
-// GetNextPoolNumberAndIncrement returns the next pool number, and increments the corresponding state entry
+/*
+Returns the next pool id number, and increments the state's next pool id number by one
+so that the next pool creation uses an autoincremented id number.
+
+args
+  ctx: the cosmos-sdk context
+
+ret
+  uint64: a pool id number
+*/
 func (k Keeper) GetNextPoolNumberAndIncrement(ctx sdk.Context) uint64 {
 	poolNumber := k.GetNextPoolNumber(ctx)
 	k.SetNextPoolNumber(ctx, poolNumber+1)
 	return poolNumber
 }
 
+/*
+Fetches a pool by id number.
+Does not modify state.
+
+args
+  ctx: the cosmos-sdk context
+  poolId: the pool id number
+
+ret
+  Pool: a Pool proto object
+  error: an error if any occurred
+*/
 func (k Keeper) FetchPool(ctx sdk.Context, poolId uint64) (types.Pool, error) {
 	store := ctx.KVStore(k.storeKey)
 	poolKey := types.GetKeyPrefixPools(poolId)
@@ -97,6 +148,16 @@ func (k Keeper) FetchPool(ctx sdk.Context, poolId uint64) (types.Pool, error) {
 	return pool, nil
 }
 
+/*
+Writes a pool to the state.
+
+args
+  ctx: the cosmos-sdk context
+  Pool: a Pool proto object
+
+ret
+  error: an error if any occurred
+*/
 func (k Keeper) SetPool(ctx sdk.Context, pool types.Pool) error {
 	bz, err := pool.Marshal()
 	if err != nil {
@@ -110,6 +171,19 @@ func (k Keeper) SetPool(ctx sdk.Context, pool types.Pool) error {
 	return nil
 }
 
+/*
+Creates a brand new pool and writes it to the state.
+
+args
+  ctx: the cosmos-sdk context
+  sender: the pool creator's address
+  poolParams: parameters of the pool, represented by a PoolParams proto object
+  poolAssets: initial assets in the pool, represented by a PoolAssets proto object array
+
+ret
+  uint64: the pool id number
+  error: an error if any occurred
+*/
 func (k Keeper) NewPool(
 	ctx sdk.Context,
 	sender sdk.AccAddress,
@@ -118,6 +192,7 @@ func (k Keeper) NewPool(
 ) (uint64, error) {
 	poolId := k.GetNextPoolNumberAndIncrement(ctx)
 	poolName := fmt.Sprintf("matrix-pool-%d", poolId)
+	// Create a new account for the pool to hold funds.
 	poolAccount := k.accountKeeper.NewAccount(ctx, authtypes.NewEmptyModuleAccount(poolName))
 
 	k.accountKeeper.SetAccount(ctx, poolAccount)
@@ -139,8 +214,8 @@ func (k Keeper) NewPool(
 	for _, asset := range poolAssets {
 		coins = append(coins, asset.Token)
 	}
-
 	coins = coins.Sort()
+
 	err = k.bankKeeper.SendCoins(ctx, sender, poolAccount.GetAddress(), coins)
 	if err != nil {
 		return 0, err
