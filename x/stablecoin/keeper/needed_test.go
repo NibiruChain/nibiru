517b50deef42efb2eac5e11077b722694c015f7f
Unique-Divine <realuniquedivine@gmail.com>
2022-03-28 14:36:35 +0200

    test, docs (stablecoin): NeededGovAmtGivenColl, NeededCollAmtGivenGov

--- x/stablecoin/keeper/needed_test.go
+++ x/stablecoin/keeper/needed_test.go
@@ -4,12 +4,49 @@ import (
 	"testing"
 
 	"github.com/MatrixDao/matrix/x/stablecoin/keeper"
-	// "github.com/MatrixDao/matrix/x/testutil/sample"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 )
+
+func requireEqualWithMessage(
+	t require.TestingT, expected interface{}, actual interface{}, varName string) {
+
+	require.Equalf(t, expected, actual,
+		"Expected '%s': %d,\nActual '%s': %d",
+		varName, expected, varName, actual)
+}
+
+func TestAsInt(t *testing.T) {
+	tests := []struct {
+		name   string
+		inDec  sdk.Dec
+		outInt sdk.Int
+	}{
+		{
+			name:   "One to int",
+			inDec:  sdk.NewDec(1),
+			outInt: sdk.NewInt(1),
+		},
+		{
+			name:   "Small loss of precision due to truncation",
+			inDec:  sdk.MustNewDecFromStr("1.1"),
+			outInt: sdk.NewInt(1),
+		},
+		{
+			name:   "Large loss of precision due to truncation",
+			inDec:  sdk.MustNewDecFromStr("9.999"),
+			outInt: sdk.NewInt(9),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sdkInt := keeper.AsInt(test.inDec)
+			require.Equal(t, test.outInt, sdkInt)
+		})
+	}
+
+}
 
 func TestMint_NeededCollAmtGivenGov(t *testing.T) {
 	tests := []struct {
@@ -23,22 +60,22 @@ func TestMint_NeededCollAmtGivenGov(t *testing.T) {
 		err               error
 	}{
 		{
-			name:              "",
-			govAmt:            sdk.ZeroInt(),
-			priceGov:          sdk.NewDec(1),
-			priceColl:         sdk.NewDec(1),
-			collRatio:         sdk.NewDec(1),
-			neededCollAmt:     sdk.NewInt(1),
-			mintableStableAmt: sdk.NewInt(1),
+			name:              "Low collateral ratio",
+			govAmt:            sdk.NewInt(10),
+			priceGov:          sdk.NewDec(90),
+			priceColl:         sdk.NewDec(10),
+			collRatio:         sdk.MustNewDecFromStr("0.1"),
+			neededCollAmt:     sdk.NewInt(9),
+			mintableStableAmt: sdk.NewInt(999),
 			err:               nil,
 		}, {
-			name:              "",
-			govAmt:            sdk.ZeroInt(),
-			priceGov:          sdk.NewDec(1),
-			priceColl:         sdk.NewDec(1),
-			collRatio:         sdk.NewDec(1),
-			neededCollAmt:     sdk.NewInt(1),
-			mintableStableAmt: sdk.NewInt(1),
+			name:              "High collateral ratio",
+			govAmt:            sdk.NewInt(10),
+			priceGov:          sdk.MustNewDecFromStr("1"),
+			priceColl:         sdk.MustNewDecFromStr("2"),
+			collRatio:         sdk.MustNewDecFromStr("0.9"),
+			neededCollAmt:     sdk.NewInt(45),
+			mintableStableAmt: sdk.NewInt(100),
 			err:               nil,
 		},
 	}
@@ -46,8 +83,10 @@ func TestMint_NeededCollAmtGivenGov(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			neededCollAmt, mintableStableAmt := keeper.NeededCollAmtGivenGov(
 				test.govAmt, test.priceGov, test.priceColl, test.collRatio)
-			require.Equal(t, neededCollAmt, test.neededCollAmt)
-			require.Equal(t, mintableStableAmt, test.mintableStableAmt)
+			requireEqualWithMessage(
+				t, neededCollAmt, test.neededCollAmt, "neededCollAmt")
+			requireEqualWithMessage(
+				t, mintableStableAmt, test.mintableStableAmt, "mintableStableAmt")
 		})
 	}
 }
@@ -64,22 +103,22 @@ func TestMint_NeededGovAmtGivenColl(t *testing.T) {
 		err               error
 	}{
 		{
-			name:              "",
-			collAmt:           sdk.ZeroInt(),
-			priceGov:          sdk.NewDec(1),
-			priceColl:         sdk.NewDec(1),
-			collRatio:         sdk.NewDec(1),
-			neededGovAmt:      sdk.NewInt(1),
-			mintableStableAmt: sdk.NewInt(1),
-			err:               sdkerrors.ErrInvalidAddress,
+			name:              "collRatio above 50%",
+			collAmt:           sdk.NewInt(70),
+			priceGov:          sdk.NewDec(10),
+			priceColl:         sdk.NewDec(1), // 70 * 1 = 70
+			collRatio:         sdk.MustNewDecFromStr("0.7"),
+			neededGovAmt:      sdk.NewInt(3), // 10 * 3 = 30
+			mintableStableAmt: sdk.NewInt(100),
+			err:               nil,
 		}, {
-			name:              "",
-			collAmt:           sdk.ZeroInt(),
-			priceGov:          sdk.NewDec(1),
-			priceColl:         sdk.NewDec(1),
-			collRatio:         sdk.NewDec(1),
-			neededGovAmt:      sdk.NewInt(1),
-			mintableStableAmt: sdk.NewInt(1),
+			name:              "collRatio below 50%",
+			collAmt:           sdk.NewInt(40),
+			priceGov:          sdk.NewDec(10),
+			priceColl:         sdk.NewDec(2),
+			collRatio:         sdk.MustNewDecFromStr("0.8"),
+			neededGovAmt:      sdk.NewInt(2),
+			mintableStableAmt: sdk.NewInt(100),
 			err:               nil,
 		},
 	}
@@ -87,9 +126,9 @@ func TestMint_NeededGovAmtGivenColl(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			neededGovAmt, mintableStableAmt := keeper.NeededGovAmtGivenColl(
 				test.collAmt, test.priceGov, test.priceColl, test.collRatio)
+			require.Equal(t, sdk.NewInt(1), sdk.NewInt(1))
 			require.Equal(t, neededGovAmt, test.neededGovAmt)
 			require.Equal(t, mintableStableAmt, test.mintableStableAmt)
 		})
 	}
-
 }
