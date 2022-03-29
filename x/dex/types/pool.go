7f8e96d3fbd0a1af86c3989bf3f66b6eb92901e3
Walter White <heisenberg@matrixsystems.co>
2022-03-29 10:03:54 -0400

    Add ability to set initial assets in pool

--- x/dex/types/pool.go
+++ x/dex/types/pool.go
@@ -1,6 +1,12 @@
 package types
 
-import fmt "fmt"
+import (
+	fmt "fmt"
+	"sort"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 /*
 Returns the *base* denomination of a pool share token for a given poolId.
@@ -30,4 +36,58 @@ ret:
 */
 func GetPoolShareDisplayDenom(poolId uint64) (poolDenom string) {
 	return fmt.Sprintf("MATRIX-POOL-%d", poolId)
+}
+
+// setInitialPoolAssets sets the PoolAssets in the pool.
+// It is only designed to be called at the pool's creation.
+// If the same denom's PoolAsset exists, will return error.
+// The list of PoolAssets must be sorted. This is done to enable fast searching for a PoolAsset by denomination.
+// TODO: Unify story for validation of []PoolAsset, some is here, some is in CreatePool.ValidateBasic()
+func (p *Pool) SetInitialPoolAssets(poolAssets []PoolAsset) error {
+	exists := make(map[string]bool)
+
+	newTotalWeight := sdk.ZeroInt()
+	scaledPoolAssets := make([]PoolAsset, 0, len(poolAssets))
+
+	// TODO: Refactor this into PoolAsset.validate()
+	for _, asset := range poolAssets {
+		if asset.Token.Amount.LTE(sdk.ZeroInt()) {
+			return fmt.Errorf("can't add the zero or negative balance of token")
+		}
+
+		err := asset.ValidateWeight()
+		if err != nil {
+			return err
+		}
+
+		if exists[asset.Token.Denom] {
+			return fmt.Errorf("same PoolAsset already exists")
+		}
+		exists[asset.Token.Denom] = true
+
+		// Scale weight from the user provided weight to the correct internal weight
+		asset.Weight = asset.Weight.MulRaw(GuaranteedWeightPrecision)
+		scaledPoolAssets = append(scaledPoolAssets, asset)
+		newTotalWeight = newTotalWeight.Add(asset.Weight)
+	}
+
+	// TODO: Change this to a more efficient sorted insert algorithm.
+	// Furthermore, consider changing the underlying data type to allow in-place modification if the
+	// number of PoolAssets is expected to be large.
+	p.PoolAssets = scaledPoolAssets
+	SortPoolAssetsByDenom(p.PoolAssets)
+
+	p.TotalWeight = newTotalWeight
+
+	return nil
+}
+
+// SortPoolAssetsByDenom sorts pool assets in place, by weight.
+func SortPoolAssetsByDenom(assets []PoolAsset) {
+	sort.Slice(assets, func(i, j int) bool {
+		PoolAssetA := assets[i]
+		PoolAssetB := assets[j]
+
+		return strings.Compare(PoolAssetA.Token.Denom, PoolAssetB.Token.Denom) == -1
+	})
 }
