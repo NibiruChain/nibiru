d118b096adef30dad70ed9b6c9237a0001039e75
Walter White <heisenberg@matrixsystems.co>
2022-03-29 10:03:58 -0400

    Add constants

--- x/dex/types/constants.go
+++ x/dex/types/constants.go
@@ -20,4 +20,17 @@ var (
 
 	// InitPoolSharesSupply is the amount of new shares to initialize a pool with.
 	InitPoolSharesSupply = OneDisplayPoolShare.MulRaw(100)
+
+	// Pool creators can specify a weight in [1, MaxUserSpecifiedWeight)
+	// for every token in the balancer pool.
+	//
+	// The weight used in the balancer equation is then creator-specified-weight * GuaranteedWeightPrecision.
+	// This is done so that LBP's / smooth weight changes can actually happen smoothly,
+	// without complex precision loss / edge effects.
+	MaxUserSpecifiedWeight sdk.Int = sdk.NewIntFromUint64(1 << 20)
+	// Scaling factor for every weight. The pool weight is:
+	// weight_in_MsgCreateBalancerPool * GuaranteedWeightPrecision
+	//
+	// This is done so that smooth weight changes have enough precision to actually be smooth.
+	GuaranteedWeightPrecision int64 = 1 << 30
 )
