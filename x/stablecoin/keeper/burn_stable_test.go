098f4acc9ed7a839a0faf5867ad8434816d4dd93
Unique-Divine <realuniquedivine@gmail.com>
2022-03-30 13:33:31 -0400

    test, refactor (stablecoin/burn): Separate passing and failing cases. Refactor for more concise sdk.Int constructors

--- x/stablecoin/keeper/burn_stable_test.go
+++ x/stablecoin/keeper/burn_stable_test.go
@@ -127,14 +127,14 @@ func TestMsgBurnResponse_NotEnoughFunds(t *testing.T) {
 	testCases := []TestCase{
 		{
 			name:     "Not enough stable",
-			accFunds: sdk.NewCoins(sdk.NewCoin(common.StableDenom, sdk.NewInt(10))),
+			accFunds: sdk.NewCoins(sdk.NewInt64Coin(common.StableDenom, 10)),
 			msgBurn: types.MsgBurnStable{
 				Creator: sample.AccAddress().String(),
-				Stable:  sdk.NewCoin(common.StableDenom, sdk.NewInt(9001)),
+				Stable:  sdk.NewInt64Coin(common.StableDenom, 9001),
 			},
 			msgResponse: types.MsgBurnStableResponse{
-				Collateral: sdk.NewCoin(common.GovDenom, sdk.NewInt(0)),
-				Gov:        sdk.NewCoin(common.CollDenom, sdk.NewInt(0)),
+				Collateral: sdk.NewCoin(common.GovDenom, sdk.ZeroInt()),
+				Gov:        sdk.NewCoin(common.CollDenom, sdk.ZeroInt()),
 			},
 			govPrice:     sdk.MustNewDecFromStr("10"),
 			collPrice:    sdk.MustNewDecFromStr("1"),
@@ -142,34 +142,14 @@ func TestMsgBurnResponse_NotEnoughFunds(t *testing.T) {
 			err:          "uusdm: Not enough balance",
 		},
 		{
-			name:      "Happy path",
-			govPrice:  sdk.MustNewDecFromStr("10"),
-			collPrice: sdk.MustNewDecFromStr("1"),
-			accFunds: sdk.NewCoins(
-				sdk.NewCoin(common.StableDenom, sdk.NewInt(1000000000)),
-			),
-			moduleFunds: sdk.NewCoins(
-				sdk.NewCoin(common.CollDenom, sdk.NewInt(100000000)),
-			),
-			msgBurn: types.MsgBurnStable{
-				Creator: sample.AccAddress().String(),
-				Stable:  sdk.NewCoin(common.StableDenom, sdk.NewInt(10000000)),
-			},
-			msgResponse: types.MsgBurnStableResponse{
-				Gov:        sdk.NewCoin(common.GovDenom, sdk.NewInt(100000)),
-				Collateral: sdk.NewCoin(common.CollDenom, sdk.NewInt(9000000)),
-			},
-			expectedPass: true,
-		},
-		{
 			name:      "Stable is zero",
 			govPrice:  sdk.MustNewDecFromStr("10"),
 			collPrice: sdk.MustNewDecFromStr("1"),
 			accFunds: sdk.NewCoins(
-				sdk.NewCoin(common.StableDenom, sdk.NewInt(1000000000)),
+				sdk.NewInt64Coin(common.StableDenom, 1000000000),
 			),
 			moduleFunds: sdk.NewCoins(
-				sdk.NewCoin(common.CollDenom, sdk.NewInt(100000000)),
+				sdk.NewInt64Coin(common.CollDenom, 100000000),
 			),
 			msgBurn: types.MsgBurnStable{
 				Creator: sample.AccAddress().String(),
@@ -181,6 +161,109 @@ func TestMsgBurnResponse_NotEnoughFunds(t *testing.T) {
 			},
 			expectedPass: true,
 			err:          types.NoCoinFound.Wrap(common.StableDenom).Error(),
+		},
+	}
+	for _, test := range testCases {
+		executeTest(t, test)
+	}
+}
+
+func TestMsgBurnResponse_EnoughFunds(t *testing.T) {
+
+	type TestCase struct {
+		name         string
+		accFunds     sdk.Coins
+		moduleFunds  sdk.Coins
+		msgBurn      types.MsgBurnStable
+		msgResponse  types.MsgBurnStableResponse
+		govPrice     sdk.Dec
+		collPrice    sdk.Dec
+		expectedPass bool
+		err          string
+	}
+
+	executeTest := func(t *testing.T, testCase TestCase) {
+		tc := testCase
+		t.Run(tc.name, func(t *testing.T) {
+
+			matrixApp, ctx := testutil.NewMatrixApp()
+			acc, _ := sdk.AccAddressFromBech32(tc.msgBurn.Creator)
+			oracle := sample.AccAddress()
+
+			// Set up markets for the pricefeed keeper.
+			priceKeeper := &matrixApp.PriceKeeper
+			pfParams := ptypes.Params{
+				Markets: []ptypes.Market{
+					{MarketID: common.GovPricePool, BaseAsset: common.CollDenom, QuoteAsset: common.GovDenom,
+						Oracles: []sdk.AccAddress{oracle}, Active: true},
+					{MarketID: common.CollStablePool, BaseAsset: common.CollDenom, QuoteAsset: common.StableDenom,
+						Oracles: []sdk.AccAddress{oracle}, Active: true},
+				}}
+			priceKeeper.SetParams(ctx, pfParams)
+
+			// Post prices to each market with the oracle.
+			priceExpiry := ctx.BlockTime().Add(time.Hour)
+			_, err := priceKeeper.SetPrice(
+				ctx, oracle, common.GovPricePool, tc.govPrice, priceExpiry,
+			)
+			require.NoError(t, err)
+			_, err = priceKeeper.SetPrice(
+				ctx, oracle, common.CollStablePool, tc.collPrice, priceExpiry,
+			)
+			require.NoError(t, err)
+
+			// Update the 'CurrentPrice' posted by the oracles.
+			for _, market := range pfParams.Markets {
+				err = priceKeeper.SetCurrentPrices(ctx, market.MarketID)
+				require.NoError(t, err, "Error posting price for market: %d", market)
+			}
+
+			// Add collaterals to the module
+			err = matrixApp.BankKeeper.MintCoins(ctx, types.ModuleName, tc.moduleFunds)
+			if err != nil {
+				panic(err)
+			}
+
+			err = simapp.FundAccount(matrixApp.BankKeeper, ctx, acc, tc.accFunds)
+			require.NoError(t, err)
+
+			// Burn USDM -> Response contains GOV and COLL
+			goCtx := sdk.WrapSDKContext(ctx)
+			burnStableResponse, err := matrixApp.StablecoinKeeper.BurnStable(
+				goCtx, &tc.msgBurn)
+
+			if !tc.expectedPass {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.err)
+
+				return
+			}
+			require.NoError(t, err)
+			testutil.RequireEqualWithMessage(
+				t, burnStableResponse, &tc.msgResponse, "burnStableResponse")
+		})
+	}
+
+	testCases := []TestCase{
+		{
+			name:      "Happy path",
+			govPrice:  sdk.MustNewDecFromStr("10"),
+			collPrice: sdk.MustNewDecFromStr("1"),
+			accFunds: sdk.NewCoins(
+				sdk.NewInt64Coin(common.StableDenom, 1000000000),
+			),
+			moduleFunds: sdk.NewCoins(
+				sdk.NewInt64Coin(common.CollDenom, 100000000),
+			),
+			msgBurn: types.MsgBurnStable{
+				Creator: sample.AccAddress().String(),
+				Stable:  sdk.NewInt64Coin(common.StableDenom, 10000000),
+			},
+			msgResponse: types.MsgBurnStableResponse{
+				Gov:        sdk.NewInt64Coin(common.GovDenom, 100000),
+				Collateral: sdk.NewInt64Coin(common.CollDenom, 9000000),
+			},
+			expectedPass: true,
 		},
 	}
 	for _, test := range testCases {
