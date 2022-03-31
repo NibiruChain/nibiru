428d66ca003454b249021dbde0cfe680c311db88
Walter White <heisenberg@matrixsystems.co>
2022-03-31 10:31:46 -0400

    Add lock token functions on keeper

--- x/lockup/keeper/keeper.go
+++ x/lockup/keeper/keeper.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/tendermint/tendermint/libs/log"
 
@@ -35,4 +36,39 @@ func NewLockupKeeper(cdc codec.Codec, storeKey sdk.StoreKey, ak types.AccountKee
 // Logger returns a logger instance.
 func (k LockupKeeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+// LockTokens lock tokens from an account for specified duration.
+func (k LockupKeeper) LockTokens(ctx sdk.Context, owner sdk.AccAddress,
+	coins sdk.Coins, duration time.Duration) (types.Lock, error) {
+	lockId := k.GetLastLockId(ctx) + 1
+	// unlock time is set at the beginning of unlocking time
+	lock := types.NewLock(lockId, owner, duration, time.Time{}, coins)
+	err := k.Lock(ctx, lock)
+	if err != nil {
+		return lock, err
+	}
+	k.SetLastLockId(ctx, lockId)
+	return lock, nil
+}
+
+// Lock is a utility to lock coins into module account.
+func (k LockupKeeper) Lock(ctx sdk.Context, lock types.Lock) error {
+	owner, err := sdk.AccAddressFromBech32(lock.Owner)
+	if err != nil {
+		return err
+	}
+	if err := k.bk.SendCoinsFromAccountToModule(ctx, owner, types.ModuleName, lock.Coins); err != nil {
+		return err
+	}
+
+	// store lock object into the store
+	store := ctx.KVStore(k.storeKey)
+	bz, err := lock.Marshal()
+	if err != nil {
+		return err
+	}
+	store.Set(lockStoreKey(lock.LockId), bz)
+
+	return nil
 }
