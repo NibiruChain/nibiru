e38242a83ac7dc6fa65e21f707029c61d6df1c57
Walter White <heisenberg@matrixsystems.co>
2022-03-30 22:14:28 -0400

    Add message boilerplate

---
+++ x/lockup/types/msgs.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"fmt"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// constants.
+const (
+	TypeMsgLockTokens        = "lock_tokens"
+	TypeMsgBeginUnlockingAll = "begin_unlocking_all"
+	TypeMsgBeginUnlocking    = "begin_unlocking"
+)
+
+var _ sdk.Msg = &MsgLockTokens{}
+
+// NewMsgLockTokens creates a message to lock tokens.
+func NewMsgLockTokens(owner sdk.AccAddress, duration time.Duration, coins sdk.Coins) *MsgLockTokens {
+	return &MsgLockTokens{
+		Owner:    owner.String(),
+		Duration: duration,
+		Coins:    coins,
+	}
+}
+
+func (m MsgLockTokens) Route() string { return RouterKey }
+func (m MsgLockTokens) Type() string  { return TypeMsgLockTokens }
+func (m MsgLockTokens) ValidateBasic() error {
+	if m.Duration <= 0 {
+		return fmt.Errorf("duration should be positive: %d < 0", m.Duration)
+	}
+	return nil
+}
+
+func (m MsgLockTokens) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&m))
+}
+
+func (m MsgLockTokens) GetSigners() []sdk.AccAddress {
+	owner, _ := sdk.AccAddressFromBech32(m.Owner)
+	return []sdk.AccAddress{owner}
+}
