fb274b2b35989dadea61cf8e62a4e9ecb47f2c0a
Unique-Divine <realuniquedivine@gmail.com>
2022-04-03 19:34:33 -0400

    refactor: Remove unnecessary testkeeper for stablecoin

--- x/stablecoin/keeper/params_test.go
+++ x/stablecoin/keeper/params_test.go
@@ -1,25 +1,20 @@
 package keeper_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/MatrixDao/matrix/x/stablecoin/types"
-	// "github.com/MatrixDao/matrix/x/testutil"
-	// "github.com/MatrixDao/matrix/x/testutil/testkeeper"
-	"github.com/MatrixDao/matrix/x/testutil/testkeeper"
+	"github.com/MatrixDao/matrix/x/testutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetParams(t *testing.T) {
-	// matrixApp, ctx := testutil.NewMatrixApp()
-	// stableKeeper := &matrixApp.StablecoinKeeper
-	stableKeeper, ctx := testkeeper.StablecoinKeeper(t)
+	matrixApp, ctx := testutil.NewMatrixApp()
+	stableKeeper := &matrixApp.StablecoinKeeper
 
 	params := types.DefaultParams()
 
 	stableKeeper.SetParams(ctx, params)
-	fmt.Println(stableKeeper.ParamSubspace)
 
 	require.EqualValues(t, params, stableKeeper.GetParams(ctx))
 }
