807eb466899538ee726dab20b8377ba1600f5f0a
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2022-04-10 15:12:43 -0400

    feat, test (stablecoin): Recollateralize computations (#137)
    * docs: Start repo and x/stablecoin docs & specs
    
    * refactor: Condense grpc_query files
    
    * refactor (common): Make pair names more accurate
    
    * --ammend
    
    * feat (stablecoin): GovAmtFromRecollateralize, GetNeededCollAmount, GetNeededCollUSD
    
    * test: GetCollRatio #wip
    
    * test, fix (stablecoin/keeper): Params getter has tests.
    
    * fix (cli_test.go): wrong names imported from x/common
    
    * test: Make Validate throw an error if the CR is negative.
    
    * test: GetNeededColl_NoError
    
    * test: Increase coverage for when the collateral price can't be retrieved
    
    * test: GetNeededCollAmount
    
    * test: GovAmtFromRecollateralize failing cases
    
    * test (collateral_ratio): Complete GovAmtFromRecollateralize cases
    
    * refactor: Make stabelcoin keeper fields public to remove the need for 'IncreaseModuleCollBalance'
    
    * Update x/stablecoin/keeper/collateral_ratio_test.go
    
    commit suggestion
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    * Update x/stablecoin/keeper/collateral_ratio_test.go
    
    commit suggestion
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    * Update x/stablecoin/keeper/collateral_ratio_test.go
    
    commit suggestion
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    * refactor: add issue tags to TODO
    
    * refactor: PR nits
    
    * refactor (collateral_ratio_test.go): Combine test methods using a field to specify which prices to post
    
    * refactor: Combine mint and burn to one script
    
    * test: Use require.Panics instead of defer and recover
    
    * refactor: Use MulInt instead of ToDec().Mul
    
    * Update x/stablecoin/keeper/collateral_ratio.go
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    * ref
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>

--- x/stablecoin/keeper/mint_burn_stable.go
+++ x/stablecoin/keeper/mint_burn_stable.go
@@ -12,6 +12,7 @@ import (
 	"github.com/MatrixDao/matrix/x/stablecoin/events"
 	"github.com/MatrixDao/matrix/x/stablecoin/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // MintStable mints stable coins given collateral (COLL) and governance (GOV)
@@ -27,13 +28,13 @@ func (k Keeper) MintStable(
 	}
 
 	// priceGov: Price of the governance token in USD
-	priceGov, err := k.priceKeeper.GetCurrentPrice(ctx, common.GovPricePool)
+	priceGov, err := k.PriceKeeper.GetCurrentPrice(ctx, common.GovCollPool)
 	if err != nil {
 		return nil, err
 	}
 
 	// priceColl: Price of the collateral token in USD
-	priceColl, err := k.priceKeeper.GetCurrentPrice(ctx, common.CollPricePool)
+	priceColl, err := k.PriceKeeper.GetCurrentPrice(ctx, common.CollStablePool)
 	if err != nil {
 		return nil, err
 	}
@@ -43,11 +44,11 @@ func (k Keeper) MintStable(
 	collRatio, _ := sdk.NewDecFromStr("0.9")
 	govRatio := sdk.OneDec().Sub(collRatio)
 
-	neededCollUSD := msg.Stable.Amount.ToDec().Mul(collRatio)
+	neededCollUSD := collRatio.MulInt(msg.Stable.Amount)
 	neededCollAmt := neededCollUSD.Quo(priceColl.Price).TruncateInt()
 	neededColl := sdk.NewCoin(common.CollDenom, neededCollAmt)
 
-	neededGovUSD := msg.Stable.Amount.ToDec().Mul(govRatio)
+	neededGovUSD := govRatio.MulInt(msg.Stable.Amount)
 	neededGovAmt := neededGovUSD.Quo(priceGov.Price).TruncateInt()
 	neededGov := sdk.NewCoin(common.GovDenom, neededGovAmt)
 
@@ -85,7 +86,7 @@ func (k Keeper) MintStable(
 func (k Keeper) sendInputCoinsToModule(
 	ctx sdk.Context, msgCreator sdk.AccAddress, coins sdk.Coins,
 ) (err error) {
-	err = k.bankKeeper.SendCoinsFromAccountToModule(
+	err = k.BankKeeper.SendCoinsFromAccountToModule(
 		ctx, msgCreator, types.ModuleName, coins)
 	if err != nil {
 		return err
@@ -102,7 +103,7 @@ func (k Keeper) sendInputCoinsToModule(
 func (k Keeper) sendMintedTokensToUser(
 	ctx sdk.Context, to sdk.AccAddress, stable sdk.Coin,
 ) error {
-	err := k.bankKeeper.SendCoinsFromModuleToAccount(
+	err := k.BankKeeper.SendCoinsFromModuleToAccount(
 		ctx, types.ModuleName, to, sdk.NewCoins(stable))
 	if err != nil {
 		return err
@@ -115,7 +116,7 @@ func (k Keeper) sendMintedTokensToUser(
 
 // burnGovTokens burns governance coins
 func (k Keeper) burnGovTokens(ctx sdk.Context, neededGov sdk.Coin) error {
-	err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.NewCoins(neededGov))
+	err := k.BankKeeper.BurnCoins(ctx, types.ModuleName, sdk.NewCoins(neededGov))
 	if err != nil {
 		return err
 	}
@@ -126,7 +127,7 @@ func (k Keeper) burnGovTokens(ctx sdk.Context, neededGov sdk.Coin) error {
 }
 
 func (k Keeper) mintStable(ctx sdk.Context, stable sdk.Coin) error {
-	err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(stable))
+	err := k.BankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(stable))
 	if err != nil {
 		return err
 	}
@@ -134,4 +135,81 @@ func (k Keeper) mintStable(ctx sdk.Context, stable sdk.Coin) error {
 	events.EmitMintStable(ctx, stable)
 
 	return nil
+}
+
+func (k Keeper) BurnStable(
+	goCtx context.Context, msg *types.MsgBurnStable,
+) (*types.MsgBurnStableResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	msgCreator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg.Stable.Amount == sdk.ZeroInt() {
+		return nil, sdkerrors.Wrap(types.NoCoinFound, msg.Stable.Denom)
+	}
+
+	// priceGov: Price of the governance token in USD
+	priceGov, err := k.PriceKeeper.GetCurrentPrice(ctx, common.GovCollPool)
+	if err != nil {
+		return nil, err
+	}
+
+	// priceColl: Price of the collateral token in USD
+	priceColl, err := k.PriceKeeper.GetCurrentPrice(ctx, common.CollStablePool)
+	if err != nil {
+		return nil, err
+	}
+
+	// The user receives a mixure of collateral (COLL) and governance (GOV) tokens
+	// based on the collateral ratio.
+	// TODO: Initialize 'collRatio' based on the collateral ratio of the protocol.
+	collRatio, _ := sdk.NewDecFromStr("0.9")
+	govRatio := sdk.NewDec(1).Sub(collRatio)
+
+	redeemColl := collRatio.MulInt(msg.Stable.Amount).Quo(
+		priceColl.Price).TruncateInt()
+	redeemGov := govRatio.MulInt(msg.Stable.Amount).Quo(
+		priceGov.Price).TruncateInt()
+
+	// Send USDM from account to module
+	stablesToBurn := sdk.NewCoins(msg.Stable)
+	err = k.BankKeeper.SendCoinsFromAccountToModule(
+		ctx, msgCreator, types.ModuleName, stablesToBurn)
+	if err != nil {
+		return nil, err
+	}
+	events.EmitTransfer(ctx, msg.Stable, msgCreator.String(), types.ModuleName)
+
+	// Mint GOV that will later be sent to the user.
+	collToSend := sdk.NewCoin(common.CollDenom, redeemColl)
+	govToSend := sdk.NewCoin(common.GovDenom, redeemGov)
+	coinsNeededToSend := sdk.NewCoins(collToSend, govToSend)
+
+	err = k.BankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(govToSend))
+	if err != nil {
+		panic(err)
+	}
+	events.EmitMintMtrx(ctx, govToSend)
+
+	// Send tokens (GOV and COLL) to the account
+	err = k.BankKeeper.SendCoinsFromModuleToAccount(
+		ctx, types.ModuleName, msgCreator, coinsNeededToSend)
+	if err != nil {
+		return nil, err
+	}
+	for _, coin := range coinsNeededToSend {
+		events.EmitTransfer(ctx, coin, types.ModuleName, msgCreator.String())
+	}
+
+	// Burn the USDM
+	err = k.BankKeeper.BurnCoins(ctx, types.ModuleName, stablesToBurn)
+	if err != nil {
+		panic(err)
+	}
+	events.EmitBurnStable(ctx, msg.Stable)
+
+	return &types.MsgBurnStableResponse{Collateral: collToSend, Gov: govToSend}, nil
 }
