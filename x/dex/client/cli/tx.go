0ce60e60e05caf32ca294ffd725e327e0c8258ab
Walter White <heisenberg@matrixsystems.co>
2022-04-11 19:57:23 -0700

    Remove unused variables

--- x/dex/client/cli/tx.go
+++ x/dex/client/cli/tx.go
@@ -2,20 +2,10 @@ package cli
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/MatrixDao/matrix/x/dex/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/cobra"
-)
-
-var (
-	DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
-)
-
-const (
-	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-	listSeparator              = ","
 )
 
 // GetTxCmd returns the transaction commands for this module
