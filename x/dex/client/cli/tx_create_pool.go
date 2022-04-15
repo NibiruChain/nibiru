ca35b5a57915ab712f8a2fa74af69d435ad9b92c
Walter White <heisenberg@matrixsystems.co>
2022-04-15 09:08:20 -0700

    Add validation check to TxCreatePool

--- x/dex/client/cli/tx_create_pool.go
+++ x/dex/client/cli/tx_create_pool.go
@@ -103,6 +103,10 @@ Where pool.json contains:
 				},
 			)
 
+			if err = msg.ValidateBasic(); err != nil {
+				return err
+			}
+
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
