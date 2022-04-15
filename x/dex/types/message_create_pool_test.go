436b85248a59605413ee093c8ca8605f7bf2c69f
Walter White <heisenberg@matrixsystems.co>
2022-04-15 09:07:27 -0700

    Add further validation for MsgCreatePool

--- x/dex/types/message_create_pool_test.go
+++ x/dex/types/message_create_pool_test.go
@@ -4,37 +4,216 @@ import (
 	"testing"
 
 	"github.com/MatrixDao/matrix/x/testutil/sample"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMsgCreatePool_ValidateBasic(t *testing.T) {
+func TestValidateBasic(t *testing.T) {
 	tests := []struct {
-		name string
-		msg  MsgCreatePool
-		err  error
+		name        string
+		msg         MsgCreatePool
+		expectedErr error
 	}{
 		{
 			name: "invalid address",
 			msg: MsgCreatePool{
 				Creator: "invalid_address",
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
 			},
-			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+			expectedErr: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid swap fee, too small",
 			msg: MsgCreatePool{
 				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("-0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
 			},
+			expectedErr: ErrInvalidSwapFee,
+		},
+		{
+			name: "invalid swap fee, too large",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("1.1"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: ErrInvalidSwapFee,
+		},
+		{
+			name: "invalid exit fee, too small",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("-0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: ErrInvalidExitFee,
+		},
+		{
+			name: "invalid exit fee, too large",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("1.1"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: ErrInvalidExitFee,
+		},
+		{
+			name: "too few assets",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: ErrTooFewPoolAssets,
+		},
+		{
+			name: "too many assets",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("ccc", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: ErrTooManyPoolAssets,
+		},
+		{
+			name: "invalid token weight",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(0),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(0),
+					},
+				},
+			},
+			expectedErr: ErrInvalidTokenWeight,
+		},
+		{
+			name: "valid create pool message",
+			msg: MsgCreatePool{
+				Creator: sample.AccAddress().String(),
+				PoolParams: &PoolParams{
+					SwapFee: sdk.MustNewDecFromStr("0.003"),
+					ExitFee: sdk.MustNewDecFromStr("0.003"),
+				},
+				PoolAssets: []PoolAsset{
+					{
+						Token:  sdk.NewInt64Coin("aaa", 1),
+						Weight: sdk.NewInt(1),
+					},
+					{
+						Token:  sdk.NewInt64Coin("bbb", 1),
+						Weight: sdk.NewInt(1),
+					},
+				},
+			},
+			expectedErr: nil,
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.msg.ValidateBasic()
+			if tc.expectedErr != nil {
+				require.ErrorIs(t, err, tc.expectedErr)
+			} else {
+				require.NoError(t, err)
 			}
-			require.NoError(t, err)
 		})
 	}
 }
