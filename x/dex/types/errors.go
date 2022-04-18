c1fb393785d705b844f812887f757b90b72c04fe
Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
2022-04-18 12:55:08 -0700

    test(dex): add create pool CLI tests (#183)
    * Change max number of tokens in a pool to 2
    
    * Add msg create pool tests
    
    * Add further validation for MsgCreatePool
    
    * Add validation check to TxCreatePool
    
    * Add create pool cli tests
    
    * Remove validation since it's done in broadcast
    
    * Add error types for invalid tx args
    
    * Add test cases for invalid tx args
    
    * Add gitignore for mockdata directory
    
    The mockdata directory is a local directory I use for storing pool files and other data that's useful for cli commands
    
    * Add umtrx to validator genesis for creating pools
    
    Co-authored-by: Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
    Co-authored-by: Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>

--- x/dex/types/errors.go
+++ x/dex/types/errors.go
@@ -13,4 +13,8 @@ var (
 	ErrInvalidSwapFee     = sdkerrors.Register(ModuleName, 3, "invalid pool swap fee, must be between [0, 1]")
 	ErrInvalidExitFee     = sdkerrors.Register(ModuleName, 4, "invalid pool exit fee, must be between [0, 1]")
 	ErrInvalidTokenWeight = sdkerrors.Register(ModuleName, 5, "token weight must be greater than zero")
+
+	// create-pool tx cli errors
+	ErrMissingPoolFileFlag   = sdkerrors.Register(ModuleName, 6, "must pass in a pool json using the --pool-file flag")
+	ErrInvalidCreatePoolArgs = sdkerrors.Register(ModuleName, 7, "deposit tokens and token weights should have same length and denom order")
 )
