648badae1bf81eba8bec7fb7292fea3cf54cf8e9
Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
2022-04-18 22:05:15 -0700

    chore(lint): Add linting workflow (#177)
    * Lint and refactor go workflow
    
    * Add lint workflow
    
    * Delete starport config
    
    * Delete unused pacakge
    
    * Fix all lint errors
    
    * Add golangci-lint config and run autofix
    
    * Add linter docs
    
    * Fix lint errors

--- x/pricefeed/client/cli/query_prices.go
+++ x/pricefeed/client/cli/query_prices.go
@@ -17,7 +17,6 @@ func CmdPrices() *cobra.Command {
 		Short: "Query prices",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
