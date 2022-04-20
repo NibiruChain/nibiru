93a0d191432fcb0bdc0d2827de5649f8ab664b04
AgentSmithMatrix <98403347+AgentSmithMatrix@users.noreply.github.com>
2022-04-20 20:04:03 +0200

    Changes on Liquidity Ratio (#180)
    * generalize GetSupply functions
    
    * refactor tests
    
    * temp commit
    
    * fix errors on merge
    
    * add CalcSpotPrice
    
    * add pool weighted calc
    
    * add option to get market cap of governance token
    
    * add comment to clarify calculation for keeper
    
    * add liquidity ratio func
    
    * add test for get liquidity ratio
    
    * add get liqudity ratio bands
    
    * update changes based on PR

--- x/stablecoin/keeper/supply.go
+++ x/stablecoin/keeper/supply.go
@@ -1,10 +1,14 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/NibiruChain/nibiru/x/common"
 )
+
+var LiquidityRatioBands = sdk.MustNewDecFromStr("0.001")
 
 func (k Keeper) GetSupplyNUSD(
 	ctx sdk.Context,
@@ -16,4 +20,53 @@ func (k Keeper) GetSupplyNIBI(
 	ctx sdk.Context,
 ) sdk.Coin {
 	return k.BankKeeper.GetSupply(ctx, common.GovDenom)
+}
+
+func (k Keeper) GetStableMarketCap(ctx sdk.Context) sdk.Int {
+	return k.GetSupplyNUSD(ctx).Amount
+}
+
+func (k Keeper) GetGovMarketCap(ctx sdk.Context) (sdk.Int, error) {
+	pairID, err := k.DexKeeper.GetFromPair(ctx, common.GovDenom, common.StableDenom)
+	if err != nil {
+		return sdk.Int{}, err
+	}
+
+	pool := k.DexKeeper.FetchPool(ctx, pairID)
+
+	price, err := pool.CalcSpotPrice(common.GovDenom, common.StableDenom)
+	if err != nil {
+		return sdk.Int{}, err
+	}
+
+	nibiSupply := k.GetSupplyNIBI(ctx)
+
+	return nibiSupply.Amount.ToDec().Mul(price).RoundInt(), nil
+}
+
+// GetLiquidityRatio returns the liquidity ratio defined as govMarketCap / stableMarketCap
+func (k Keeper) GetLiquidityRatio(ctx sdk.Context) (sdk.Dec, error) {
+	govMarketCap, err := k.GetGovMarketCap(ctx)
+	if err != nil {
+		return sdk.Dec{}, err
+	}
+
+	stableMarketCap := k.GetStableMarketCap(ctx)
+	if stableMarketCap.Equal(sdk.ZeroInt()) {
+		return sdk.Dec{}, fmt.Errorf("stable maket cap is equal to zero")
+	}
+
+	return govMarketCap.ToDec().Quo(stableMarketCap.ToDec()), nil
+}
+
+func (k Keeper) GetLiquidityRatioBands(ctx sdk.Context) (lowBand, upBand sdk.Dec, err error) {
+	liquidityRatio, err := k.GetLiquidityRatio(ctx)
+	if err != nil {
+		return sdk.Dec{}, sdk.Dec{}, err
+	}
+
+	lowBand = liquidityRatio.Mul(sdk.OneDec().Sub(LiquidityRatioBands))
+	upBand = liquidityRatio.Mul(sdk.OneDec().Add(LiquidityRatioBands))
+
+	return lowBand, upBand, nil
 }
