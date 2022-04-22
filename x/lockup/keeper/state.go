2e28690440d7bcb664d5ca368f184b1bad379332
godismercilex <98415576+godismercilex@users.noreply.github.com>
2022-04-22 14:55:42 +0200

    fix: bad iteration over locked/unlocked coins of an address (#236)
    * fix: bad iteration over locked/unlocked coins of an address
    
    * fix: bad commit
    
    * Update x/lockup/keeper/state_test.go
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    * Update x/lockup/keeper/state_test.go
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>
    
    Co-authored-by: Walter White <101130700+MatrixHeisenberg@users.noreply.github.com>

--- x/lockup/keeper/state.go
+++ x/lockup/keeper/state.go
@@ -99,14 +99,14 @@ func (s LockState) Get(id uint64) (*types.Lock, error) {
 // UnlockedIDsByAddress returns the list of types.Lock IDs which can be
 // unlocked given the lock owner sdk.AccAddress.
 func (s LockState) UnlockedIDsByAddress(addr sdk.AccAddress) []uint64 {
-	key := s.keyAddrTime(addr.String(), s.ctx.BlockTime(), nil)
-	iter := s.addrTimeIndex.Iterator(nil, key)
+	iter := prefix.NewStore(s.addrTimeIndex, s.keyAddr(addr.String(), nil)). // this creates a store which prefixes over addr's lock namespace
+											Iterator(nil, s.keyTime(s.ctx.BlockTime(), nil)) // this iterates over locks with end time <= current time
 	defer iter.Close()
 
 	var ids []uint64
 
 	for ; iter.Valid(); iter.Next() {
-		primaryKey := iter.Key()[len(key):]
+		primaryKey := iter.Key()[8:]
 		ids = append(ids, sdk.BigEndianToUint64(primaryKey))
 	}
 
@@ -114,16 +114,15 @@ func (s LockState) UnlockedIDsByAddress(addr sdk.AccAddress) []uint64 {
 }
 
 func (s LockState) IterateLockedCoins(addr sdk.AccAddress) sdk.Coins {
-	key := s.keyAddrTime(addr.String(), s.ctx.BlockTime(), nil)
-
-	iter := s.addrTimeIndex.Iterator(key, nil)
+	iter := prefix.NewStore(s.addrTimeIndex, s.keyAddr(addr.String(), nil)). // this creates a store which prefixes over addr's lock namespace
+											Iterator(s.keyTime(s.ctx.BlockTime(), nil), nil) // this iterates over locks with end time >= current time
 	defer iter.Close()
 
 	coins := sdk.NewCoins()
 	for ; iter.Valid(); iter.Next() {
 		lock := new(types.Lock)
 
-		primaryKey := iter.Key()[len(key):] // strip index key and just keep primary key
+		primaryKey := iter.Key()[8:] // we're stripping the size of the time in form of bytes of the key to get only the primary key
 		if !s.locks.Has(primaryKey) {
 			panic(fmt.Errorf("state corruption: %v", primaryKey))
 		}
@@ -135,16 +134,15 @@ func (s LockState) IterateLockedCoins(addr sdk.AccAddress) sdk.Coins {
 }
 
 func (s LockState) IterateUnlockedCoins(addr sdk.AccAddress) sdk.Coins {
-	key := s.keyAddrTime(addr.String(), s.ctx.BlockTime(), nil)
-
-	iter := s.addrTimeIndex.Iterator(nil, key)
+	iter := prefix.NewStore(s.addrTimeIndex, s.keyAddr(addr.String(), nil)). // this creates a store which prefixes over addr's lock namespace
+											Iterator(nil, s.keyTime(s.ctx.BlockTime(), nil)) // this iterates over locks with end time <= current time
 	defer iter.Close()
 
 	coins := sdk.NewCoins()
 	for ; iter.Valid(); iter.Next() {
 		lock := new(types.Lock)
 
-		primaryKey := iter.Key()[len(key):] // strip index key and just keep primary key
+		primaryKey := iter.Key()[8:] // strip index key and just keep primary key
 		if !s.locks.Has(primaryKey) {
 			panic(fmt.Errorf("state corruption: %v", primaryKey))
 		}
