650a27917ab3fbae015a90b6c409b8c4d77f3d05
Walter White <heisenberg@matrixsystems.co>
2022-04-26 07:36:03 -0700

    Add MsgSwapAssets struct type

---
+++ x/dex/types/message_swap_assets.go
@@ -0,0 +1,48 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgSwapAssets = "swap_assets"
+
+var _ sdk.Msg = &MsgSwapAssets{}
+
+func NewMsgSwapAssets(sender string, poolId uint64, tokensIn sdk.Coin, tokenOutDenom string) *MsgSwapAssets {
+	return &MsgSwapAssets{
+		Sender:        sender,
+		PoolId:        poolId,
+		TokensIn:      tokensIn,
+		TokenOutDenom: tokenOutDenom,
+	}
+}
+
+func (msg *MsgSwapAssets) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSwapAssets) Type() string {
+	return TypeMsgSwapAssets
+}
+
+func (msg *MsgSwapAssets) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSwapAssets) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSwapAssets) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
---
+++ x/dex/types/message_swap_assets_test.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/NibiruChain/nibiru/x/testutil/sample"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgSwapAssets_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgSwapAssets
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgSwapAssets{
+				Sender: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		}, {
+			name: "valid address",
+			msg: MsgSwapAssets{
+				Sender: sample.AccAddress().String(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
