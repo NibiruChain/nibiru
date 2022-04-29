6ae6dea492a1a4d101b04099a1e86870db8a5795
godismercilex <mercilex@matrixsystems.co>
2022-04-29 12:46:30 +0200

    change: align lockup specification

--- x/lockup/types/errors.go
+++ x/lockup/types/errors.go
@@ -8,7 +8,8 @@ import (
 
 // x/lockup module sentinel errors.
 var (
-	ErrNotLockOwner   = sdkerrors.Register(ModuleName, 1, "msg sender is not the owner of specified lock")
-	ErrLockupNotFound = sdkerrors.Register(ModuleName, 2, "lockup not found")
-	ErrLockEndTime    = sdkerrors.Register(ModuleName, 3, "lock end time not met")
+	ErrNotLockOwner     = sdkerrors.Register(ModuleName, 1, "msg sender is not the owner of specified lock")
+	ErrLockupNotFound   = sdkerrors.Register(ModuleName, 2, "lockup not found")
+	ErrLockEndTime      = sdkerrors.Register(ModuleName, 3, "lock end time not met")
+	ErrAlreadyUnlocking = sdkerrors.Register(ModuleName, 4, "lockup is already unlocking")
 )
