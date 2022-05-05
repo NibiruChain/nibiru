a09affe4788ea67c67e5d57e97b3e5c426122e4f
Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
2022-05-05 16:11:34 -0300

    Create simulations for Dex actions (#296)
    * feat: Add simulation for pool balancer
    
    * feat: Add simulation for create pool
    
    * fix: Add missing expected account keeper
    
    * feat: Add simapp files (wip)
    
    * fix: Sanitize coins on pool asset creation
    
    * fix: Adjust logic for 2 assets
    
    * fix: Update the name of the fee token
    
    * fix: Use dex genesis state for simulation
    
    * fix: remove changes made to simulation state
    
    * lint: Lint and cleaning
    
    * feat: Add fetch all pools function in keeper
    
    * feat: Simulate join pool
    
    * feat: Simulate exit pool
    
    Co-authored-by: Matthias Darblade <matthias.darblade@gmail.com>

---
+++ simapp/sim_test.go
@@ -0,0 +1,81 @@
+package simapp
+
+import (
+	"os"
+	"testing"
+
+	testutil "github.com/NibiruChain/nibiru/x/testutil"
+	sdkSimapp "github.com/cosmos/cosmos-sdk/simapp"
+	simulationtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+)
+
+// Profile with:
+// /usr/local/go/bin/go test -benchmem -run=^$ github.com/NibiruChain/nibiru/simapp -bench ^BenchmarkFullAppSimulation$ -Commit=true -cpuprofile cpu.out
+func BenchmarkFullAppSimulation(b *testing.B) {
+	// -Enabled=true -NumBlocks=1000 -BlockSize=200 \
+	// -Period=1 -Commit=true -Seed=57 -v -timeout 24h
+	sdkSimapp.FlagEnabledValue = true
+	sdkSimapp.FlagNumBlocksValue = 1000
+	sdkSimapp.FlagBlockSizeValue = 200
+	sdkSimapp.FlagCommitValue = true
+	sdkSimapp.FlagVerboseValue = true
+	// sdkSimapp.FlagPeriodValue = 1000
+	fullAppSimulation(b, false)
+}
+
+func TestFullAppSimulation(t *testing.T) {
+	// -Enabled=true -NumBlocks=1000 -BlockSize=200 \
+	// -Period=1 -Commit=true -Seed=57 -v -timeout 24h
+	sdkSimapp.FlagEnabledValue = true
+	sdkSimapp.FlagNumBlocksValue = 50
+	sdkSimapp.FlagBlockSizeValue = 50
+	sdkSimapp.FlagCommitValue = true
+	sdkSimapp.FlagVerboseValue = true
+	sdkSimapp.FlagPeriodValue = 10
+	sdkSimapp.FlagSeedValue = 10
+	fullAppSimulation(t, true)
+}
+
+func fullAppSimulation(tb testing.TB, is_testing bool) {
+	config, db, dir, _, _, err := sdkSimapp.SetupSimulation("goleveldb-app-sim", "Simulation")
+	if err != nil {
+		tb.Fatalf("simulation setup failed: %s", err.Error())
+	}
+
+	defer func() {
+		db.Close()
+		err = os.RemoveAll(dir)
+		if err != nil {
+			tb.Fatal(err)
+		}
+	}()
+
+	nibiru := testutil.NewTestApp( /*shouldUseDefaultGenesis*/ true)
+
+	// Run randomized simulation:
+	_, simParams, simErr := simulation.SimulateFromSeed(
+		/* tb */ tb,
+		/* w */ os.Stdout,
+		/* app */ nibiru.BaseApp,
+		/* appStateFn */ AppStateFn(nibiru.AppCodec(), nibiru.SimulationManager()),
+		/* randAccFn */ simulationtypes.RandomAccounts, // Replace with own random account function if using keys other than secp256k1
+		/* ops */ sdkSimapp.SimulationOperations(nibiru, nibiru.AppCodec(), config), // Run all registered operations
+		/* blockedAddrs */ nibiru.ModuleAccountAddrs(),
+		/* config */ config,
+		/* cdc */ nibiru.AppCodec(),
+	)
+
+	// export state and simParams before the simulation error is checked
+	if err = sdkSimapp.CheckExportSimulation(nibiru, config, simParams); err != nil {
+		tb.Fatal(err)
+	}
+
+	if simErr != nil {
+		tb.Fatal(simErr)
+	}
+
+	if config.Commit {
+		sdkSimapp.PrintStats(db)
+	}
+}
