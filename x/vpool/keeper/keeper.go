6ca95eeae684989880a9aa5cff8a1f48d8d52e8b
Walter White <heisenberg@matrixsystems.co>
2022-05-10 09:41:48 -0500

    Move pool state methods to pool_state.go

--- x/vpool/keeper/keeper.go
+++ x/vpool/keeper/keeper.go
@@ -143,56 +143,7 @@ func (k Keeper) SwapInput(
 	return baseAssetAmount, nil
 }
 
-// getPool returns the pool from database
-func (k Keeper) getPool(ctx sdk.Context, pair common.TokenPair) (
-	*types.Pool, error,
-) {
-	bz := ctx.KVStore(k.storeKey).Get(types.GetPoolKey(pair))
-	if bz == nil {
-		return nil, fmt.Errorf("Could not find vpool for pair %s", pair.String())
-	}
 
-	var pool types.Pool
-	k.codec.MustUnmarshal(bz, &pool)
-	return &pool, nil
-}
-
-// CreatePool creates a pool for a specific pair.
-func (k Keeper) CreatePool(
-	ctx sdk.Context,
-	pair string,
-	tradeLimitRatio sdk.Dec, // integer with 6 decimals, 1_000_000 means 1.0
-	quoteAssetReserve sdk.Int,
-	baseAssetReserve sdk.Int,
-	fluctuationLimitRatio sdk.Dec,
-) {
-	pool := types.NewPool(
-		pair,
-		tradeLimitRatio,
-		quoteAssetReserve,
-		baseAssetReserve,
-		fluctuationLimitRatio,
-	)
-
-	k.savePool(ctx, pool)
-	k.saveSnapshot(ctx, pool, 0)
-	k.saveSnapshotCounter(ctx, common.TokenPair(pair), 0)
-}
-
-func (k Keeper) savePool(
-	ctx sdk.Context,
-	pool *types.Pool,
-) {
-	bz := k.codec.MustMarshal(pool)
-	ctx.KVStore(k.storeKey).Set(types.GetPoolKey(common.TokenPair(pool.Pair)), bz)
-}
-
-
-
-// existsPool returns true if pool exists, false if not.
-func (k Keeper) existsPool(ctx sdk.Context, pair common.TokenPair) bool {
-	return ctx.KVStore(k.storeKey).Has(types.GetPoolKey(pair))
-}
 
 func (k Keeper) checkFluctuationLimitRatio(ctx sdk.Context, pool *types.Pool) error {
 	if pool.FluctuationLimitRatio.GT(sdk.ZeroDec()) {
