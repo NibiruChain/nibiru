93ed5dc42fe961e89fa367c57b4d74241ba33136
MD <matthias@matrixsystems.co>
2022-05-11 17:53:02 -0300

    fix: Minor issue in error comments

--- x/perp/types/errors.go
+++ x/perp/types/errors.go
@@ -1,12 +1,10 @@
 package types
 
-// DONTCOVER
-
 import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// x/stablecoin module sentinel errors
+// x/perp module sentinel errors
 var (
 	MarginHighEnough = sdkerrors.Register(ModuleName, 1, "Margin is higher than required maintenant margin ratio")
 )
