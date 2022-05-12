45da288e38cfb88576fec5e22792c08004eb2bb4
godismercilex <mercilex@matrixsystems.co>
2022-05-12 18:25:26 +0200

    add: tests

--- x/perp/keeper/settle_position.go
+++ x/perp/keeper/settle_position.go
@@ -9,39 +9,39 @@ import (
 
 // SettlePosition settles a trader position
 // TODO(mercilex): test
-func (k Keeper) SettlePosition(ctx sdk.Context, pair common.TokenPair, trader string) error {
+func (k Keeper) SettlePosition(ctx sdk.Context, pair common.TokenPair, trader string) (transferredCoins sdk.Coins, err error) {
 	// TODO(mercilex): requireAmm?
 
 	pos, err := k.Positions().Get(ctx, pair, trader)
 	if err != nil {
-		return err
+		return
 	}
 
 	// check if pos size != 0
 	if pos.Size_.IsZero() {
-		return types.ErrPositionSizeZero
+		return transferredCoins, types.ErrPositionSizeZero
 	}
 	// clear pos
 	err = k.ClearPosition(ctx, pair, trader)
 	if err != nil {
-		return err
+		return
 	}
 
 	// run calculations on settled values
 	settlementPrice, err := k.VpoolKeeper.GetSettlementPrice(ctx, pair)
 	if err != nil {
-		return err
+		return
 	}
 	settledValue := sdk.ZeroDec()
 
 	switch settlementPrice.IsZero() {
 	case true:
-		settledValue = pos.Margin.ToDec()
+		settledValue = pos.Margin
 	default:
 		// openPrice = positionOpenNotional / abs(positionSize)
-		openPrice := pos.OpenNotional.Quo(pos.Size_.Abs()).ToDec()
+		openPrice := pos.OpenNotional.Quo(pos.Size_.Abs())
 		// returnedFund := positionSize * (settlementPrice - openPrice) + positionMargin
-		returnedFund := pos.Size_.ToDec().Mul(settlementPrice.Sub(openPrice)).Add(pos.Margin.ToDec())
+		returnedFund := pos.Size_.Mul(settlementPrice.Sub(openPrice)).Add(pos.Margin)
 		if returnedFund.GT(sdk.ZeroDec()) {
 			settledValue = returnedFund.Abs()
 		}
@@ -52,15 +52,15 @@ func (k Keeper) SettlePosition(ctx sdk.Context, pair common.TokenPair, trader st
 		panic(err) // NOTE(mercilex): must never happen
 	}
 
-	transferredCoins := sdk.NewCoins() // TODO(mercilex): maybe here it would be cleaner to create a zero coin amount of the quote asset of the virtual pool
+	transferredCoins = sdk.NewCoins() // TODO(mercilex): maybe here it would be cleaner to create a zero coin amount of the quote asset of the virtual pool
 	if settledValue.GT(sdk.ZeroDec()) {
 		// transfer, NOTE(mercilex): transferredCoins is over-written here.
 		transferredCoins, err = k.Transfer(ctx, pair, traderAddr, settledValue.RoundInt())
 		if err != nil {
-			return err
+			return
 		}
 	}
 
 	events.EmitPositionSettle(ctx, pair.String(), traderAddr, transferredCoins)
-	return nil
+	return
 }
--- x/perp/keeper/settle_position_test.go
+++ x/perp/keeper/settle_position_test.go
@@ -1,1 +1,164 @@
-package keeper
+package keeper_test
+
+import (
+	"github.com/NibiruChain/nibiru/x/common"
+	"github.com/NibiruChain/nibiru/x/perp/keeper"
+	"github.com/NibiruChain/nibiru/x/perp/types"
+	"github.com/NibiruChain/nibiru/x/testutil/mock"
+	"github.com/NibiruChain/nibiru/x/testutil/sample"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmdb "github.com/tendermint/tm-db"
+	"testing"
+)
+
+func TestKeeper_SettlePosition(t *testing.T) {
+	t.Run("success - settlement price zero", func(t *testing.T) {
+		k, dep, ctx := getKeeper(t)
+		addr := sample.AccAddress()
+		pair, err := common.NewTokenPairFromStr("LUNA:UST") // memeing
+		require.NoError(t, err)
+
+		dep.mockVpoolKeeper.
+			EXPECT().
+			GetSettlementPrice(gomock.Eq(ctx), gomock.Eq(pair)).
+			Return(sdk.ZeroDec(), error(nil))
+
+		pos := &types.Position{
+			Address:      addr.String(),
+			Pair:         pair.String(),
+			Size_:        sdk.NewDec(100),
+			Margin:       sdk.NewDec(100),
+			OpenNotional: sdk.NewDec(1000),
+		}
+		err = k.Positions().Create(ctx, pos)
+		require.NoError(t, err)
+
+		coins, err := k.SettlePosition(ctx, pair, addr.String())
+		require.NoError(t, err)
+
+		require.Equal(t, sdk.NewCoins(sdk.NewCoin("todo", pos.Size_.RoundInt())), coins) // TODO(mercilex): here we should have different denom, depends on Transfer impl
+	})
+
+	t.Run("success - settlement price not zero", func(t *testing.T) {
+		k, dep, ctx := getKeeper(t)
+		addr := sample.AccAddress()
+		pair, err := common.NewTokenPairFromStr("LUNA:UST") // memeing
+		require.NoError(t, err)
+
+		dep.mockVpoolKeeper.
+			EXPECT().
+			GetSettlementPrice(gomock.Eq(ctx), gomock.Eq(pair)).
+			Return(sdk.NewDec(1000), error(nil))
+
+		// this means that the user
+		// has bought 100 contracts
+		// for an open notional of 1_000 coins
+		// which means entry price is 10
+		// now price is 1_000
+		// which means current pos value is 100_000
+		// now we need to return the user the profits
+		// which are 99000 coins
+		// we also need to return margin which is 100coin
+		// so total is 99_100 coin
+		pos := &types.Position{
+			Address:      addr.String(),
+			Pair:         pair.String(),
+			Size_:        sdk.NewDec(100),
+			Margin:       sdk.NewDec(100),
+			OpenNotional: sdk.NewDec(1000),
+		}
+		err = k.Positions().Create(ctx, pos)
+		require.NoError(t, err)
+
+		coins, err := k.SettlePosition(ctx, pair, addr.String())
+		require.NoError(t, err)
+		require.Equal(t, coins, sdk.NewCoins(sdk.NewInt64Coin("todo", 99100))) // todo(mercilex): modify denom once transfer is impl
+	})
+
+	t.Run("position size is zero", func(t *testing.T) {
+		k, _, ctx := getKeeper(t)
+		addr := sample.AccAddress()
+		pair, err := common.NewTokenPairFromStr("LUNA:UST") // memeing
+		require.NoError(t, err)
+		err = k.Positions().Create(ctx, &types.Position{
+			Address: addr.String(),
+			Pair:    pair.String(),
+			Size_:   sdk.ZeroDec(),
+		})
+		require.NoError(t, err)
+
+		coins, err := k.SettlePosition(ctx, pair, addr.String())
+		require.ErrorIs(t, err, types.ErrPositionSizeZero)
+		require.Len(t, coins, 0)
+	})
+}
+
+// TODO(mercilex): copied from clearing_house_test.go, to cleanup
+
+type mockedDependencies struct {
+	mockAccountKeeper *mock.MockAccountKeeper
+	mockBankKeeper    *mock.MockBankKeeper
+	mockPriceKeeper   *mock.MockPriceKeeper
+	mockVpoolKeeper   *mock.MockVpoolKeeper
+}
+
+func getKeeper(t *testing.T) (keeper.Keeper, mockedDependencies, sdk.Context) {
+	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	memStoreKey := storetypes.NewMemoryStoreKey(types.StoreKey)
+
+	db := tmdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	protoCodec := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+	params := initParamsKeeper(protoCodec, codec.NewLegacyAmino(), storeKey, memStoreKey)
+
+	subSpace, found := params.GetSubspace(types.ModuleName)
+	require.True(t, found)
+
+	ctrl := gomock.NewController(t)
+	mockedAccountKeeper := mock.NewMockAccountKeeper(ctrl)
+	mockedBankKeeper := mock.NewMockBankKeeper(ctrl)
+	mockedPriceKeeper := mock.NewMockPriceKeeper(ctrl)
+	mockedVpoolKeeper := mock.NewMockVpoolKeeper(ctrl)
+
+	mockedAccountKeeper.
+		EXPECT().GetModuleAddress(types.ModuleName).
+		Return(authtypes.NewModuleAddress(types.ModuleName))
+
+	k := keeper.NewKeeper(
+		protoCodec,
+		storeKey,
+		subSpace,
+		mockedAccountKeeper,
+		mockedBankKeeper,
+		mockedPriceKeeper,
+		mockedVpoolKeeper,
+	)
+
+	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, nil)
+
+	return k, mockedDependencies{
+		mockAccountKeeper: mockedAccountKeeper,
+		mockBankKeeper:    mockedBankKeeper,
+		mockPriceKeeper:   mockedPriceKeeper,
+		mockVpoolKeeper:   mockedVpoolKeeper,
+	}, ctx
+}
+
+func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino, key, tkey sdk.StoreKey) paramskeeper.Keeper {
+	paramsKeeper := paramskeeper.NewKeeper(appCodec, legacyAmino, key, tkey)
+	paramsKeeper.Subspace(types.ModuleName)
+
+	return paramsKeeper
+}
