d8bbfdbb636248c6d22f2b03a8967cfe428d7026
Agent Smith <agentsmith@matrixsystems.co>
2022-05-13 10:01:53 +0200

    fix linter

--- x/perp/types/expected_keepers.go
+++ x/perp/types/expected_keepers.go
@@ -3,14 +3,16 @@ package types
 //go:generate  mockgen -destination=../../testutil/mock/perp_interfaces.go -package=mock github.com/NibiruChain/nibiru/x/perp/types AccountKeeper,BankKeeper,PriceKeeper,VpoolKeeper
 
 import (
-	"github.com/NibiruChain/nibiru/x/common"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/NibiruChain/nibiru/x/common"
 
 	"time"
 
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
 	pftypes "github.com/NibiruChain/nibiru/x/pricefeed/types"
 	vpooltypes "github.com/NibiruChain/nibiru/x/vpool/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // ----------------------------------------------------------
