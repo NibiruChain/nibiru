36506cb7f725007dc3e67458e965e0829344fb67
MD <matthias@matrixsystems.co>
2022-05-16 16:31:28 -0300

    fix: Add send module to module keeper

--- x/perp/types/expected_keepers.go
+++ x/perp/types/expected_keepers.go
@@ -36,6 +36,8 @@ type BankKeeper interface {
 		ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string,
 		amt sdk.Coins,
 	) error
+	SendCoinsFromModuleToModule(
+		ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(
 		ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress,
 		amt sdk.Coins,
