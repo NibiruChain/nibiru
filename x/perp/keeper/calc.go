26aa29093bb795bb56000e9e6483ec7c4013b966
Unique-Divine <realuniquedivine@gmail.com>
2022-05-16 23:42:42 -0500

    refactor: move CalcFee to calc.go

--- x/perp/keeper/calc.go
+++ x/perp/keeper/calc.go
@@ -118,3 +118,26 @@ func (k Keeper) calcFreeCollateral(ctx sdk.Context, pos types.Position, fundingP
 	freeCollateral := minCollateral.Sub(marginRequirement.ToDec()).TruncateInt()
 	return freeCollateral, nil
 }
+
+/* CalcFee calculates the total tx fee for exchanging `quoteAmt` of tokens on
+the exchange.
+
+Args:
+	quoteAmt (sdk.Int):
+
+Returns:
+	toll (sdk.Int): Amount of tokens transferred to the the fee pool.
+	spread (sdk.Int): Amount of tokens transferred to the PerpEF.
+*/
+func (k Keeper) CalcFee(ctx sdk.Context, quoteAmt sdk.Int) (toll sdk.Int, spread sdk.Int, err error) {
+	if quoteAmt.Equal(sdk.ZeroInt()) {
+		return sdk.ZeroInt(), sdk.ZeroInt(), nil
+	}
+
+	params := k.GetParams(ctx)
+
+	tollRatio := params.GetTollRatioAsDec()
+	spreadRatio := params.GetSpreadRatioAsDec()
+
+	return sdk.NewDecFromInt(quoteAmt).Mul(tollRatio).TruncateInt(), sdk.NewDecFromInt(quoteAmt).Mul(spreadRatio).TruncateInt(), nil
+}
