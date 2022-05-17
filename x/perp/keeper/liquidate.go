8841ed8968c85d430e4d61ff7ee09c26544ada88
Unique-Divine <realuniquedivine@gmail.com>
2022-05-17 02:04:23 -0500

    feat (liquidate.go): ExecuteFullLiquidation, distributeLiquidateRewards

--- x/perp/keeper/liquidate.go
+++ x/perp/keeper/liquidate.go
@@ -6,35 +6,39 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/NibiruChain/nibiru/x/common"
+	"github.com/NibiruChain/nibiru/x/perp/events"
 	"github.com/NibiruChain/nibiru/x/perp/types"
 )
 
-type LiquidationOutput struct {
-	FeeToPerpEcosystemFund sdk.Dec
+type LiquidateResp struct {
 	BadDebt                sdk.Dec
 	FeeToLiquidator        sdk.Dec
+	FeeToPerpEcosystemFund sdk.Dec
+	Liquidator             sdk.AccAddress
 	PositionResp           *types.PositionResp
 }
 
-func (l *LiquidationOutput) String() string {
+func (l *LiquidateResp) String() string {
 	return fmt.Sprintf(`
-	liquidationOutput {
-		FeeToPerpEcosystemFund: %v,
+	LiquidateResp {
 		BadDebt: %v,
 		FeeToLiquidator: %v,
+		FeeToPerpEcosystemFund: %v,
 		PositionResp: %v,
+		Liquidator: %v,
 	}
 	`,
-		l.FeeToPerpEcosystemFund,
-		l.BadDebt,
-		l.FeeToLiquidator,
+		l.BadDebt.String(),
+		l.FeeToLiquidator.String(),
+		l.FeeToPerpEcosystemFund.String(),
 		l.PositionResp,
+		l.Liquidator.String(),
 	)
 }
 
-func (l *LiquidationOutput) Validate() error {
+func (l *LiquidateResp) Validate() error {
 	for _, field := range []sdk.Dec{
-		l.FeeToPerpEcosystemFund, l.BadDebt, l.FeeToLiquidator} {
+		l.BadDebt, l.FeeToLiquidator, l.FeeToPerpEcosystemFund} {
 		if field.IsNil() {
 			return fmt.Errorf(
 				`invalid liquidationOutput: %v,
@@ -44,15 +48,18 @@ func (l *LiquidationOutput) Validate() error {
 	return nil
 }
 
-// CreateLiquidation create a liquidation of a position and compute the fee to ecosystem fund
-func (k Keeper) CreateLiquidation(
-	ctx sdk.Context, pair common.TokenPair, owner sdk.AccAddress, position *types.Position,
-) (LiquidationOutput, error) {
+// ExecuteFullLiquidation fully liquidates a position.
+func (k Keeper) ExecuteFullLiquidation(
+	ctx sdk.Context, liquidator sdk.AccAddress, position *types.Position,
+) (err error) {
 	params := k.GetParams(ctx)
 
-	positionResp, err := k.closePositionEntirely(ctx, *position, sdk.ZeroDec())
+	positionResp, err := k.closePositionEntirely(
+		ctx,
+		/* currentPosition */ *position,
+		/* quoteAssetAmountLimit */ sdk.ZeroDec())
 	if err != nil {
-		return LiquidationOutput{}, err
+		return err
 	}
 
 	remainMargin := positionResp.MarginToVault.Abs()
@@ -71,23 +78,101 @@ func (k Keeper) CreateLiquidation(
 		remainMargin = remainMargin.Sub(feeToLiquidator)
 	}
 
-	var feeToPerpEcosystemFund sdk.Dec
-	if remainMargin.GT(sdk.ZeroDec()) {
+	feeToPerpEcosystemFund := sdk.ZeroDec()
+	if remainMargin.IsPositive() {
 		feeToPerpEcosystemFund = remainMargin
-	} else {
-		feeToPerpEcosystemFund = sdk.ZeroDec()
 	}
 
-	output := LiquidationOutput{
-		FeeToPerpEcosystemFund: feeToPerpEcosystemFund,
+	err = k.distributeLiquidateRewards(ctx, LiquidateResp{
 		BadDebt:                totalBadDebt,
 		FeeToLiquidator:        feeToLiquidator,
+		FeeToPerpEcosystemFund: feeToPerpEcosystemFund,
+		Liquidator:             liquidator,
 		PositionResp:           positionResp,
+	})
+	if err != nil {
+		return err
 	}
 
-	err = output.Validate()
+	return nil
+}
+
+func (k Keeper) distributeLiquidateRewards(
+	ctx sdk.Context, liquidateResp LiquidateResp) (err error) {
+	// --------------------------------------------------------------
+	//  Preliminary validations
+	// --------------------------------------------------------------
+
+	// validate response
+	err = liquidateResp.Validate()
 	if err != nil {
-		return LiquidationOutput{}, err
+		return err
 	}
-	return output, err
+
+	// validate liquidator
+	liquidator, err := sdk.AccAddressFromBech32(liquidateResp.Liquidator.String())
+	if err != nil {
+		return err
+	}
+
+	// validate pair
+	pair, err := common.NewTokenPairFromStr(liquidateResp.PositionResp.Position.Pair)
+	if err != nil {
+		return err
+	}
+	err = k.requireVpool(ctx, pair)
+	if err != nil {
+		return err
+	}
+
+	// --------------------------------------------------------------
+	// Distribution of rewards
+	// --------------------------------------------------------------
+
+	vaultAddr := k.AccountKeeper.GetModuleAddress(types.VaultModuleAccount)
+	perpEFAddr := k.AccountKeeper.GetModuleAddress(types.PerpEFModuleAccount)
+
+	// Transfer fee from vault to PerpEF
+	feeToPerpEF := liquidateResp.FeeToPerpEcosystemFund.TruncateInt()
+	if feeToPerpEF.IsPositive() {
+		coinToPerpEF := sdk.NewCoin(
+			pair.GetQuoteTokenDenom(), feeToPerpEF)
+		err = k.BankKeeper.SendCoinsFromModuleToModule(
+			ctx,
+			/* from */ types.VaultModuleAccount,
+			/* to */ types.PerpEFModuleAccount,
+			sdk.NewCoins(coinToPerpEF),
+		)
+		if err != nil {
+			return err
+		}
+		events.EmitTransfer(ctx,
+			/* coin */ coinToPerpEF,
+			/* from */ vaultAddr.String(),
+			/* to */ perpEFAddr.String(),
+		)
+	}
+
+	// Transfer fee from PerpEF to liquidator
+	feeToLiquidator := liquidateResp.FeeToLiquidator.TruncateInt()
+	if feeToLiquidator.IsPositive() {
+		coinToLiquidator := sdk.NewCoin(
+			pair.GetQuoteTokenDenom(), liquidateResp.FeeToLiquidator.TruncateInt())
+		err = k.BankKeeper.SendCoinsFromModuleToAccount(
+			ctx,
+			/* from */ types.PerpEFModuleAccount,
+			/* to */ liquidator,
+			sdk.NewCoins(coinToLiquidator),
+		)
+		if err != nil {
+			return err
+		}
+		events.EmitTransfer(ctx,
+			/* coin */ coinToLiquidator,
+			/* from */ perpEFAddr.String(),
+			/* to */ liquidator.String(),
+		)
+	}
+
+	return nil
 }
