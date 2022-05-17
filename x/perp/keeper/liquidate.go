6439a0472bf4e43b2bfbd2e58f58325636793573
Unique-Divine <realuniquedivine@gmail.com>
2022-05-17 00:11:34 -0500

    feat (perp): CreatePartialLiquidation

--- x/perp/keeper/liquidate.go
+++ x/perp/keeper/liquidate.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/NibiruChain/nibiru/x/common"
 	"github.com/NibiruChain/nibiru/x/perp/types"
+	vpooltypes "github.com/NibiruChain/nibiru/x/vpool/types"
 )
 
 type LiquidationOutput struct {
@@ -90,4 +91,74 @@ func (k Keeper) CreateLiquidation(
 		return LiquidationOutput{}, err
 	}
 	return output, err
+}
+
+/* CreatePartialLiquidation returns the 'LiquidationOutput' of a partial liquidation.
+
+Args:
+- ctx (sdk.Context): Carries information about the current state of the application.
+- pair (common.TokenPair): identifier for the virtual pool
+- trader (sdk.AccAddress): address of the owner of the position
+- position: the position that is will be partially liquidated
+
+Returns:
+- (*LiquidationOutput): fees, bad debt, and position response for the partial liquidation
+- (error): An error if one is raised.
+*/
+func (k Keeper) CreatePartialLiquidation(
+	ctx sdk.Context,
+	pair common.TokenPair,
+	trader sdk.AccAddress,
+	position *types.Position,
+) (*LiquidationOutput, error) {
+
+	// Get position direction: long or short
+	var (
+		dir vpooltypes.Direction
+	)
+	if position.Size_.GTE(sdk.ZeroDec()) {
+		dir = vpooltypes.Direction_ADD_TO_POOL
+	} else {
+		dir = vpooltypes.Direction_REMOVE_FROM_POOL
+	}
+
+	// Compute the notional of the portion of position that's being liquidated
+	params := k.GetParams(ctx)
+	partiallyLiquidatedPositionNotional, err := k.VpoolKeeper.GetBaseAssetPrice(
+		ctx, pair, dir,
+		/* baseAssetAmount */ position.Size_.Mul(params.GetPartialLiquidationRatioAsDec()).Abs(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Partially close (i.e. open reverse) the position
+	positionResp, err := k.openReversePosition(
+		/* ctx */ ctx,
+		/* currentPosition */ *position,
+		/* quoteAssetAmount */ partiallyLiquidatedPositionNotional,
+		/* leverage */ sdk.OneDec(),
+		/* baseAssetAmountLimit */ sdk.ZeroDec(),
+		/* canOverFluctuationLimit */ true,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Compute the liquidation penality, of which half goes to the liquidator
+	// and half goes to the ecosystem fund.
+	liquidationPenalty := positionResp.ExchangedQuoteAssetAmount.Mul(params.GetLiquidationFeeAsDec())
+	feeToLiquidator := liquidationPenalty.Quo(sdk.MustNewDecFromStr("2"))
+	feeToPerpEF := liquidationPenalty.Sub(feeToLiquidator)
+
+	positionResp.Position.Margin = positionResp.Position.Margin.Sub(liquidationPenalty)
+	k.SetPosition(ctx, pair, trader.String(), positionResp.Position)
+
+	return &LiquidationOutput{
+		FeeToPerpEcosystemFund: feeToPerpEF,
+		FeeToLiquidator:        feeToLiquidator,
+		PositionResp:           positionResp,
+		BadDebt:                positionResp.BadDebt,
+	}, nil
+
 }
