815fa8c825ed319345aaba5ee95772034c2da82f
Walter White <heisenberg@matrixsystems.co>
2022-05-21 12:34:27 -0400

    Add distribute liquidate rewards

--- x/perp/keeper/liquidate.go
+++ x/perp/keeper/liquidate.go
@@ -8,55 +8,6 @@ import (
 	"github.com/NibiruChain/nibiru/x/perp/types"
 )
 
-// ExecuteFullLiquidation fully liquidates a position.
-func (k Keeper) ExecuteFullLiquidation(
-	ctx sdk.Context, liquidator sdk.AccAddress, position *types.Position,
-) (err error) {
-	params := k.GetParams(ctx)
-
-	positionResp, err := k.closePositionEntirely(
-		ctx,
-		/* currentPosition */ *position,
-		/* quoteAssetAmountLimit */ sdk.ZeroDec())
-	if err != nil {
-		return err
-	}
-
-	remainMargin := positionResp.MarginToVault.Abs()
-
-	feeToLiquidator := params.GetLiquidationFeeAsDec().
-		Mul(positionResp.ExchangedQuoteAssetAmount).
-		QuoInt64(2)
-	totalBadDebt := positionResp.BadDebt
-
-	if feeToLiquidator.GT(remainMargin) {
-		// if the remainMargin is not enough for liquidationFee, count it as bad debt
-		liquidationBadDebt := feeToLiquidator.Sub(remainMargin)
-		totalBadDebt = totalBadDebt.Add(liquidationBadDebt)
-	} else {
-		// Otherwise, the remaining margin rest will be transferred to ecosystemFund
-		remainMargin = remainMargin.Sub(feeToLiquidator)
-	}
-
-	feeToPerpEcosystemFund := sdk.ZeroDec()
-	if remainMargin.IsPositive() {
-		feeToPerpEcosystemFund = remainMargin
-	}
-
-	err = k.distributeLiquidateRewards(ctx, types.LiquidateResp{
-		BadDebt:                totalBadDebt,
-		FeeToLiquidator:        feeToLiquidator,
-		FeeToPerpEcosystemFund: feeToPerpEcosystemFund,
-		Liquidator:             liquidator,
-		PositionResp:           positionResp,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func (k Keeper) distributeLiquidateRewards(
 	ctx sdk.Context, liquidateResp types.LiquidateResp) (err error) {
 	// --------------------------------------------------------------
