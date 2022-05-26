8601294c7bf03ecfe58dbfff01e74b2c338a16d7
Unique-Divine <realuniquedivine@gmail.com>
2022-05-26 10:43:51 -0400

    refactor: more CmdPrices to query.go

--- x/pricefeed/client/cli/query.go
+++ x/pricefeed/client/cli/query.go
@@ -15,10 +15,11 @@ import (
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd() *cobra.Command {
-	// Group pricefeed queries under a subcommand
+	// Group pricefeed queries as subcommands of query
 	queryCmd := &cobra.Command{
-		Use:                        types.ModuleName,
-		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		Use: types.ModuleName,
+		Short: fmt.Sprintf(
+			"Querying commands for the %s module", types.ModuleName),
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
@@ -57,6 +58,35 @@ func CmdPrice() *cobra.Command {
 			params := &types.QueryPriceRequest{PairId: pair}
 
 			res, err := queryClient.Price(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdPrices() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "prices",
+		Short: "Display current prices for all pairs",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryPricesRequest{}
+
+			res, err := queryClient.Prices(cmd.Context(), params)
 			if err != nil {
 				return err
 			}
