c39a3cafe75ad944dc2cc28f6611489855601d05
Agent Smith <agentsmith@matrixsystems.co>
2022-05-27 17:23:38 -0400

    fix imports

--- x/pricefeed/client/cli/query.go
+++ x/pricefeed/client/cli/query.go
@@ -3,15 +3,11 @@ package cli
 import (
 	"fmt"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 
 	"github.com/NibiruChain/nibiru/x/common"
-
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-
-	// sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/NibiruChain/nibiru/x/pricefeed/types"
 )
 
