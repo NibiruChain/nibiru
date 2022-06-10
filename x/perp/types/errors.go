69cb864f722ad64af491779a17d08bf2c5c41842
kai0x <kai0x@outlook.com>
2022-06-09 20:39:04 -0700

    clarify the error

--- x/perp/types/errors.go
+++ x/perp/types/errors.go
@@ -13,7 +13,7 @@ var (
 	ErrMarginHighEnough = sdkerrors.Register(ModuleName, 1,
 		"Margin is higher than required maintenant margin ratio")
 	ErrFailedToRemoveDueToBadDebt = sdkerrors.Register(ModuleName, 2,
-		"failed to remove margin; position has bad debt")
+		"failed to remove margin; position would have bad debt if margin is removed")
 	ErrPositionNotFound     = errors.New("no position found")
 	ErrPairNotFound         = errors.New("pair doesn't have live vpool")
 	ErrPairMetadataNotFound = errors.New("pair doesn't have metadata")
