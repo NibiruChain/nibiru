1b92609abed0a89195d5961b1b29aa5e1af26b4b
Kevin Yang <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-06-11 17:31:21 -0700

    Fix imports

--- x/epochs/keeper/keeper_test.go
+++ x/epochs/keeper/keeper_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/NibiruChain/nibiru/app"
 	"github.com/NibiruChain/nibiru/x/epochs/keeper"
 	"github.com/NibiruChain/nibiru/x/epochs/types"
 	testutilapp "github.com/NibiruChain/nibiru/x/testutil/app"
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/suite"
 )
 
 type KeeperTestSuite struct {
