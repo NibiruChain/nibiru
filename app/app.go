0af70c2292dbe08f7bc12c12e7660a07a4c2b597
Unique-Divine <realuniquedivine@gmail.com>
2022-06-20 08:24:21 -0500

    (app):register proposal handler in govRouter

--- app/app.go
+++ app/app.go
@@ -476,7 +476,8 @@ func NewNibiruApp(
 		AddRoute(paramproposal.RouterKey, params.NewParamChangeProposalHandler(app.ParamsKeeper)).
 		AddRoute(distrtypes.RouterKey, distr.NewCommunityPoolSpendProposalHandler(app.DistrKeeper)).
 		AddRoute(upgradetypes.RouterKey, upgrade.NewSoftwareUpgradeProposalHandler(app.UpgradeKeeper)).
-		AddRoute(ibcclienttypes.RouterKey, ibcclient.NewClientProposalHandler(app.IBCKeeper.ClientKeeper))
+		AddRoute(ibcclienttypes.RouterKey, ibcclient.NewClientProposalHandler(app.IBCKeeper.ClientKeeper)).
+		AddRoute(pricefeedtypes.RouterKey, pricefeed.NewProposalHandler(app.PricefeedKeeper))
 
 	app.TransferKeeper = ibctransferkeeper.NewKeeper(
 		appCodec,
