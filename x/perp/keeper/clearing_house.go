85bd870d7bcc3b7f0c16e48de18778985e022f61
NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-07-09 09:53:16 -0700

    Refactor ClosePosition to use closePositionEntirely

--- x/perp/keeper/clearing_house.go
+++ x/perp/keeper/clearing_house.go
@@ -608,33 +608,25 @@ func (k Keeper) closePositionEntirely(
 }
 
 // ClosePosition gets the current position, and calls OpenPosition to open a reverse position with amount equal to the current open notional.
-func (k Keeper) ClosePosition(ctx sdk.Context, pair common.AssetPair, addr sdk.AccAddress) (*types.PositionResp, error) {
-	position, err := k.PositionsState(ctx).Get(pair, addr)
+func (k Keeper) ClosePosition(ctx sdk.Context, pair common.AssetPair, traderAddr sdk.AccAddress) (*types.PositionResp, error) {
+	position, err := k.PositionsState(ctx).Get(pair, traderAddr)
 	if err != nil {
 		return nil, err
 	}
 
-	positionNotional, _, err := k.getPositionNotionalAndUnrealizedPnL(ctx, *position, types.PnLCalcOption_SPOT_PRICE)
+	positionResp, err := k.closePositionEntirely(ctx, *position, sdk.ZeroDec())
 	if err != nil {
 		return nil, err
 	}
 
-	positionResp, err := k.openReversePosition(
-		ctx,
-		*position,
-		positionNotional,
-		/* leverage */ sdk.OneDec(),
-		/* baseLimit */ sdk.ZeroDec(),
-		/* canOverFluctuationLimit */ false,
-	)
-	if err != nil {
-		return nil, err
+	if positionResp.BadDebt.IsPositive() {
+		return nil, fmt.Errorf("underwater position")
 	}
 
 	if err = k.afterPositionUpdate(
 		ctx,
 		pair,
-		addr,
+		traderAddr,
 		k.GetParams(ctx),
 		/* isNewPosition */ false,
 		*positionResp,
