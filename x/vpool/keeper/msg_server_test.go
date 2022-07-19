3f6485585200ffb24e542fcc5a8e4633dbce5f54
godismercilex <mercilex@matrixsystems.co>
2022-07-19 10:31:22 +0200

    add: msg server tests

--- x/vpool/keeper/msg_server_test.go
+++ x/vpool/keeper/msg_server_test.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"github.com/NibiruChain/nibiru/x/testutil/sample"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,6 +12,33 @@ import (
 	"github.com/NibiruChain/nibiru/x/testutil/mock"
 	"github.com/NibiruChain/nibiru/x/vpool/types"
 )
+
+func TestMsgServer_CreatePool(t *testing.T) {
+	poolCreator := sample.AccAddress()
+
+	msg := &types.MsgCreatePool{
+		Sender:                poolCreator.String(),
+		Pair:                  "BTC:USD",
+		TradeLimitRatio:       sdk.MustNewDecFromStr("0.2"),
+		QuoteAssetReserve:     sdk.NewDec(1_000_000),
+		BaseAssetReserve:      sdk.NewDec(100),
+		FluctuationLimitRatio: sdk.MustNewDecFromStr("0.2"),
+		MaxOracleSpreadRatio:  sdk.MustNewDecFromStr("0.05"),
+	}
+
+	priceFeed := mock.NewMockPricefeedKeeper(gomock.NewController(t))
+	priceFeed.EXPECT().
+		IsActivePair(gomock.Any(), gomock.Eq(msg.Pair)).
+		Return(true)
+
+	vpoolKeeper, ctx := VpoolKeeper(t, priceFeed)
+	require.NoError(t, vpoolKeeper.Whitelist(ctx).Add(poolCreator))
+
+	s := NewMsgServer(vpoolKeeper)
+
+	_, err := s.CreatePool(sdk.WrapSDKContext(ctx), msg)
+	require.NoError(t, err)
+}
 
 func TestQueryReserveAssets(t *testing.T) {
 	t.Log("initialize vpoolkeeper")
