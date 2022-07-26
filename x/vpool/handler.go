fc10055aa77baa26fabddfa4bda08062b26635dc
godismercilex <98415576+godismercilex@users.noreply.github.com>
2022-07-26 11:41:03 +0200

    feat(vpool): create pools gov handler (#725)
    * add: proposal proto and wiring
    
    * add: gov handler implementation
    
    * add: gov CLI handler
    
    * chore: fix comment
    
    * chore: lint
    
    * chore: CHANGELOG.md
    
    * add: cli tests
    
    * chore: les lintos
    
    * add: proposal tests
    
    * chore: lint
    
    * change: address reviews
    
    * change: address merge changes
    
    * chore: lint
    
    Co-authored-by: AgentSmithMatrix <98403347+AgentSmithMatrix@users.noreply.github.com>
    Co-authored-by: NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>

--- x/vpool/handler.go
+++ x/vpool/handler.go
@@ -3,6 +3,10 @@ package vpool
 import (
 	"fmt"
 
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+
+	"github.com/NibiruChain/nibiru/x/common"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
@@ -17,5 +21,32 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 
 		errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+	}
+}
+
+func NewCreatePoolProposalHandler(k keeper.Keeper) govtypes.Handler {
+	return func(ctx sdk.Context, content govtypes.Content) error {
+		switch m := content.(type) {
+		case *types.CreatePoolProposal:
+			if err := m.ValidateBasic(); err != nil {
+				return err
+			}
+			pair := common.MustNewAssetPair(m.Pair)
+			k.CreatePool(
+				ctx,
+				pair,
+				m.TradeLimitRatio,
+				m.QuoteAssetReserve,
+				m.BaseAssetReserve,
+				m.FluctuationLimitRatio,
+				m.MaxOracleSpreadRatio,
+				m.MaintenanceMarginRatio,
+			)
+			return nil
+		default:
+			return sdkerrors.Wrapf(
+				sdkerrors.ErrUnknownRequest,
+				"unrecognized %s proposal content type: %T", types.ModuleName, m)
+		}
 	}
 }
