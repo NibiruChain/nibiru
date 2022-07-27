471efa591559afdc7834491e239067c371333d40
Unique-Divine <realuniquedivine@gmail.com>
2022-07-27 13:50:15 -0500

    fix,docs: vpool gov cli test

--- x/vpool/client/cli/cli_test.go
+++ x/vpool/client/cli/cli_test.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdktestutil "github.com/cosmos/cosmos-sdk/testutil"
 	sdktestutilcli "github.com/cosmos/cosmos-sdk/testutil/cli"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -27,14 +25,14 @@ import (
 	vpooltypes "github.com/NibiruChain/nibiru/x/vpool/types"
 )
 
-type IntegrationTestSuite struct {
+type VpoolCLISuite struct {
 	suite.Suite
 
 	cfg     testutilcli.Config
 	network *testutilcli.Network
 }
 
-func (s *IntegrationTestSuite) SetupSuite() {
+func (s *VpoolCLISuite) SetupSuite() {
 	if testing.Short() {
 		s.T().Skip("skipping integration test suite")
 	}
@@ -61,29 +59,16 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.Assert().Equal(sdk.NewDec(10), res.Price.Price)
 }
 
-func (s *IntegrationTestSuite) TearDownSuite() {
+func (s *VpoolCLISuite) TearDownSuite() {
 	s.T().Log("tearing down integration test suite")
 	s.network.Cleanup()
 }
 
-func (s IntegrationTestSuite) TestX_CmdAddVpool() {
+func (s VpoolCLISuite) TestX_CmdAddVpool() {
+
 	s.Require().Len(s.network.Validators, 1)
 	val := s.network.Validators[0]
 	clientCtx := val.ClientCtx.WithOutputFormat("json")
-	proposer, _, err := val.ClientCtx.Keyring.NewMnemonic(
-		/* uid */ "proposer",
-		/* language */ keyring.English,
-		/* hdPath */ sdk.FullFundraiserPath,
-		/* bip39Passphrase */ "",
-		/* algo */ hd.Secp256k1,
-	)
-	s.Require().NoError(err)
-
-	s.T().Log("Fill proposer wallet to pay gas for prosal")
-	gasTokens := sdk.NewCoins(sdk.NewInt64Coin(s.cfg.BondDenom, 100_000_000))
-	oracle := sdk.AccAddress(proposer.GetPubKey().Address())
-	_, err = testutilcli.FillWalletFromValidator(oracle, gasTokens, val, s.cfg.BondDenom)
-	s.Require().NoError(err)
 
 	s.T().Log("load example json as bytes")
 	proposal := &vpooltypes.CreatePoolProposal{
@@ -131,7 +116,7 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 	s.Assert().EqualValues(0, txResp.Code, out.String())
 
 	s.T().Log(`Check that proposal was correctly submitted with gov client
-			$ nibid query gov proposal 1`)
+			$ nibid query gov proposal 2`)
 	// the proposal tx won't be included until next block
 	s.Assert().NoError(s.network.WaitForNextBlock())
 	govQueryClient := govtypes.NewQueryClient(clientCtx)
@@ -153,11 +138,12 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 
 	s.T().Log(`Move proposal to vote status by meeting min deposit
 			$ nibid tx gov deposit [proposal-id] [deposit] [flags]`)
+	expectedProposalIDStr := "1"
 	govDepositParams, err := govQueryClient.Params(
 		context.Background(), &govtypes.QueryParamsRequest{ParamsType: govtypes.ParamDeposit})
 	s.Assert().NoError(err)
 	args = []string{
-		/*proposal-id=*/ "1",
+		/*proposal-id=*/ expectedProposalIDStr,
 		/*deposit=*/ govDepositParams.DepositParams.MinDeposit.String(),
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 		fmt.Sprintf("--%s=test", flags.FlagKeyringBackend),
@@ -180,7 +166,7 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 			$ nibid tx gov vote [proposal-id] [option] [flags]
 			e.g. $ nibid tx gov vote 1 yes`)
 	args = []string{
-		/*proposal-id=*/ "1",
+		/*proposal-id=*/ expectedProposalIDStr,
 		/*option=*/ "yes",
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 		fmt.Sprintf("--%s=test", flags.FlagKeyringBackend),
@@ -223,10 +209,9 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 			found = true
 		}
 	}
-
 	require.True(s.T(), found, "pool does not exist")
 }
 
-func TestIntegrationTestSuite(t *testing.T) {
-	suite.Run(t, new(IntegrationTestSuite))
+func TestVpoolCLISuite(t *testing.T) {
+	suite.Run(t, new(VpoolCLISuite))
 }
