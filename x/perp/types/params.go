37a0dc01d28b51e5780fe6c2917059d1543bdb3f
NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-08-03 13:30:51 -0700

    Remove genesis params

--- x/perp/types/params.go
+++ x/perp/types/params.go
@@ -79,15 +79,7 @@ func NewParams(
 
 // DefaultParams returns the default parameters for the x/perp module.
 func DefaultParams() Params {
-	return NewParams(
-		/* stopped */ false,
-		/* feePoolFeeRatio */ sdk.MustNewDecFromStr("0.001"), // 10 bps
-		/* ecosystemFundFeeRatio */ sdk.MustNewDecFromStr("0.001"), // 10 bps
-		/* liquidationFee */ sdk.MustNewDecFromStr("0.025"), // 250 bps
-		/* partialLiquidationRatio */ sdk.MustNewDecFromStr("0.25"),
-		/* epochIdentifier */ "30 min",
-		/* twapLookbackWindow */ 15*time.Minute,
-	)
+	return Params{}
 }
 
 // Validate validates the set of params
