c5ba451c45b799b582b2e1e72152de7f1c1e0189
godismercilex <mercilex@matrixsystems.co>
2022-08-03 12:03:08 +0200

    add: init oracle

---
+++ x/oracle/exported/alias.go
@@ -0,0 +1,10 @@
+//nolint:deadcode,unused
+//DONTCOVER
+package exported
+
+import "github.com/NibiruChain/nibiru/x/oracle/types"
+
+type (
+	MsgAggregateExchangeRatePrevote = types.MsgAggregateExchangeRatePrevote
+	MsgAggregateExchangeRateVote    = types.MsgAggregateExchangeRateVote
+)
---
+++ x/oracle/types/genesis_test.go
@@ -0,0 +1,27 @@
+package types_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/NibiruChain/nibiru/app"
+	"github.com/NibiruChain/nibiru/x/oracle/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesisValidation(t *testing.T) {
+	genState := types.DefaultGenesisState()
+	require.NoError(t, types.ValidateGenesis(genState))
+
+	genState.Params.VotePeriod = 0
+	require.Error(t, types.ValidateGenesis(genState))
+}
+
+func TestGetGenesisStateFromAppState(t *testing.T) {
+	cdc := app.MakeEncodingConfig().Marshaler
+	appState := make(map[string]json.RawMessage)
+
+	defaultGenesisState := types.DefaultGenesisState()
+	appState[types.ModuleName] = cdc.MustMarshalJSON(defaultGenesisState)
+	require.Equal(t, *defaultGenesisState, *types.GetGenesisStateFromAppState(cdc, appState))
+}
