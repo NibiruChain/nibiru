b6f6ce237f549b9f7e3daaf6d4449490280005c4
godismercilex <mercilex@matrixsystems.co>
2022-08-05 13:23:51 +0200

    change: ParseTuplesStr logic to use pairs instead of denoms

--- x/oracle/types/vote.go
+++ x/oracle/types/vote.go
@@ -158,31 +158,10 @@ func (tuples ExchangeRateTuples) ToString() (string, error) {
 
 // ParseExchangeRateTuples ExchangeRateTuple parser
 func ParseExchangeRateTuples(tuplesStr string) (ExchangeRateTuples, error) {
-	tuplesStr = strings.TrimSpace(tuplesStr)
-	if len(tuplesStr) == 0 {
-		return nil, nil
+	t := new(ExchangeRateTuples)
+	if err := t.FromString(tuplesStr); err != nil {
+		return nil, err
 	}
 
-	tupleStrs := strings.Split(tuplesStr, ",")
-	tuples := make(ExchangeRateTuples, len(tupleStrs))
-	duplicateCheckMap := make(map[string]bool)
-	for i, tupleStr := range tupleStrs {
-		decCoin, err := sdk.ParseDecCoin(tupleStr)
-		if err != nil {
-			return nil, err
-		}
-
-		tuples[i] = ExchangeRateTuple{
-			Pair:         decCoin.Denom,
-			ExchangeRate: decCoin.Amount,
-		}
-
-		if _, ok := duplicateCheckMap[decCoin.Denom]; ok {
-			return nil, fmt.Errorf("duplicated denom %s", decCoin.Denom)
-		}
-
-		duplicateCheckMap[decCoin.Denom] = true
-	}
-
-	return tuples, nil
+	return *t, nil
 }
