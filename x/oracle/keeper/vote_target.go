f2c859cc5cb4f89543b4f8fa2cd61d6a95c9d8b7
godismercilex <mercilex@matrixsystems.co>
2022-08-08 14:21:58 +0200

    chore: rename denom=>pair vote_target.go

--- x/oracle/keeper/vote_target.go
+++ x/oracle/keeper/vote_target.go
@@ -2,16 +2,16 @@ package keeper
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// IsVoteTarget returns existence of a denom in the the voting target list
-func (k Keeper) IsVoteTarget(ctx sdk.Context, denom string) bool {
-	_, err := k.GetTobinTax(ctx, denom)
+// IsVoteTarget returns existence of a pair in the voting target list
+func (k Keeper) IsVoteTarget(ctx sdk.Context, pair string) bool {
+	_, err := k.GetTobinTax(ctx, pair)
 	return err == nil
 }
 
 // GetVoteTargets returns the voting target list on current vote period
 func (k Keeper) GetVoteTargets(ctx sdk.Context) (voteTargets []string) {
-	k.IterateTobinTaxes(ctx, func(denom string, _ sdk.Dec) bool {
-		voteTargets = append(voteTargets, denom)
+	k.IterateTobinTaxes(ctx, func(pair string, _ sdk.Dec) bool {
+		voteTargets = append(voteTargets, pair)
 		return false
 	})
 
