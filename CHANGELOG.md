701b27fc9dc9418098258ddba9e6333da57186cb
Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
2022-08-15 09:35:01 -0300

    feat(app): Make post price txs gasless (#817)
    * feat: Gasless txs for post price and liquidate
    
    * fix: Fix anteHandler call to include keepers
    
    * fix: Update changelog and lint fix
    
    * fix: merge with master
    
    * fix,docs: vpool gov cli test
    
    * docs(vpool,pricefeed): readadbility and cli commenets for the gov cli tests
    
    * fix: check for error on chainedhandler
    
    * wip: Update the fees to 0 in cli test for pricefeed
    
    * feat: Fork fee deduct ante-handler
    
    * fix: Remove unused function
    
    * tests: Add test for gas or no gas in post price cli test
    
    * feat: Update changelog
    
    * fix: Remove perpkeeper from gasless ante
    
    * fix: Remove println from codebase
    
    * test gasmeter infinite
    
    * change test for gassless decorator
    
    * update gasless test
    
    * remove unused code
    
    * fix comment
    
    * remove unused code
    
    * linter
    
    * linter 2
    
    * final clean
    
    * remove unused func
    
    * remove comment
    
    * fix: Fix logic on pricefeed query tests for cli_test
    
    * test: Add test for simulation run
    
    * fix: Remove nil check for address of variable
    
    Co-authored-by: matthiasmatt <matthias@matrixsystems.co>
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>
    Co-authored-by: AgentSmithMatrix <98403347+AgentSmithMatrix@users.noreply.github.com>
    Co-authored-by: Agent Smith <agentsmith@matrixsystems.co>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -54,6 +54,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * [#801](https://github.com/NibiruChain/nibiru/pull/801) - remove unused pair constants
 * [#788](https://github.com/NibiruChain/nibiru/pull/788) - add --overwrite flag to the nibid init call of localnet.sh
 * [#804](https://github.com/NibiruChain/nibiru/pull/804) - bump ibc-go to v3.1.1
+* [#817](https://github.com/NibiruChain/nibiru/pull/817) - Make post prices transactions gasless for whitelisted oracles
 * [#818](https://github.com/NibiruChain/nibiru/pull/818) - fix(localnet.sh): add max leverage to vpools in genesis to fix open-position
 * [#819](https://github.com/NibiruChain/nibiru/pull/819) - add golangci-linter using docker in Makefile
 
--- x/vpool/client/cli/cli_test.go
+++ x/vpool/client/cli/cli_test.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdktestutil "github.com/cosmos/cosmos-sdk/testutil"
 	sdktestutilcli "github.com/cosmos/cosmos-sdk/testutil/cli"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -27,14 +25,14 @@ import (
 	vpooltypes "github.com/NibiruChain/nibiru/x/vpool/types"
 )
 
-type IntegrationTestSuite struct {
+type VpoolCLISuite struct {
 	suite.Suite
 
 	cfg     testutilcli.Config
 	network *testutilcli.Network
 }
 
-func (s *IntegrationTestSuite) SetupSuite() {
+func (s *VpoolCLISuite) SetupSuite() {
 	if testing.Short() {
 		s.T().Skip("skipping integration test suite")
 	}
@@ -58,31 +56,19 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.Assert().Equal(sdk.NewDec(10), res.Price.Price)
 }
 
-func (s *IntegrationTestSuite) TearDownSuite() {
+func (s *VpoolCLISuite) TearDownSuite() {
 	s.T().Log("tearing down integration test suite")
 	s.network.Cleanup()
 }
 
-func (s IntegrationTestSuite) TestX_CmdAddVpool() {
+func (s VpoolCLISuite) TestX_CmdAddVpool() {
 	s.Require().Len(s.network.Validators, 1)
 	val := s.network.Validators[0]
 	clientCtx := val.ClientCtx.WithOutputFormat("json")
-	proposer, _, err := val.ClientCtx.Keyring.NewMnemonic(
-		/* uid */ "proposer",
-		/* language */ keyring.English,
-		/* hdPath */ sdk.FullFundraiserPath,
-		/* bip39Passphrase */ "",
-		/* algo */ hd.Secp256k1,
-	)
-	s.Require().NoError(err)
 
-	s.T().Log("Fill proposer wallet to pay gas for prosal")
-	gasTokens := sdk.NewCoins(sdk.NewInt64Coin(s.cfg.BondDenom, 100_000_000))
-	oracle := sdk.AccAddress(proposer.GetPubKey().Address())
-	_, err = testutilcli.FillWalletFromValidator(oracle, gasTokens, val, s.cfg.BondDenom)
-	s.Require().NoError(err)
-
-	s.T().Log("load example json as bytes")
+	// ----------------------------------------------------------------------
+	s.T().Log("load example proposal json as bytes")
+	// ----------------------------------------------------------------------
 	proposal := &vpooltypes.CreatePoolProposal{
 		Title:                  "Create ETH:USD pool",
 		Description:            "Creates an ETH:USD pool",
@@ -107,7 +93,9 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 	val.ClientCtx.Codec.MustUnmarshalJSON(contents, proposal)
 	s.Require().NoError(proposal.ValidateBasic())
 
+	// ----------------------------------------------------------------------
 	s.T().Log("Submit proposal and unmarshal tx response")
+	// ----------------------------------------------------------------------
 	args := []string{
 		proposalJSON.Name(),
 		fmt.Sprintf("--%s=1000unibi", govcli.FlagDeposit),
@@ -129,8 +117,10 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 	s.Assert().NoError(err)
 	s.Assert().EqualValues(0, txResp.Code, out.String())
 
+	// ----------------------------------------------------------------------
 	s.T().Log(`Check that proposal was correctly submitted with gov client
 			$ nibid query gov proposal 1`)
+	// ----------------------------------------------------------------------
 	// the proposal tx won't be included until next block
 	s.Assert().NoError(s.network.WaitForNextBlock())
 	govQueryClient := govtypes.NewQueryClient(clientCtx)
@@ -150,13 +140,16 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 		proposalsQueryResponse.Proposals[0].TotalDeposit,
 	)
 
+	// ----------------------------------------------------------------------
 	s.T().Log(`Move proposal to vote status by meeting min deposit
 			$ nibid tx gov deposit [proposal-id] [deposit] [flags]`)
+	// ----------------------------------------------------------------------
+	expectedProposalIDStr := "1"
 	govDepositParams, err := govQueryClient.Params(
 		context.Background(), &govtypes.QueryParamsRequest{ParamsType: govtypes.ParamDeposit})
 	s.Assert().NoError(err)
 	args = []string{
-		/*proposal-id=*/ "1",
+		/*proposal-id=*/ expectedProposalIDStr,
 		/*deposit=*/ govDepositParams.DepositParams.MinDeposit.String(),
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 		fmt.Sprintf("--%s=test", flags.FlagKeyringBackend),
@@ -175,11 +168,13 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 		proposalsQueryResponse.Proposals[0].Status,
 		"proposal should be in voting period since min deposit has been met")
 
+	// ----------------------------------------------------------------------
 	s.T().Log(`Vote on the proposal.
 			$ nibid tx gov vote [proposal-id] [option] [flags]
 			e.g. $ nibid tx gov vote 1 yes`)
+	// ----------------------------------------------------------------------
 	args = []string{
-		/*proposal-id=*/ "1",
+		/*proposal-id=*/ expectedProposalIDStr,
 		/*option=*/ "yes",
 		fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
 		fmt.Sprintf("--%s=test", flags.FlagKeyringBackend),
@@ -201,7 +196,9 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 	}, 20*time.Second, 2*time.Second,
 		"proposal should pass after voting period")
 
+	// ----------------------------------------------------------------------
 	s.T().Log("verify that the new proposed pool exists")
+	// ----------------------------------------------------------------------
 	cmd = cli.CmdGetVpools()
 	args = []string{}
 	queryResp := &vpooltypes.QueryAllPoolsResponse{}
@@ -223,10 +220,9 @@ func (s IntegrationTestSuite) TestX_CmdAddVpool() {
 			found = true
 		}
 	}
-
 	require.True(s.T(), found, "pool does not exist")
 }
 
-func TestIntegrationTestSuite(t *testing.T) {
-	suite.Run(t, new(IntegrationTestSuite))
+func TestVpoolCLISuite(t *testing.T) {
+	suite.Run(t, new(VpoolCLISuite))
 }
