e27157e2e3ee99292f47820b42cabf4a7e1fbc82
SomeName <104315304+superfiredoge@users.noreply.github.com>
2022-08-19 03:41:52 +0530

    feat(genesis): add vpool at genesis (#827)
    * feat: add vpool at genesis
    
    * change json to flag parse
    
    * fix lint
    
    * chore: changelog.md
    
    * root cmd and tests
    
    * move add-genesis to vpool cmd
    
    * adding command to root
    
    * flags to args
    
    * fix changelog
    
    Co-authored-by: AgentSmithMatrix <98403347+AgentSmithMatrix@users.noreply.github.com>
    Co-authored-by: Agent Smith <agentsmith@matrixsystems.co>

---
+++ x/vpool/client/cli/genvpool.go
@@ -0,0 +1,130 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	"github.com/spf13/cobra"
+
+	"github.com/NibiruChain/nibiru/x/common"
+	"github.com/NibiruChain/nibiru/x/vpool/types"
+)
+
+var (
+	FlagPair                   = "pair"
+	FlagBaseAssetReserve       = "base-asset-reserve"
+	FlagQuoteAssetReserve      = "quote-asset-reserve"
+	FlagTradeLimitRatio        = "trade-limit-ratio"
+	FlagFluctuationLimitRatio  = "fluctuation-limit-ratio"
+	FlagMaxOracleSpreadRatio   = "maxOracle-spread-ratio"
+	FlagMaintenanceMarginRatio = "maintenance-margin-ratio"
+	FlagMaxLeverage            = "max-leverage"
+)
+
+// AddVPoolGenesisCmd returns add-vpool-genesis
+func AddVPoolGenesisCmd(defaultNodeHome string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: fmt.Sprintf("add-genesis-vpool [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s]", FlagPair, FlagBaseAssetReserve,
+			FlagQuoteAssetReserve, FlagTradeLimitRatio, FlagFluctuationLimitRatio, FlagMaxOracleSpreadRatio,
+			FlagMaintenanceMarginRatio, FlagMaxLeverage),
+		Short: "Add vPools to genesis.json",
+		Long:  `Add vPools to genesis.json.`,
+		Args:  cobra.ExactArgs(8),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
+
+			config.SetRoot(clientCtx.HomeDir)
+
+			genFile := config.GenesisFile()
+			appState, genDoc, err := genutiltypes.GenesisStateFromGenFile(genFile)
+			if err != nil {
+				return err
+			}
+
+			vPool, err := parseVpoolParams(args)
+			if err != nil {
+				return err
+			}
+
+			vPoolGenState := types.GetGenesisStateFromAppState(clientCtx.Codec, appState)
+			vPoolGenState.Vpools = append(vPoolGenState.Vpools, vPool)
+
+			vPoolGenStateBz, err := clientCtx.Codec.MarshalJSON(vPoolGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal bank genesis state: %w", err)
+			}
+
+			appState[types.ModuleName] = vPoolGenStateBz
+
+			appStateJSON, err := json.Marshal(appState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal application genesis state: %w", err)
+			}
+
+			genDoc.AppState = appStateJSON
+			return genutil.ExportGenesisFile(genDoc, genFile)
+		},
+	}
+
+	cmd.Flags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func parseVpoolParams(args []string) (*types.Pool, error) {
+	vPair, err := common.NewAssetPair(args[0])
+	if err != nil {
+		return nil, err
+	}
+
+	baseAsset, err := sdk.NewDecFromStr(args[1])
+	if err != nil {
+		return nil, err
+	}
+	quoteAsset, err := sdk.NewDecFromStr(args[2])
+	if err != nil {
+		return nil, err
+	}
+	tradeLimit, err := sdk.NewDecFromStr(args[3])
+	if err != nil {
+		return nil, err
+	}
+	fluctuationLimitRatio, err := sdk.NewDecFromStr(args[4])
+	if err != nil {
+		return nil, err
+	}
+	maxOracleSpread, err := sdk.NewDecFromStr(args[5])
+	if err != nil {
+		return nil, err
+	}
+	maintenanceMarginRatio, err := sdk.NewDecFromStr(args[6])
+	if err != nil {
+		return nil, err
+	}
+	maxLeverage, err := sdk.NewDecFromStr(args[7])
+	if err != nil {
+		return nil, err
+	}
+
+	vPool := types.NewPool(
+		vPair,
+		tradeLimit,
+		quoteAsset,
+		baseAsset,
+		fluctuationLimitRatio,
+		maxOracleSpread,
+		maintenanceMarginRatio,
+		maxLeverage,
+	)
+
+	return vPool, nil
+}
---
+++ x/vpool/client/cli/genvpool_test.go
@@ -0,0 +1,145 @@
+package cli_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	genutiltest "github.com/cosmos/cosmos-sdk/x/genutil/client/testutil"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/NibiruChain/nibiru/x/vpool/client/cli"
+)
+
+var testModuleBasicManager = module.NewBasicManager(genutil.AppModuleBasic{})
+
+// Tests "add-genesis-vpool", a command that adds a vpool to genesis.json
+func TestAddGenesisVpoolCmd(t *testing.T) {
+	type TestCase struct {
+		name          string
+		pairName      string
+		baseAsset     string
+		quoteAsset    string
+		tradeLimit    string
+		flucLimit     string
+		maxOracle     string
+		maintainRatio string
+		maxLeverage   string
+		expectError   bool
+	}
+
+	var executeTest = func(t *testing.T, testCase TestCase) {
+		tc := testCase
+		t.Run(tc.name, func(t *testing.T) {
+			home := t.TempDir()
+			logger := log.NewNopLogger()
+			cfg, err := genutiltest.CreateDefaultTendermintConfig(home)
+			require.NoError(t, err)
+
+			appCodec := simapp.MakeTestEncodingConfig().Marshaler
+			err = genutiltest.ExecInitCmd(
+				testModuleBasicManager, home, appCodec)
+			require.NoError(t, err)
+
+			serverCtx := server.NewContext(viper.New(), cfg, logger)
+			clientCtx := client.Context{}.WithCodec(appCodec).WithHomeDir(home)
+
+			ctx := context.Background()
+			ctx = context.WithValue(ctx, client.ClientContextKey, &clientCtx)
+			ctx = context.WithValue(ctx, server.ServerContextKey, serverCtx)
+
+			cmd := cli.AddVPoolGenesisCmd("home")
+			cmd.SetArgs([]string{
+				tc.pairName,
+				tc.baseAsset,
+				tc.maxOracle,
+				tc.maxLeverage,
+				tc.tradeLimit,
+				tc.maintainRatio,
+				tc.flucLimit,
+				tc.quoteAsset,
+				fmt.Sprintf("--%s=home", flags.FlagHome)})
+
+			if tc.expectError {
+				require.Error(t, cmd.ExecuteContext(ctx))
+			} else {
+				require.NoError(t, cmd.ExecuteContext(ctx))
+			}
+		})
+	}
+
+	testCases := []TestCase{
+		{
+			name:          "pair name empty",
+			pairName:      "",
+			baseAsset:     "1",
+			quoteAsset:    "1",
+			tradeLimit:    "1",
+			flucLimit:     "1",
+			maxOracle:     "1",
+			maintainRatio: "1",
+			maxLeverage:   "1",
+			expectError:   true,
+		},
+		{
+			name:          "invalid pair name",
+			pairName:      "token0:token1:token2",
+			baseAsset:     "1",
+			quoteAsset:    "1",
+			tradeLimit:    "1",
+			flucLimit:     "1",
+			maxOracle:     "1",
+			maintainRatio: "1",
+			maxLeverage:   "1",
+			expectError:   true,
+		},
+		{
+			name:          "invalid flag input",
+			pairName:      "token0:token1",
+			baseAsset:     "1",
+			quoteAsset:    "1",
+			tradeLimit:    "test",
+			flucLimit:     "1",
+			maxOracle:     "1",
+			maintainRatio: "1",
+			maxLeverage:   "1",
+			expectError:   true,
+		},
+		{
+			name:          "empty vpool fields",
+			pairName:      "token0:token1",
+			baseAsset:     "",
+			quoteAsset:    "1",
+			tradeLimit:    "1",
+			flucLimit:     "1",
+			maxOracle:     "1",
+			maintainRatio: "1",
+			maxLeverage:   "1",
+			expectError:   true,
+		},
+		{
+			name:          "valid vpool pair",
+			pairName:      "token0:token1",
+			baseAsset:     "100",
+			quoteAsset:    "100",
+			tradeLimit:    "0.1",
+			flucLimit:     "0.1",
+			maxOracle:     "0.1",
+			maintainRatio: "0.1",
+			maxLeverage:   "10",
+			expectError:   false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		executeTest(t, testCase)
+	}
+}
