c931bc41578c5d334a3cba91cc9802bcf4c643f5
godismercilex <mercilex@matrixsystems.co>
2022-08-22 23:51:07 +0800

    change: move rewardsforpair from alias_functions.go to reward.go

--- x/oracle/keeper/alias_functions.go
+++ x/oracle/keeper/alias_functions.go
@@ -4,22 +4,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-	"github.com/NibiruChain/nibiru/x/common"
-
 	"github.com/NibiruChain/nibiru/x/oracle/types"
 )
 
 // GetOracleAccount returns oracle ModuleAccount
 func (k Keeper) GetOracleAccount(ctx sdk.Context) authtypes.ModuleAccountI {
 	return k.accountKeeper.GetModuleAccount(ctx, types.ModuleName)
-}
-
-// GetRewardsForPair retrieves the balance of the oracle module account
-func (k Keeper) GetRewardsForPair(ctx sdk.Context, denom string) sdk.Coin {
-	// TODO(mercilex): this logic needs to be redefined. https://github.com/NibiruChain/nibiru/issues/805
-	if denom != common.DenomGov {
-		return sdk.NewCoin("zero", sdk.ZeroInt())
-	}
-	acc := k.accountKeeper.GetModuleAccount(ctx, types.ModuleName)
-	return k.bankKeeper.GetBalance(ctx, acc.GetAddress(), denom)
 }
--- x/oracle/keeper/reward.go
+++ x/oracle/keeper/reward.go
@@ -159,3 +159,13 @@ func (k Keeper) RewardBallotWinners(
 		panic(fmt.Sprintf("[oracle] Failed to send coins to distribution module %s", err.Error()))
 	}
 }
+
+// GetRewardsForPair retrieves the balance of the oracle module account
+func (k Keeper) GetRewardsForPair(ctx sdk.Context, denom string) sdk.Coin {
+	// TODO(mercilex): this logic needs to be redefined. https://github.com/NibiruChain/nibiru/issues/805
+	if denom != common.DenomGov {
+		return sdk.NewCoin("zero", sdk.ZeroInt())
+	}
+	acc := k.accountKeeper.GetModuleAccount(ctx, types.ModuleName)
+	return k.bankKeeper.GetBalance(ctx, acc.GetAddress(), denom)
+}
