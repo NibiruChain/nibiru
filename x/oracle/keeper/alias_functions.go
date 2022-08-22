7e6d26a110ad678dd08b9e16b74f568aa462ebc9
godismercilex <mercilex@matrixsystems.co>
2022-08-22 21:04:02 +0800

    chore: remove unused function

--- x/oracle/keeper/alias_functions.go
+++ x/oracle/keeper/alias_functions.go
@@ -23,9 +23,3 @@ func (k Keeper) GetRewardPool(ctx sdk.Context, denom string) sdk.Coin {
 	acc := k.accountKeeper.GetModuleAccount(ctx, types.ModuleName)
 	return k.bankKeeper.GetBalance(ctx, acc.GetAddress(), denom)
 }
-
-// GetRewardPool retrieves the balance of the oracle module account
-func (k Keeper) GetRewardPoolLegacy(ctx sdk.Context) sdk.Coins {
-	acc := k.accountKeeper.GetModuleAccount(ctx, types.ModuleName)
-	return k.bankKeeper.GetAllBalances(ctx, acc.GetAddress())
-}
