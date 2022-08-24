e75000cf4139d782ef59eacc4d9ba354dee9f115
Agent Smith <agentsmith@matrixsystems.co>
2022-08-24 14:20:57 +0200

    add check in max leverage

--- x/vpool/client/cli/genvpool.go
+++ x/vpool/client/cli/genvpool.go
@@ -98,21 +98,31 @@ func parseVpoolParams(args []string) (*types.Pool, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	fluctuationLimitRatio, err := sdk.NewDecFromStr(args[4])
 	if err != nil {
 		return nil, err
 	}
+
 	maxOracleSpread, err := sdk.NewDecFromStr(args[5])
 	if err != nil {
 		return nil, err
 	}
+
 	maintenanceMarginRatio, err := sdk.NewDecFromStr(args[6])
 	if err != nil {
 		return nil, err
 	}
+
 	maxLeverage, err := sdk.NewDecFromStr(args[7])
 	if err != nil {
 		return nil, err
+	}
+
+	fmt.Printf("%s", maxLeverage)
+
+	if !maxLeverage.GT(sdk.ZeroDec()) {
+		return nil, fmt.Errorf("invalid max leverage value")
 	}
 
 	vPool := types.NewPool(
