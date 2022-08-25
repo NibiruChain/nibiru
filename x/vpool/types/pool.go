6ddb49eb3a9dd8b5d59530725326e84a180fa2ce
NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-08-25 07:40:02 -0700

    fix: update error messages

--- x/vpool/types/pool.go
+++ x/vpool/types/pool.go
@@ -157,7 +157,7 @@ func (m *Pool) Validate() error {
 	}
 	// trade limit ratio always between 0 and 1
 	if m.TradeLimitRatio.LT(sdk.ZeroDec()) || m.TradeLimitRatio.GT(sdk.OneDec()) {
-		return fmt.Errorf("trade limit ratio must be 0 <= ratio <= 1")
+		return fmt.Errorf("trade limit ratio must be 0 < ratio < 1")
 	}
 
 	// quote asset reserve always > 0
@@ -172,20 +172,20 @@ func (m *Pool) Validate() error {
 
 	// fluctuation limit ratio between 0 and 1
 	if m.FluctuationLimitRatio.LT(sdk.ZeroDec()) || m.FluctuationLimitRatio.GT(sdk.OneDec()) {
-		return fmt.Errorf("fluctuation limit ratio must be 0 <= ratio <= 1")
+		return fmt.Errorf("fluctuation limit ratio must be 0 < ratio < 1")
 	}
 
 	// max oracle spread ratio between 0 and 1
 	if m.MaxOracleSpreadRatio.LT(sdk.ZeroDec()) || m.MaxOracleSpreadRatio.GT(sdk.OneDec()) {
-		return fmt.Errorf("max oracle spread ratio must be 0 <= ratio <= 1")
+		return fmt.Errorf("max oracle spread ratio must be 0 < ratio < 1")
 	}
 
 	if m.MaintenanceMarginRatio.LT(sdk.ZeroDec()) || m.MaintenanceMarginRatio.GT(sdk.OneDec()) {
-		return fmt.Errorf("maintenance margin ratio ratio must be 0 <= ratio <= 1")
+		return fmt.Errorf("maintenance margin ratio ratio must be 0 < ratio < 1")
 	}
 
 	if m.MaxLeverage.LTE(sdk.ZeroDec()) {
-		return fmt.Errorf("Max leverage must be >= 0")
+		return fmt.Errorf("Max leverage must be > 0")
 	}
 
 	if sdk.OneDec().Quo(m.MaxLeverage).LT(m.MaintenanceMarginRatio) {
