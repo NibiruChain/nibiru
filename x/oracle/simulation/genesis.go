de632c3170e36073fe18a33d1c204efff0c65937
godismercilex <98415576+godismercilex@users.noreply.github.com>
2022-08-25 17:51:25 +0200

    feat(oracle): remove reference pair (#846)
    * add: wire oracle
    
    * temp: attempt to fix localnet.sh
    
    * upgrade changelog
    
    * add: slash logging
    
    * add: miss counters logging
    
    * tmp
    
    * fix: almost there....
    
    * add: finalize first oracle scenario integration test
    
    * add: function to find private key info for an address
    
    * fix: test
    
    * chore: move oracle to simapp
    
    * chore: lint
    
    * chore: docs and function simplification
    
    * chore: CHANGELOG.md
    
    * chore: move update exchange rates logic to another function
    
    * fix: make clear exchange rates semantically correct
    
    * change: rename Claim to ValidatorPerformance
    
    * change: rename Claim to ValidatorPerformance 2
    
    * change: rename Claim to ValidatorPerformance 3
    
    * chore: document function
    
    * temp: move app_test.go to a separate pkg
    
    * refactor: move exchange rate update logic to the keeper
    
    * chore: simplify genesis tests
    
    * chore: move abci_test to keeper
    
    * change: move slash_test package to keeper_test
    
    * change: move slashing tests to slash_test.go
    
    * chore: rename abci_test to update_exchange_rate_test.go
    
    * chore: lint
    
    * chore: CHANGELOG.md
    
    * chore: remove unused function
    
    * change: rename GetRewardsPool to GetRewardsForPair
    
    * change: remove distribution window parameter
    
    * change: adjust RewardBallotWinners
    
    * chore: adjust doc
    
    * add: rewards protos
    
    * temp: panics so i don't forget
    
    * add: initial keeper methods
    
    * chore: adjust genesis
    
    * change: move rewardsforpair from alias_functions.go to reward.go
    
    * add: rewards distribution logic
    
    * fix: err check
    
    * chore: adjust TestOracleRewardDistribution
    
    * chore: document test
    
    * chore: rename variables to clarify scenario
    
    * chore: adjust test in update_exchange_rate_test.go
    
    * chore: rename denom reference to pair
    
    * chore: remove unused test
    
    * chore: adjust update_exchange_rate_test.go
    
    * add: rewards tests
    
    * change: API name
    
    * chore: lint
    
    * chore: update CHANGELOG.md
    
    * remove: cross rate with sort + test
    
    * remove: partially remove logic of PickReferencePair
    
    * change: rename PickReferencePair to RemoveInvalidBallots and adjust its functionality to only remove invalid ballots.
    
    * rename: update_exchange_rate_test.go to update_exchange_rates_test.go
    
    * fix: redundant
    
    * chore: CHANGELOG.md
    
    Co-authored-by: Agent Smith <agentsmith@matrixsystems.co>

--- x/oracle/simulation/genesis.go
+++ x/oracle/simulation/genesis.go
@@ -119,7 +119,7 @@ func RandomizedGenState(simState *module.SimulationState) {
 		[]types.AggregateExchangeRatePrevote{},
 		[]types.AggregateExchangeRateVote{},
 		[]types.Pair{},
-		[]*types.PairReward{},
+		[]types.PairReward{},
 	)
 
 	bz, err := json.MarshalIndent(&oracleGenesis.Params, "", " ")
