28a3d5fc73ee198b71b87f5d7d21e5d6c0590e03
godismercilex <mercilex@matrixsystems.co>
2022-09-02 11:15:01 +0800

    add: cover case of json handler disconnecting and failing to parse the msg

--- feeder/websocket/websocket.go
+++ feeder/websocket/websocket.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"encoding/json"
+	"strings"
 	"sync/atomic"
 	"time"
 
@@ -13,6 +14,8 @@ var (
 	Timeout   = 30 * time.Second
 )
 
+// NewJSON creates a websocket which as soon as it connects sends the initMsg U.
+// Then every message received is deserialized into T and handler is called.
 func NewJSON[T any, U any](endpoint string, initMsg U, handler func(T), errHandler func(err error)) (done, stop chan struct{}, err error) {
 	initMsgBytes, err := json.Marshal(initMsg)
 	if err != nil {
@@ -21,19 +24,32 @@ func NewJSON[T any, U any](endpoint string, initMsg U, handler func(T), errHandl
 
 	done, stop, err = NewRawBytes(
 		endpoint, initMsgBytes,
-		func(rawMsg []byte) {
-			x := new(T)
-			err := json.Unmarshal(rawMsg, x)
-			if err != nil {
-				errHandler(err) // TODO(mercilex): assert it does not dead-lock
-				return
-			}
-			handler(*x)
-		},
+		genericJSONHandler(handler),
 		errHandler,
 	)
 
 	return
+}
+
+// genericJSONHandler creates a function which handles JSON websocket messages
+// in case there's a failure in JSON decoding, if the error is an unexpected end
+// of the JSON input then no error is emitted since it would mean that
+// the connection has been interrupted and the message was received partially.
+// This means the websocket error handler would quit immediately afterwards.
+// Even in reconnection scenarios the msg would still be lost.
+func genericJSONHandler[T any](handler func(T)) func([]byte) {
+	return func(rawMsg []byte) {
+		x := new(T)
+		err := json.Unmarshal(rawMsg, x)
+		if err != nil {
+			if strings.Contains(err.Error(), "unexpected end of JSON input") {
+				return
+			} else {
+				panic(err)
+			}
+		}
+		handler(*x)
+	}
 }
 
 func NewRawBytes(endpoint string, initMsg []byte, handler func(msg []byte), errHandler func(err error)) (done, stop chan struct{}, err error) {
