dcc6d9d45e81936f15b83da91ffa58799eee3b51
godismercilex <mercilex@matrixsystems.co>
2022-09-13 14:10:46 +0200

    change: remove unused key types

--- collections/keys/keys.go
+++ collections/keys/keys.go
@@ -35,24 +35,6 @@ type Key interface {
 	fmt.Stringer
 }
 
-type Uint8Key uint8
-
-func (u Uint8Key) KeyBytes() []byte {
-	return []byte{uint8(u)}
-}
-
-func (u Uint8Key) FromKeyBytes(b []byte) (i int, k Key) {
-	return 0, Uint8Key(b[0])
-}
-
-func (u Uint8Key) String() string { return fmt.Sprintf("%d", u) }
-
-type Uint32 uint32
-
-type Uint64 uint64
-
-type Int64 int64
-
 func validString[T ~string](s T) error {
 	for i, c := range s {
 		if c == 0 {
