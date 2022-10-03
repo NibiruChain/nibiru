96acf9c92efa2eca578e7ae859327673c6658b2f
Jonathan Gimeno <jgimeno@gmail.com>
2022-10-03 11:31:40 +0200

    lint

--- x/vpool/types/pool_test.go
+++ x/vpool/types/pool_test.go
@@ -1,9 +1,10 @@
 package types
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
