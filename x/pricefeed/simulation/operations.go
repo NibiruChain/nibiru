234635550972a9d3c36643d002d44b652f2c64af
NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-10-11 16:13:22 -0700

    test: add post price simulation

--- x/pricefeed/simulation/operations.go
+++ x/pricefeed/simulation/operations.go
@@ -2,18 +2,24 @@ package simulation
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 
+	"github.com/NibiruChain/nibiru/x/common"
 	"github.com/NibiruChain/nibiru/x/pricefeed/keeper"
 	"github.com/NibiruChain/nibiru/x/pricefeed/types"
 )
 
 // WeightedOperations returns all the operations from the module with their respective weights
-func WeightedOperations(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper) simulation.WeightedOperations {
+func WeightedOperations(
+	ak types.AccountKeeper,
+	bk types.BankKeeper, k keeper.Keeper,
+) simulation.WeightedOperations {
 	return simulation.WeightedOperations{
 		simulation.NewWeightedOperation(
 			100,
@@ -29,13 +35,28 @@ func SimulateMsgPostPrice(
 ) simtypes.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
-		simAccount, _ := simtypes.RandomAcc(r, accs)
+		sender, _ := simtypes.RandomAcc(r, accs)
 		msg := &types.MsgPostPrice{
-			Oracle: simAccount.Address.String(),
+			Oracle: sender.Address.String(),
+			Token0: common.DenomBTC,
+			Token1: common.DenomNUSD,
+			Price:  simtypes.RandomDecAmount(r, sdk.NewDec(100_000)).Add(sdk.OneDec()),
+			Expiry: ctx.BlockTime().Add(time.Hour),
 		}
 
-		// TODO: Handling the PostPrice simulation
-
-		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "PostPrice simulation not implemented"), nil, nil
+		return simulation.GenAndDeliverTxWithRandFees(simulation.OperationInput{
+			R:               r,
+			App:             app,
+			TxGen:           simapp.MakeTestEncodingConfig().TxConfig,
+			Cdc:             nil,
+			Msg:             msg,
+			MsgType:         msg.Type(),
+			Context:         ctx,
+			SimAccount:      sender,
+			AccountKeeper:   ak,
+			Bankkeeper:      bk,
+			ModuleName:      types.ModuleName,
+			CoinsSpentInMsg: sdk.NewCoins(),
+		})
 	}
 }
