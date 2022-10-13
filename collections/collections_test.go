4cd4f90406ec25a1d571311354829b9d86adebea
Jonathan Gimeno <jgimeno@gmail.com>
2022-10-13 15:45:16 +0200

    move some test types to utils

--- collections/collections_test.go
+++ collections/collections_test.go
@@ -1,9 +1,6 @@
 package collections
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
@@ -26,33 +23,4 @@ func deps() (sdk.StoreKey, sdk.Context, codec.BinaryCodec) {
 			WithMultiStore(ms).
 			WithGasMeter(sdk.NewGasMeter(1_000_000_000)),
 		codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-}
-
-// stringValue is a ValueEncoder for string, used for testing.
-type stringValue struct{}
-
-func (s stringValue) ValueEncode(value string) []byte { return []byte(value) }
-func (s stringValue) ValueDecode(b []byte) string     { return string(b) }
-func (s stringValue) Stringify(value string) string   { return value }
-func (s stringValue) Name() string                    { return "test string" }
-
-// jsonValue is a ValueEncoder for objects to be turned into json.
-// used for testing.
-type jsonValue[T any] struct{}
-
-func (jsonValue[T]) ValueEncode(value T) []byte {
-	b, _ := json.Marshal(value)
-	return b
-}
-
-func (jsonValue[T]) ValueDecode(b []byte) T {
-	v := new(T)
-	_ = json.Unmarshal(b, v)
-	return *v
-}
-
-func (jsonValue[T]) Stringify(v T) string { return fmt.Sprintf("%#v", v) }
-func (jsonValue[T]) Name() string {
-	var t T
-	return fmt.Sprintf("json-value-%T", t)
 }
