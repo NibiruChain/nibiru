5203fdee08bbe4d281086c99883b0c5859d6bb60
Matthias <97468149+matthiasmatt@users.noreply.github.com>
2022-12-06 02:22:53 -0300

    feat: whitelist ubtc and ueth in pricefeed (#1108)
    * feat: whitelist ubtc and ueth in pricefeed
    
    * feat: add uusdc to the lcoalnet
    
    * fix(localnet.sh):
    1. Move pair initialization calls of 'nibid add_genesis_param' before
    calls of 'add-genesis-oracle'.
    2. Use USDT instead of USDC to match devnet and testnet
    
    Co-authored-by: Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -9,25 +9,26 @@ console_log_text_color() {
   reset=$(tput sgr0)
 }
 
-if [ console_log_text_color ]; then echo "succesfully toggled console coloring"
+if [ console_log_text_color ]; then
+  echo "succesfully toggled console coloring"
 else
   # For Ubuntu and Debian. MacOS has tput by default.
   apt-get install libncurses5-dbg -y
 fi
 
-echo_info () {
+echo_info() {
   echo "${blue}"
   echo "$1"
   echo "${reset}"
 }
 
-echo_error () {
+echo_error() {
   echo "${red}"
   echo "$1"
   echo "${reset}"
 }
 
-echo_success () {
+echo_success() {
   echo "${green}"
   echo "$1"
   echo "${reset}"
@@ -47,8 +48,10 @@ CHAIN_ID="nibiru-localnet-0"
 RPC_PORT="26657"
 GRPC_PORT="9090"
 MNEMONIC="guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host"
-GENESIS_COINS="1000000000unibi,10000000000000unusd"
+GENESIS_COINS="10000000000000unibi,10000000000000unusd,10000000000000uusdt"
 CHAIN_DIR="$HOME/.nibid"
+echo "CHAIN_DIR: $CHAIN_DIR"
+echo "CHAIN_ID: $CHAIN_ID"
 
 SEDOPTION=""
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -56,9 +59,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # Stop nibid if it is already running
-if pgrep -x "$BINARY" > /dev/null; then
-    echo_error "Terminating $BINARY..."
-    killall nibid
+if pgrep -x "$BINARY" >/dev/null; then
+  echo_error "Terminating $BINARY..."
+  killall nibid
 fi
 
 # Remove previous data
@@ -79,7 +82,6 @@ else
   echo_error "Failed to initialize $CHAIN_ID"
 fi
 
-
 # Configure keyring-backend to "test"
 echo_info "Configuring keyring-backend..."
 if $BINARY config keyring-backend test; then
@@ -87,7 +89,6 @@ if $BINARY config keyring-backend test; then
 else
   echo_error "Failed to configure keyring-backend"
 fi
-
 
 # Configure chain-id
 echo_info "Configuring chain-id..."
@@ -170,7 +171,7 @@ fi
 add_genesis_param() {
   echo "jq input $1"
   # copy param ($1) to tmp_genesis.json
-  cat $CHAIN_DIR/config/genesis.json | jq "$1" > $CHAIN_DIR/config/tmp_genesis.json
+  cat $CHAIN_DIR/config/genesis.json | jq "$1" >$CHAIN_DIR/config/tmp_genesis.json
   # rewrite genesis.json with the contents of tmp_genesis.json
   mv $CHAIN_DIR/config/tmp_genesis.json $CHAIN_DIR/config/genesis.json
 }
@@ -182,13 +183,13 @@ add_genesis_vpools_with_coingecko_prices() {
   curl -X 'GET' \
     'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd' \
     -H 'accept: application/json' \
-    > $temp_json_fname
+    >$temp_json_fname
 
   local M=1000000
 
   local num_users=24000
-  local faucet_nusd_amt=100 
-  local quote_amt=$(($num_users * $faucet_nusd_amt * $M)) 
+  local faucet_nusd_amt=100
+  local quote_amt=$(($num_users * $faucet_nusd_amt * $M))
 
   price_btc=$(cat tmp_vpool_prices.json | jq -r '.bitcoin.usd')
   price_btc=${price_btc%.*}
@@ -201,7 +202,7 @@ add_genesis_vpools_with_coingecko_prices() {
   nibid add-genesis-vpool ubtc:unusd $base_amt_btc $quote_amt 0.1 0.1 0.1 0.0625 12
   nibid add-genesis-vpool ueth:unusd $base_amt_eth $quote_amt 0.1 0.1 0.1 0.04 20
 
-  echo 'tmp_vpool_prices: ' 
+  echo 'tmp_vpool_prices: '
   cat $temp_json_fname | jq .
   rm -f $temp_json_fname
 }
@@ -213,16 +214,16 @@ add_genesis_vpools_default() {
   local quote_amt=10$KILO$MEGA
   local base_amt_btc=$(($quote_amt / 16500))
   local base_amt_eth=$(($quote_amt / 1200))
-  nibid add-genesis-vpool ubtc:unusd $base_amt_btc $quote_amt  0.1 0.1 0.1 0.0625 12
+  nibid add-genesis-vpool ubtc:unusd $base_amt_btc $quote_amt 0.1 0.1 0.1 0.0625 12
   nibid add-genesis-vpool ueth:unusd $base_amt_eth $quote_amt 0.1 0.1 0.1 0.0625 10
 }
 
 # x/vpool
-if add_genesis_vpools_with_coingecko_prices; then 
+if add_genesis_vpools_with_coingecko_prices; then
   echo_success "set vpools with coingecko prices"
-elif add_genesis_vpools_default; then 
-  echo_success "set vpools with defaults" 
-else 
+elif add_genesis_vpools_default; then
+  echo_success "set vpools with defaults"
+else
   echo_error "failed to set genesis vpools"
 fi
 
@@ -240,9 +241,13 @@ add_genesis_param '.app_state.perp.pair_metadata[1].pair = {token0:"ueth",token1
 add_genesis_param '.app_state.perp.pair_metadata[1].latest_cumulative_premium_fraction = "0"'
 
 # x/pricefeed
+add_genesis_param '.app_state.pricefeed.params.pairs[0] = {token0:"ubtc",token1:"unusd"}'
+add_genesis_param '.app_state.pricefeed.params.pairs[0] = {token0:"ueth",token1:"unusd"}'
+add_genesis_param '.app_state.pricefeed.params.pairs[0] = {token0:"ueth",token1:"unusd"}'
+add_genesis_param '.app_state.pricefeed.params.twap_lookback_window = "900s"'
 nibid add-genesis-oracle nibi1zaavvzxez0elundtn32qnk9lkm8kmcsz44g7xl
-
-cat $HOME/.nibid/config/genesis.json | jq '.app_state.pricefeed.params.twap_lookback_window = "900s"' > $HOME/.nibid/config/tmp_genesis.json && mv $HOME/.nibid/config/tmp_genesis.json $HOME/.nibid/config/genesis.json
+nibid add-genesis-oracle nibi1lptpc0e3upq2n7e4zmqtr0yxyrldp6jzz8z8xm
+nibid add-genesis-oracle nibi15cdcxznuwpuk5hw7t678wpyesy78kwy00qcesa
 
 # Start the network
 echo_info "Starting $CHAIN_ID in $CHAIN_DIR..."
