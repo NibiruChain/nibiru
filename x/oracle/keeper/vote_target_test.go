5c864566c3f8f275e5ab3a300b645ec57fbf0abb
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-01-12 09:44:22 -0800

    refactor(oracle): rename functions and variables for readability (#1141)
    * refactor(oracle): rename variables for readability
    
    * refactor(oracle): rename pairs to whitelistedpairs
    
    * chore: update changelog
    
    * chore: pull changes from #1129

--- x/oracle/keeper/vote_target_test.go
+++ x/oracle/keeper/vote_target_test.go
@@ -31,13 +31,13 @@ func TestKeeper_GetVoteTargets(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			input := CreateTestInput(t)
 
-			for _, p := range input.OracleKeeper.Pairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
-				input.OracleKeeper.Pairs.Delete(input.Ctx, p)
+			for _, p := range input.OracleKeeper.WhitelistedPairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
+				input.OracleKeeper.WhitelistedPairs.Delete(input.Ctx, p)
 			}
 
 			expectedTargets := tc.in
 			for _, target := range expectedTargets {
-				input.OracleKeeper.Pairs.Insert(input.Ctx, target)
+				input.OracleKeeper.WhitelistedPairs.Insert(input.Ctx, target)
 			}
 
 			var panicAssertFn func(t assert.TestingT, f assert.PanicTestFunc, msgAndArgs ...interface{}) bool
@@ -56,13 +56,13 @@ func TestKeeper_GetVoteTargets(t *testing.T) {
 
 	input := CreateTestInput(t)
 
-	for _, p := range input.OracleKeeper.Pairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
-		input.OracleKeeper.Pairs.Delete(input.Ctx, p)
+	for _, p := range input.OracleKeeper.WhitelistedPairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
+		input.OracleKeeper.WhitelistedPairs.Delete(input.Ctx, p)
 	}
 
 	expectedTargets := []string{"bar", "foo", "whoowhoo"}
 	for _, target := range expectedTargets {
-		input.OracleKeeper.Pairs.Insert(input.Ctx, target)
+		input.OracleKeeper.WhitelistedPairs.Insert(input.Ctx, target)
 	}
 
 	targets := input.OracleKeeper.GetWhitelistedPairs(input.Ctx)
@@ -72,13 +72,13 @@ func TestKeeper_GetVoteTargets(t *testing.T) {
 func TestKeeper_IsVoteTarget(t *testing.T) {
 	input := CreateTestInput(t)
 
-	for _, p := range input.OracleKeeper.Pairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
-		input.OracleKeeper.Pairs.Delete(input.Ctx, p)
+	for _, p := range input.OracleKeeper.WhitelistedPairs.Iterate(input.Ctx, collections.Range[string]{}).Keys() {
+		input.OracleKeeper.WhitelistedPairs.Delete(input.Ctx, p)
 	}
 
 	validTargets := []string{"bar", "foo", "whoowhoo"}
 	for _, target := range validTargets {
-		input.OracleKeeper.Pairs.Insert(input.Ctx, target)
+		input.OracleKeeper.WhitelistedPairs.Insert(input.Ctx, target)
 		require.True(t, input.OracleKeeper.IsWhitelistedPair(input.Ctx, target))
 	}
 }
