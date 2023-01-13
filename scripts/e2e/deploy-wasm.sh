06471d2761ef82bde81df754b16473d518a1c34e
Jonathan Gimeno <jgimeno@gmail.com>
2023-01-13 10:23:42 +0100

    remove address

--- scripts/e2e/deploy-wasm.sh
+++ scripts/e2e/deploy-wasm.sh
@@ -2,7 +2,6 @@ BINARY="./nibid"
 DENOM="unibi"
 CHAIN_ID="nibiru-localnet-0"
 TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID"
-DEFAULT_DEV_ADDRESS="juno16g2rahf5846rxzp3fwlswy08fz8ccuwk03k57y"
 
 # validator addr
 VALIDATOR_ADDR=$($BINARY keys show validator --address)
@@ -12,10 +11,6 @@ echo "$VALIDATOR_ADDR"
 BALANCE_1=$($BINARY q bank balances $VALIDATOR_ADDR)
 echo "Pre-store balance:"
 echo "$BALANCE_1"
-
-# you ideally want to run locally, get a user and then
-# pass that addr in here
-echo "Address to deploy contracts: $DEFAULT_DEV_ADDRESS"
 echo "TX Flags: $TXFLAG"
 
 CONTRACT_CODE=$($BINARY tx wasm store "./scripts/e2e/contracts/cw_nameservice.wasm" --from validator $TXFLAG --output json | jq -r '.logs[0].events[-1].attributes[-1].value')
