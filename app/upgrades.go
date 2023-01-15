f37d30a1e1cd26b95a9edf9a3e03851ede1ec86b
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-01-15 10:00:33 -0600

    test: only add oracle store upgrade

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,9 +4,12 @@ import (
 	"log"
 	"sort"
 
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	oracletypes "github.com/NibiruChain/nibiru/x/oracle/types"
 )
 
 // NOTE: When performing upgrades, make sure to keep / register the handlers
@@ -27,5 +30,19 @@ func (app NibiruApp) RegisterUpgradeHandlers() {
 		app.upgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		})
+	}
+
+	upgradeInfo, err := app.upgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == "v0.17.0" && !app.upgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{oracletypes.StoreKey},
+		}
+
+		// configure store loader that checks if version == upgradeHeight and applies store upgrades
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
 }
