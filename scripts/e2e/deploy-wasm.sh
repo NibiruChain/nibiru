8de90c16b3b9d68fb892791913d036c9fa7a3f27
Jonathan Gimeno <jgimeno@gmail.com>
2023-01-15 05:00:03 +0100

    ci: Create e2e wasm unit test (#1107)
    * update changelog
    
    * update changelog
    
    * build flag for linux
    
    * build flag for linux
    
    * temp commit
    
    * update main
    
    * add dist
    
    * add cgo enabled
    
    * Update go release.
    
    * add flags
    
    * other test
    
    * add rm dist flag
    
    * add flag amd64
    
    * add go releaser action
    
    * add flags
    
    * remove ledger flag
    
    * add tm flag
    
    * add lib
    
    * try sudo
    
    * add link statically
    
    * change back ldflags
    
    * add muslc as build tag
    
    * some test
    
    * remove file
    
    * last try build flags
    
    * add ldflag linkmode
    
    * add another ldflags
    
    * add colon
    
    * Comment all related to muslc
    
    * add external ld flags
    
    * add netgo
    
    * add install musl dev
    
    * add lm linker flag
    
    * add math lib
    
    * update changelog
    
    * add lost wasm line
    
    * move entry to breaking changes
    
    * fix: localnet script
    
    * create e2e wasm
    
    * add tag for version
    
    * use self repository
    
    * ls release folder
    
    * echo release folder
    
    * echo outputs
    
    * echo outputs
    
    * download release
    
    * try release
    
    * try release output
    
    * try var
    
    * use wget
    
    * use another action
    
    * fix action
    
    * use filter
    
    * unpack release
    
    * change name
    
    * show release
    
    * add nibid version
    
    * execute nibid
    
    * add nibid
    
    * run network
    
    * run network
    
    * add runs on
    
    * insert code
    
    * ls test
    
    * launch localnet
    
    * remove build localnet
    
    * refactor binary
    
    * try query
    
    * add deploy wasm test
    
    * add validator address
    
    * temp commit
    
    * deploy wasm
    
    * add folder
    
    * add chain id
    
    * add node
    
    * add nameservice code
    
    * add register service
    
    * remove nibid
    
    * add end to end teset
    
    * add changelog
    
    * add header
    
    Co-authored-by: NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>

---
+++ scripts/e2e/deploy-wasm.sh
@@ -0,0 +1,42 @@
+BINARY="./nibid"
+DENOM="unibi"
+CHAIN_ID="nibiru-localnet-0"
+TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID"
+
+# validator addr
+VALIDATOR_ADDR=$($BINARY keys show validator --address)
+echo "Validator address:"
+echo "$VALIDATOR_ADDR"
+
+BALANCE_1=$($BINARY q bank balances $VALIDATOR_ADDR)
+echo "Pre-store balance:"
+echo "$BALANCE_1"
+echo "TX Flags: $TXFLAG"
+
+CONTRACT_CODE=$($BINARY tx wasm store "./scripts/e2e/contracts/cw_nameservice.wasm" --from validator $TXFLAG --output json | jq -r '.logs[0].events[-1].attributes[-1].value')
+echo "Stored: $CONTRACT_CODE"
+
+BALANCE_2=$($BINARY q bank balances $VALIDATOR_ADDR)
+echo "Post-store balance:"
+echo "$BALANCE_2"
+
+INIT='{"purchase_price":{"amount":"100","denom":"unibi"},"transfer_price":{"amount":"999","denom":"unibi"}}'
+$BINARY tx wasm instantiate $CONTRACT_CODE "$INIT" --from validator $TXFLAG --label "awesome name service" --no-admin
+
+CONTRACT_ADDRESS=$($BINARY query wasm list-contract-by-code $CONTRACT_CODE --output json | jq -r '.contracts[-1]')
+echo "Contract Address: $CONTRACT_ADDRESS"
+
+$BINARY query wasm contract $CONTRACT_ADDRESS
+
+# purchase a domain name
+$BINARY tx wasm execute $CONTRACT_ADDRESS '{"register":{"name":"uniques-domain"}}' --amount 100$DENOM --from validator $TXFLAG -y
+
+# query registered name
+NAME_QUERY='{"resolve_record": {"name": "uniques-domain"}}'
+DOMAIN_OWNER=$($BINARY query wasm contract-state smart $CONTRACT_ADDRESS "$NAME_QUERY" --output json | jq -r '.data.address')
+echo "Owner: $DOMAIN_OWNER"
+
+if [ $DOMAIN_OWNER != $VALIDATOR_ADDR ]; then
+  echo "Domain owner is not the validator address"
+  exit 1
+fi
