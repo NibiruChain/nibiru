1f849b02b10290dbf0d7751ce68ade9f3334a696
Unique-Divine <realuniquedivine@gmail.com>
2023-02-02 15:23:08 -0600

    fix: uint256 breaks compatibility between v1.1.1 and v1.2.1

--- x/dex/handler.go
+++ x/dex/handler.go
@@ -16,19 +16,20 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		goCtx := sdk.WrapSDKContext(ctx)
 
 		switch msg := msg.(type) {
 		case *types.MsgCreatePool:
-			res, err := msgServer.CreatePool(sdk.WrapSDKContext(ctx), msg)
+			res, err := msgServer.CreatePool(goCtx, msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgJoinPool:
-			res, err := msgServer.JoinPool(sdk.WrapSDKContext(ctx), msg)
+			res, err := msgServer.JoinPool(goCtx, msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgExitPool:
-			res, err := msgServer.ExitPool(sdk.WrapSDKContext(ctx), msg)
+			res, err := msgServer.ExitPool(goCtx, msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgSwapAssets:
-			res, err := msgServer.SwapAssets(sdk.WrapSDKContext(ctx), msg)
+			res, err := msgServer.SwapAssets(goCtx, msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
