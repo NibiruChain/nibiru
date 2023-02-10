6360d112b2d68e2ae5e751ac85bf25421b1ba057
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-02-09 21:07:41 -0500

    ci(codecov): add Codecov reporting (#1189)
    * ci(codecov): add Codecov reporting
    
    * chore: update changelog
    
    * retrigger checks
    
    ---------
    
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>

--- contrib/make/test.mk
+++ contrib/make/test.mk
@@ -2,15 +2,20 @@
 ###                            		  Tests 					                        ###
 ###############################################################################
 
-PACKAGES_NOSIMULATION = $(shell go list ./... | grep -v '/simapp')
+PACKAGES_NOSIMULATION = ${shell go list ./... | grep -v simapp}
 
 .PHONY: test-unit
 test-unit:
-	@go test $(PACKAGES_NOSIMULATION) -short -cover
+	go test -v $(PACKAGES_NOSIMULATION) -short -coverprofile=coverage.txt -covermode=count
 
 .PHONY: test-integration
 test-integration:
-	@go test -v $(PACKAGES_NOSIMULATION) -cover
+	go test -v $(PACKAGES_NOSIMULATION) -coverprofile=coverage.txt -covermode=count
+
+# Used for CI by Codecov
+.PHONY: test-coverage
+test-coverage:
+	go test ./... -coverprofile=coverage.txt -covermode=atomic -race
 
 # Require Python3
 .PHONY: test-create-test-cases
