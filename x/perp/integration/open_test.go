f77c7ab4a2b1dce5ed47cec40dafdd86c661a71a
Jonathan Gimeno <jgimeno@gmail.com>
2023-02-23 10:10:52 +0100

    temp commit

---
+++ x/perp/integration/open_test.go
@@ -0,0 +1,12 @@
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/NibiruChain/nibiru/x/common/testutil/testapp"
+)
+
+func TestHappyPath(t *testing.T) {
+	nibiruApp, ctx := testapp.NewNibiruTestAppAndContext(true)
+
+}
