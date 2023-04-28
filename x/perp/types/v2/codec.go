2da204aab2476cfc8d7e53343732afeb6c023b29
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-04-27 23:04:56 -0500

    refactor(perp)!: perp data model updates (#1296)
    * feat: add v2 perp types
    
    * feat: add perp v2 query types
    
    * feat: add perp v2 tx
    
    * feat: add perp v2 genesis state
    
    * feat: add perp v2 event types
    
    * feat: add perp v2 gov protos
    
    * fix: versioned http urls
    
    * chore: update changelog
    
    * feat: validation functions for AMM and Market
    
    * refactor: add prepaid bad debt to Market proto
    
    * fix: last updated block number as uint64
    
    * fix: last updated block number as int64
    
    * fix: remove prepaid bad debt from genesis state
    
    * refactor: remove PositionResp and LiquidateResp from protos
    
    * feat: add codec and msg interfaces
    
    * feat: add ReserveSnapshot
    
    * feat: add reserve snapshot timestamp
    
    * fix: perp v2 swap event
    
    * fix: non-nullable prepaid bad debt
    
    * refactor: remove mark price from position changed event
    
    * chore: update changelog

---
+++ x/perp/types/v2/codec.go
@@ -0,0 +1,36 @@
+package v2
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgAddMargin{}, "perp/add_margin", nil)
+	cdc.RegisterConcrete(&MsgRemoveMargin{}, "perp/remove_margin", nil)
+	cdc.RegisterConcrete(&MsgOpenPosition{}, "perp/open_position", nil)
+	cdc.RegisterConcrete(&MsgClosePosition{}, "perp/close_position", nil)
+	cdc.RegisterConcrete(&MsgDonateToEcosystemFund{}, "perp/donate_to_ef", nil)
+	cdc.RegisterConcrete(&MsgMultiLiquidate{}, "perp/multi_liquidate", nil)
+}
+
+func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		/* interface */ (*sdk.Msg)(nil),
+		/* implementations */
+		&MsgRemoveMargin{},
+		&MsgAddMargin{},
+		&MsgOpenPosition{},
+		&MsgClosePosition{},
+		&MsgMultiLiquidate{},
+	)
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+var (
+	Amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+)
