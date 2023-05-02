3c7c9916a8ece23ce2be4deb6f2bdd7cc15c505a
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-01 19:13:36 -0500

    feat(perp): complete perp v2 types

--- x/perp/types/v2/amm.go
+++ x/perp/types/v2/amm.go
@@ -3,6 +3,7 @@ package v2
 import (
 	fmt "fmt"
 
+	"github.com/NibiruChain/nibiru/x/common"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -24,4 +25,227 @@ func (amm *AMM) Validate() error {
 	}
 
 	return nil
+}
+
+func (amm *AMM) SwapQuoteAsset(quoteAssetAmt sdk.Dec, dir Direction) (updatedAMM *AMM, baseAssetDelta sdk.Dec, err error) {
+	// check trade limit ratio on quote in either direction
+	quoteReserveAmt := amm.FromQuoteAssetToReserve(quoteAssetAmt)
+	baseReserveDelta, err := amm.GetBaseReserveAmt(quoteReserveAmt, dir)
+	if err != nil {
+		return nil, sdk.Dec{}, err
+	}
+
+	if dir == Direction_LONG {
+		amm.QuoteReserve = amm.QuoteReserve.Add(quoteReserveAmt)
+		amm.BaseReserve = amm.BaseReserve.Sub(baseReserveDelta)
+		amm.Bias = amm.Bias.Add(baseReserveDelta)
+	} else if dir == Direction_SHORT {
+		amm.QuoteReserve = amm.QuoteReserve.Sub(quoteReserveAmt)
+		amm.BaseReserve = amm.BaseReserve.Add(baseReserveDelta)
+		amm.Bias = amm.Bias.Sub(baseReserveDelta)
+	}
+
+	return amm, baseReserveDelta, nil
+}
+
+func (amm *AMM) SwapBaseAsset(baseAssetAmt sdk.Dec, dir Direction) (updatedAMM *AMM, quoteAssetDelta sdk.Dec, err error) {
+	// check trade limit ratio on quote in either direction
+	quoteReserveDelta, err := amm.GetQuoteReserveAmt(baseAssetAmt, dir)
+	if err != nil {
+		return nil, sdk.Dec{}, err
+	}
+
+	if dir == Direction_LONG {
+		amm.QuoteReserve = amm.QuoteReserve.Add(quoteReserveDelta)
+		amm.BaseReserve = amm.BaseReserve.Sub(baseAssetAmt)
+		amm.Bias = amm.Bias.Add(baseAssetAmt)
+	} else if dir == Direction_SHORT {
+		amm.QuoteReserve = amm.QuoteReserve.Sub(quoteReserveDelta)
+		amm.BaseReserve = amm.BaseReserve.Add(baseAssetAmt)
+		amm.Bias = amm.Bias.Sub(baseAssetAmt)
+	}
+
+	return amm, amm.FromQuoteReserveToAsset(quoteReserveDelta), nil
+}
+
+/*
+FromQuoteAssetToReserve returns the amount of quote reserve equivalent to the
+amount of quote asset given
+*/
+func (amm *AMM) FromQuoteAssetToReserve(quoteAsset sdk.Dec) sdk.Dec {
+	return quoteAsset.Quo(amm.PriceMultiplier)
+}
+
+/*
+FromQuoteReserveToAsset returns the amount of quote asset equivalent to the
+amount of quote reserve given
+*/
+func (amm *AMM) FromQuoteReserveToAsset(quoteReserve sdk.Dec) sdk.Dec {
+	return quoteReserve.Mul(amm.PriceMultiplier)
+}
+
+func (amm *AMM) GetBaseReserveAmt(
+	quoteReserveAmt sdk.Dec,
+	dir Direction,
+) (baseReserveDelta sdk.Dec, err error) {
+	if quoteReserveAmt.LTE(sdk.ZeroDec()) {
+		return sdk.ZeroDec(), nil
+	}
+
+	invariant := amm.QuoteReserve.Mul(amm.BaseReserve) // x * y = k
+
+	var quoteReservesAfter sdk.Dec
+	if dir == Direction_LONG {
+		quoteReservesAfter = amm.QuoteReserve.Add(quoteReserveAmt)
+	} else {
+		quoteReservesAfter = amm.QuoteReserve.Sub(quoteReserveAmt)
+	}
+
+	if quoteReservesAfter.LTE(sdk.ZeroDec()) {
+		return sdk.Dec{}, ErrQuoteReserveAtZero
+	}
+
+	baseReservesAfter := invariant.Quo(quoteReservesAfter)
+	baseReserveDelta = baseReservesAfter.Sub(amm.BaseReserve).Abs()
+
+	return baseReserveDelta, nil
+}
+
+func (amm *AMM) GetQuoteReserveAmt(
+	baseReserveAmt sdk.Dec,
+	dir Direction,
+) (quoteReserveDeltaAbs sdk.Dec, err error) {
+	if baseReserveAmt.LTE(sdk.ZeroDec()) {
+		return sdk.ZeroDec(), nil
+	}
+
+	invariant := amm.QuoteReserve.Mul(amm.BaseReserve) // x * y = k
+
+	var baseReservesAfter sdk.Dec
+	if dir == Direction_LONG {
+		baseReservesAfter = amm.BaseReserve.Sub(baseReserveAmt)
+	} else {
+		baseReservesAfter = amm.BaseReserve.Add(baseReserveAmt)
+	}
+
+	if baseReservesAfter.LTE(sdk.ZeroDec()) {
+		return sdk.Dec{}, ErrBaseReserveAtZero.Wrapf(
+			"base assets below zero after trying to swap %s base assets",
+			baseReserveAmt.String(),
+		)
+	}
+
+	quoteReservesAfter := invariant.Quo(baseReservesAfter)
+	quoteReserveDeltaAbs = quoteReservesAfter.Sub(amm.QuoteReserve).Abs()
+
+	return quoteReserveDeltaAbs, nil
+}
+
+/*
+SwapBaseForQuote returns the amount of quote reserves you will get out
+by giving a specified amount of base reserve
+
+args:
+  - baseReserveAmt: the amount of base reserve to add to/remove from the pool
+  - dir: LONG or SHORT the base asset from the user's perspective
+
+ret:
+  - quoteOutAbs: the amount of quote assets required from the user's perspective
+  - err: error
+*/
+func (amm *AMM) SwapBaseForQuote(
+	baseReserveAmt sdk.Dec,
+	dir Direction,
+) (quoteReserveAmt sdk.Dec, err error) {
+	if baseReserveAmt.LTE(sdk.ZeroDec()) {
+		return sdk.ZeroDec(), nil
+	}
+
+	invariant := amm.QuoteReserve.Mul(amm.BaseReserve) // x * y = k
+
+	var baseReservesAfter sdk.Dec
+	if dir == Direction_LONG {
+		baseReservesAfter = amm.BaseReserve.Sub(baseReserveAmt)
+	} else {
+		baseReservesAfter = amm.BaseReserve.Add(baseReserveAmt)
+	}
+
+	if baseReservesAfter.LTE(sdk.ZeroDec()) {
+		return sdk.Dec{}, ErrBaseReserveAtZero.Wrapf(
+			"base assets below zero after trying to swap %s base assets",
+			baseReserveAmt.String(),
+		)
+	}
+
+	quoteReservesAfter := invariant.Quo(baseReservesAfter)
+	quoteReserveAmt = quoteReservesAfter.Sub(amm.QuoteReserve).Neg() // from the user's perspective
+
+	return quoteReserveAmt, nil
+}
+
+// Returns the instantaneous mark price of the trading pair
+func (amm AMM) MarkPrice() sdk.Dec {
+	if amm.BaseReserve.IsNil() || amm.BaseReserve.IsZero() ||
+		amm.QuoteReserve.IsNil() || amm.QuoteReserve.IsZero() {
+		return sdk.ZeroDec()
+	}
+
+	return amm.QuoteReserve.Quo(amm.BaseReserve).Mul(amm.PriceMultiplier)
+}
+
+func (amm *AMM) ComputeSqrtDepth() (sqrtDepth sdk.Dec, err error) {
+	liqDepth := amm.QuoteReserve.Mul(amm.BaseReserve)
+	return common.SqrtDec(liqDepth)
+}
+
+// ValidateReserves checks that reserves are positive.
+func (amm *AMM) ValidateReserves() error {
+	if !amm.QuoteReserve.IsPositive() || !amm.BaseReserve.IsPositive() {
+		return ErrNonPositiveReserves.Wrap("pool: " + amm.String())
+	} else {
+		return nil
+	}
+}
+
+// ValidateLiquidityDepth checks that reserves are positive.
+func (amm *AMM) ValidateLiquidityDepth() error {
+	computedSqrtDepth, err := amm.ComputeSqrtDepth()
+	if err != nil {
+		return err
+	}
+
+	if !amm.SqrtDepth.IsPositive() {
+		return ErrLiquidityDepth.Wrap(
+			"liq depth must be positive. pool: " + amm.String())
+	} else if !amm.SqrtDepth.Sub(computedSqrtDepth).Abs().LTE(sdk.NewDec(1)) {
+		return ErrLiquidityDepth.Wrap(
+			"computed sqrt and current sqrt are mismatched. pool: " + amm.String())
+	} else {
+		return nil
+	}
+}
+
+func (amm *AMM) WithBaseReserve(baseReserve sdk.Dec) *AMM {
+	amm.BaseReserve = baseReserve
+	return amm
+}
+
+func (amm *AMM) WithQuoteReserve(quoteReserve sdk.Dec) *AMM {
+	amm.QuoteReserve = quoteReserve
+	return amm
+}
+
+func (amm *AMM) WithPriceMultiplier(priceMultiplier sdk.Dec) *AMM {
+	amm.PriceMultiplier = priceMultiplier
+	return amm
+}
+
+func (amm *AMM) WithBias(bias sdk.Dec) *AMM {
+	amm.Bias = bias
+	return amm
+}
+
+func (amm *AMM) WithSqrtDepth(sqrtDepth sdk.Dec) *AMM {
+	amm.SqrtDepth = sqrtDepth
+	return amm
 }
--- x/perp/types/v2/market.go
+++ x/perp/types/v2/market.go
@@ -3,6 +3,7 @@ package v2
 import (
 	fmt "fmt"
 
+	"github.com/NibiruChain/nibiru/x/common/asset"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -41,6 +42,116 @@ func (market *Market) Validate() error {
 
 	if sdk.OneDec().Quo(market.MaxLeverage).LT(market.MaintenanceMarginRatio) {
 		return fmt.Errorf("margin ratio opened with max leverage position will be lower than Maintenance margin ratio")
+	}
+
+	return nil
+}
+
+func (market *Market) WithPriceFluctuationLimitRatio(value sdk.Dec) *Market {
+	market.PriceFluctuationLimitRatio = value
+	return market
+}
+
+func (market *Market) WithMaintenanceMarginRatio(value sdk.Dec) *Market {
+	market.MaintenanceMarginRatio = value
+	return market
+}
+
+func (market *Market) WithMaxLeverage(value sdk.Dec) *Market {
+	market.MaxLeverage = value
+	return market
+}
+
+func (market *Market) WithEcosystemFee(value sdk.Dec) *Market {
+	market.EcosystemFundFeeRatio = value
+	return market
+}
+
+func (market *Market) WithExchangeFee(value sdk.Dec) *Market {
+	market.ExchangeFeeRatio = value
+	return market
+}
+
+func (market *Market) WithLiquidationFee(value sdk.Dec) *Market {
+	market.LiquidationFeeRatio = value
+	return market
+}
+
+func (market *Market) WithPartialLiquidationRatio(value sdk.Dec) *Market {
+	market.PartialLiquidationRatio = value
+	return market
+}
+
+func (market *Market) WithFundingRateEpochId(value string) *Market {
+	market.FundingRateEpochId = value
+	return market
+}
+
+func (market *Market) WithPair(value asset.Pair) *Market {
+	market.Pair = value
+	return market
+}
+
+func (market *Market) WithLatestCumulativePremiumFraction(value sdk.Dec) *Market {
+	market.LatestCumulativePremiumFraction = value
+	return market
+}
+
+func MarketsAreEqual(expected, actual *Market) error {
+	if expected.Pair != actual.Pair {
+		return fmt.Errorf("expected market pair %s, got %s", expected.Pair, actual.Pair)
+	}
+
+	if !expected.PriceFluctuationLimitRatio.Equal(actual.PriceFluctuationLimitRatio) {
+		return fmt.Errorf("expected market price fluctuation limit ratio %s, got %s", expected.PriceFluctuationLimitRatio, actual.PriceFluctuationLimitRatio)
+	}
+
+	if !expected.MaintenanceMarginRatio.Equal(actual.MaintenanceMarginRatio) {
+		return fmt.Errorf("expected market maintenance margin ratio %s, got %s", expected.MaintenanceMarginRatio, actual.MaintenanceMarginRatio)
+	}
+
+	if !expected.MaxLeverage.Equal(actual.MaxLeverage) {
+		return fmt.Errorf("expected market max leverage %s, got %s", expected.MaxLeverage, actual.MaxLeverage)
+	}
+
+	if !expected.EcosystemFundFeeRatio.Equal(actual.EcosystemFundFeeRatio) {
+		return fmt.Errorf("expected market ecosystem fund fee ratio %s, got %s", expected.EcosystemFundFeeRatio, actual.EcosystemFundFeeRatio)
+	}
+
+	if !expected.ExchangeFeeRatio.Equal(actual.ExchangeFeeRatio) {
+		return fmt.Errorf("expected market exchange fee ratio %s, got %s", expected.ExchangeFeeRatio, actual.ExchangeFeeRatio)
+	}
+
+	if !expected.LiquidationFeeRatio.Equal(actual.LiquidationFeeRatio) {
+		return fmt.Errorf("expected market liquidation fee ratio %s, got %s", expected.LiquidationFeeRatio, actual.LiquidationFeeRatio)
+	}
+
+	if !expected.PartialLiquidationRatio.Equal(actual.PartialLiquidationRatio) {
+		return fmt.Errorf("expected market partial liquidation ratio %s, got %s", expected.PartialLiquidationRatio, actual.PartialLiquidationRatio)
+	}
+
+	if expected.FundingRateEpochId != actual.FundingRateEpochId {
+		return fmt.Errorf("expected market funding rate epoch id %s, got %s", expected.FundingRateEpochId, actual.FundingRateEpochId)
+	}
+
+	if !expected.PrepaidBadDebt.Equal(actual.PrepaidBadDebt) {
+		return fmt.Errorf("expected market prepaid bad debt %s, got %s", expected.PrepaidBadDebt, actual.PrepaidBadDebt)
+	}
+
+	if expected.Enabled != actual.Enabled {
+		return fmt.Errorf("expected market enabled %t, got %t", expected.Enabled, actual.Enabled)
+	}
+
+	if expected.TwapLookbackWindow != actual.TwapLookbackWindow {
+		return fmt.Errorf("expected market twap lookback window %s, got %s", expected.TwapLookbackWindow, actual.TwapLookbackWindow)
+	}
+
+	if !expected.LatestCumulativePremiumFraction.Equal(actual.LatestCumulativePremiumFraction) {
+		return fmt.Errorf(
+			"expected market latest cumulative premium fraction %s, got %s",
+			expected.LatestCumulativePremiumFraction,
+			actual.LatestCumulativePremiumFraction,
+		)
 	}
 
 	return nil
