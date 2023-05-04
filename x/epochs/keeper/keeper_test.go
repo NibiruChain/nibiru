d31401f8b6a1021dbee0c72b6957f512a3bc1c42
Jonathan Gimeno <jgimeno@gmail.com>
2023-05-04 12:36:34 +0200

    add week tessts epoch

--- x/epochs/keeper/keeper_test.go
+++ x/epochs/keeper/keeper_test.go
@@ -29,11 +29,12 @@ func TestUpsertEpochInfo_HappyPath(t *testing.T) {
 
 	allEpochs := nibiruApp.EpochsKeeper.AllEpochInfos(ctx)
 
-	require.Len(t, allEpochs, 3)
+	require.Len(t, allEpochs, 4)
 	// Epochs are ordered in alphabetical order
 	require.Equal(t, "15 min", allEpochs[0].Identifier)
 	require.Equal(t, "30 min", allEpochs[1].Identifier)
 	require.Equal(t, "monthly", allEpochs[2].Identifier)
+	require.Equal(t, "week", allEpochs[3].Identifier)
 }
 
 func TestEpochExists(t *testing.T) {
