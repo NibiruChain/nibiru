e224a1cec0bd4f83f66a925ffd94592b32db2af9
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-07 17:17:27 -0400

    feat: add sysroot submodule

--- contrib/make/release.mk
+++ contrib/make/release.mk
@@ -8,12 +8,11 @@ GOLANG_CROSS_VERSION  ?= v1.19.4
 release:
 	docker run \
 		--rm \
-		--platform=linux/amd64 \
 		--privileged \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v "$(CURDIR)":/go/src/$(PACKAGE_NAME) \
+		-v "$(CURDIR)/goreleaser-sysroot":/sysroot \
 		-w /go/src/$(PACKAGE_NAME) \
 		-e CGO_ENABLED=1 \
-		-e TM_VERSION=$(shell go list -m github.com/tendermint/tendermint | sed 's:.* ::') \
 		goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
 		build --rm-dist --debug --skip-validate
