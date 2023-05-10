44e94f9ce1ad09c4cb47765162023658df71ce00
Helder M <helder.moreira@voluzi.com>
2023-05-10 17:24:14 +0100

    remove sysroot

--- contrib/make/release.mk
+++ contrib/make/release.mk
@@ -3,7 +3,7 @@
 ###############################################################################
 
 PACKAGE_NAME		  := github.com/NibiruChain/nibiru
-GOLANG_CROSS_VERSION  ?= v1.19.5
+GOLANG_CROSS_VERSION  ?= v1.19.4
 
 release:
 	docker run \
@@ -11,7 +11,6 @@ release:
 		--privileged \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v "$(CURDIR)":/go/src/$(PACKAGE_NAME) \
-		-v "$(CURDIR)/goreleaser-sysroot":/sysroot \
 		-w /go/src/$(PACKAGE_NAME) \
 		-e CGO_ENABLED=1 \
 		-e GITHUB_TOKEN=${GITHUB_TOKEN} \
