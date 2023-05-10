f957e890bbc3da9faf267b0a9402ac258d40b4a3
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-10 15:11:56 -0400

    refactor(perp): move keeper files to v1 directory

--- x/perp/keeper/v1/clearing_house_integration_test.go
+++ x/perp/keeper/v1/clearing_house_integration_test.go
@@ -21,7 +21,7 @@ import (
 	perpammtypes "github.com/NibiruChain/nibiru/x/perp/amm/types"
 	. "github.com/NibiruChain/nibiru/x/perp/integration/action"
 	. "github.com/NibiruChain/nibiru/x/perp/integration/assertion"
-	"github.com/NibiruChain/nibiru/x/perp/keeper"
+	"github.com/NibiruChain/nibiru/x/perp/keeper/v1"
 	"github.com/NibiruChain/nibiru/x/perp/types/v1"
 )
 
