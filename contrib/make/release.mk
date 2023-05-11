f5579b56f7e1e50015c430d2546ff5ffe999d42b
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2023-05-11 18:02:17 +0400

    ci: build rocksdb (#1304)
    * fix: goreleaser cross version
    
    * ci: revert goreleaser/cross ci
    
    * ci(release): fix static builds (#1208)
    
    * ci(release): fix static builds
    
    * add e2e test
    
    * revert e2e test
    
    ---------
    
    Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>
    
    * fix: revert to docker goreleaser image
    
    * feat: integration with gorocksdb
    
    * chore: changelog update
    
    * feat: add rocksdb to goreleaser
    
    * feat: add sysroot submodule
    
    * feat: Dockerfile builds
    
    * fix: goreleaser
    
    * ci: enable darwin builds
    
    * chore: enable universal binary for darwin
    
    * remove sysroot
    
    * fix: create snapshots directory
    
    * chore: remove unused make recipe
    
    * chore: make rocksdb the default db backend
    
    ---------
    
    Co-authored-by: Kevin Yang <5478483+k-yang@users.noreply.github.com>
    Co-authored-by: Helder Moreira <helder-moreira@users.noreply.github.com>
    Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>
    Co-authored-by: Matthias <97468149+matthiasmatt@users.noreply.github.com>
    Co-authored-by: Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
    Co-authored-by: Helder M <helder.moreira@voluzi.com>

--- contrib/make/release.mk
+++ contrib/make/release.mk
@@ -8,13 +8,11 @@ GOLANG_CROSS_VERSION  ?= v1.19.4
 release:
 	docker run \
 		--rm \
-		--platform=linux/amd64 \
 		--privileged \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v "$(CURDIR)":/go/src/$(PACKAGE_NAME) \
 		-w /go/src/$(PACKAGE_NAME) \
 		-e CGO_ENABLED=1 \
-		-e TM_VERSION=$(shell go list -m github.com/tendermint/tendermint | sed 's:.* ::') \
 		-e GITHUB_TOKEN=${GITHUB_TOKEN} \
 		goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
 		release --rm-dist
