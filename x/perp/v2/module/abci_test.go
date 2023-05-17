e18feddcb445bd2211f41c1cb2f48eddb6061e4d
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-17 12:38:10 -0400

    feat(perp): CreatePool keeper method (#1370)
    * feat(perp): add abci hook to store reserve snapshot
    
    * feat(perp): add CreatePool keeper method
    
    * chore: update changelog

---
+++ x/perp/v2/module/abci_test.go
@@ -0,0 +1,99 @@
+package perp_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/NibiruChain/collections"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/NibiruChain/nibiru/x/common/asset"
+	"github.com/NibiruChain/nibiru/x/common/denoms"
+	"github.com/NibiruChain/nibiru/x/common/testutil/mock"
+	"github.com/NibiruChain/nibiru/x/common/testutil/testapp"
+	perp "github.com/NibiruChain/nibiru/x/perp/v2/module"
+	"github.com/NibiruChain/nibiru/x/perp/v2/types"
+)
+
+func TestSnapshotUpdates(t *testing.T) {
+	app, ctx := testapp.NewNibiruTestAppAndContext(true)
+	initialMarket := *mock.TestMarket()
+	initialAmm := *mock.TestAMMDefault()
+
+	runBlock := func(duration time.Duration) {
+		perp.EndBlocker(ctx, app.PerpKeeperV2)
+		ctx = ctx.
+			WithBlockHeight(ctx.BlockHeight() + 1).
+			WithBlockTime(ctx.BlockTime().Add(duration))
+	}
+
+	ctx = ctx.WithBlockTime(time.Date(2015, 10, 21, 0, 0, 0, 0, time.UTC)).WithBlockHeight(1)
+
+	require.NoError(t, app.PerpKeeperV2.CreatePool(
+		/* ctx */ ctx,
+		/* pair */ asset.Registry.Pair(denoms.BTC, denoms.NUSD),
+		/* market */ initialMarket,
+		/* amm */ initialAmm,
+	))
+
+	expectedSnapshot := types.ReserveSnapshot{
+		Amm:         initialAmm,
+		TimestampMs: ctx.BlockTime().UnixMilli(),
+	}
+
+	t.Log("run one block of 5 seconds")
+	runBlock(5 * time.Second)
+	snapshot, err := app.PerpKeeperV2.ReserveSnapshots.Get(ctx,
+		collections.Join(
+			asset.Registry.Pair(denoms.BTC, denoms.NUSD),
+			time.UnixMilli(expectedSnapshot.TimestampMs),
+		),
+	)
+	require.NoError(t, err)
+	assert.EqualValues(t, expectedSnapshot, snapshot)
+
+	t.Log("affect mark price")
+	market, err := app.PerpKeeperV2.Markets.Get(ctx, asset.Registry.Pair(denoms.BTC, denoms.NUSD))
+	require.NoError(t, err)
+	amm, err := app.PerpKeeperV2.AMMs.Get(ctx, asset.Registry.Pair(denoms.BTC, denoms.NUSD))
+	require.NoError(t, err)
+
+	_, baseAmtAbs, err := app.PerpKeeperV2.SwapQuoteAsset(
+		ctx,
+		market,
+		amm,
+		types.Direction_LONG,
+		sdk.NewDec(250e9),
+		sdk.ZeroDec(),
+	)
+	require.NoError(t, err)
+	// dxAmm := (k / (y + dyAmm)) - x = (1e24 / (1e12 + 250e9)) - 1e12 = -200e9
+	assert.EqualValues(t, sdk.NewDec(200e9), baseAmtAbs)
+
+	expectedSnapshot = types.ReserveSnapshot{
+		Amm: types.AMM{
+			Pair:            asset.Registry.Pair(denoms.BTC, denoms.NUSD),
+			BaseReserve:     sdk.NewDec(800e9),
+			QuoteReserve:    sdk.NewDec(1.25e12),
+			SqrtDepth:       sdk.NewDec(1e12),
+			PriceMultiplier: sdk.OneDec(),
+			TotalLong:       sdk.NewDec(200e9),
+			TotalShort:      sdk.ZeroDec(),
+		},
+		TimestampMs: ctx.BlockTime().UnixMilli(),
+	}
+
+	t.Log("run one block of 5 seconds")
+	runBlock(5 * time.Second)
+
+	snapshot, err = app.PerpKeeperV2.ReserveSnapshots.Get(ctx,
+		collections.Join(
+			asset.Registry.Pair(denoms.BTC, denoms.NUSD),
+			time.UnixMilli(expectedSnapshot.TimestampMs),
+		),
+	)
+	require.NoError(t, err)
+	assert.EqualValues(t, expectedSnapshot, snapshot)
+}
