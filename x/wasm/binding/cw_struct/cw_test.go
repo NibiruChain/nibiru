80d145cb42d9a877d1d93137ccf3eb31f4c2a045
Unique-Divine <realuniquedivine@gmail.com>
2023-05-18 16:27:47 -0400

    feat(wasmbin): aadd create_market to controller contract + interface test

--- x/wasm/binding/cw_struct/cw_test.go
+++ x/wasm/binding/cw_struct/cw_test.go
@@ -110,6 +110,7 @@ func (s *TestSuiteBindingJsonTypes) TestExecuteMsgs() {
 		"edit_oracle_params",
 		"set_market_enabled",
 		"insurance_fund_withdraw",
+		"create_market",
 	}
 
 	for _, name := range testCaseMap {
