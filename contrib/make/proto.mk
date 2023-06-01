0f7af0a48c1522268cc7097d0abfdeb129d79cea
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-06-01 02:46:06 -0500

    feat(perp): emit information events per block (#1406)
    * chore: use buf to generate protos
    
    * fix: buf config
    
    * chore: lint proto files
    
    * chore: regen protos using buf
    
    * refactor: FundingRateChangedEvent
    
    * feat: emit MarketUpdatedEvent
    
    * refactor: rename variable
    
    * feat: emit AmmUpdated and MarketUpdated event at the end of every block
    
    * chore: remove other instances of AmmUpdatedEvent
    
    * feat: emit mark twap and index twap per block
    
    * chore: update changelog

--- contrib/make/proto.mk
+++ contrib/make/proto.mk
@@ -1,21 +1,27 @@
 ###############################################################################
 ###                                  Proto                                  ###
 ###############################################################################
+PROJECT_NAME = $(shell git remote get-url origin | xargs basename -s .git)
 
-containerProtoVer=v0.3
-containerProtoImage=tendermintdev/sdk-proto-gen:$(containerProtoVer)
-containerProtoGen=cosmos-sdk-proto-gen-$(containerProtoVer)
-containerProtoGenSwagger=cosmos-sdk-proto-gen-swagger-$(containerProtoVer)
-containerProtoFmt=cosmos-sdk-proto-fmt-$(containerProtoVer)
+protoVer=v0.7
+protoImageName=tendermintdev/sdk-proto-gen:$(protoVer)
+containerProtoGen=$(PROJECT_NAME)-proto-gen-$(protoVer)
+containerProtoGenAny=$(PROJECT_NAME)-proto-gen-any-$(protoVer)
+containerProtoGenSwagger=$(PROJECT_NAME)-proto-gen-swagger-$(protoVer)
+containerProtoFmt=$(PROJECT_NAME)-proto-fmt-$(protoVer)
 
-.PHONY: proto-gen
+proto-all: proto-format proto-lint proto-gen
+
 proto-gen:
 	@echo "Generating Protobuf files"
-	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoGen}$$"; then \
-		docker start -a $(containerProtoGen); else \
-		docker run --name $(containerProtoGen) \
-			-v "$(CURDIR)":/workspace \
-			-w /workspace \
-			$(containerProtoImage) \
-			sh ./contrib/scripts/protocgen.sh; \
-	fi
+	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoGen}$$"; then docker start -a $(containerProtoGen); else docker run --name $(containerProtoGen) -v $(CURDIR):/workspace --workdir /workspace $(protoImageName) \
+		sh ./contrib/scripts/protocgen.sh; fi
+
+proto-format:
+	@echo "Formatting Protobuf files"
+	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoFmt}$$"; then docker start -a $(containerProtoFmt); else docker run --name $(containerProtoFmt) -v $(CURDIR):/workspace --workdir /workspace tendermintdev/docker-build-proto \
+		find ./ -not -path "./third_party/*" -name "*.proto" -exec clang-format -i {} \; ; fi
+
+
+proto-lint:
+	@$(DOCKER_BUF) lint --error-format=json
