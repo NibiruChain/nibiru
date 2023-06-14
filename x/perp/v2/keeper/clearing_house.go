2d08f1ef029c54976c8356e199447909f6dcf496
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2023-06-14 10:49:14 -0400

    feat(perp): Add change type and exchanged margin to position changed events. (#1424)
    * feat(perp): Add change type and exchanged margin to position changed events.
    
    * changelog: add PR number
    
    * test: add correctness check on the change type field of position changed event

--- x/perp/v2/keeper/clearing_house.go
+++ x/perp/v2/keeper/clearing_house.go
@@ -98,7 +98,10 @@ func (k Keeper) OpenPosition(
 		}
 	}
 
-	if err = k.afterPositionUpdate(ctx, market, *updatedAMM, traderAddr, *positionResp); err != nil {
+	changeType := "open_position"
+	if err = k.afterPositionUpdate(
+		ctx, market, *updatedAMM, traderAddr, *positionResp, changeType,
+	); err != nil {
 		return nil, err
 	}
 
@@ -515,6 +518,7 @@ func (k Keeper) afterPositionUpdate(
 	amm types.AMM,
 	traderAddr sdk.AccAddress,
 	positionResp types.PositionResp,
+	changeType string,
 ) (err error) {
 	// check bad debt
 	if !positionResp.BadDebt.IsZero() {
@@ -576,6 +580,8 @@ func (k Keeper) afterPositionUpdate(
 			BadDebt:          sdk.NewCoin(market.Pair.QuoteDenom(), positionResp.BadDebt.RoundInt()),
 			FundingPayment:   positionResp.FundingPayment,
 			BlockHeight:      ctx.BlockHeight(),
+			ExchangedMargin:  marginToVault.Neg().Sub(transferredFee),
+			ChangeType:       changeType,
 		},
 	)
 
@@ -749,12 +755,14 @@ func (k Keeper) ClosePosition(ctx sdk.Context, pair asset.Pair, traderAddr sdk.A
 		return positionResp, nil
 	}
 
+	changeType := "close_position"
 	if err = k.afterPositionUpdate(
 		ctx,
 		market,
 		*updatedAMM,
 		traderAddr,
 		*positionResp,
+		changeType,
 	); err != nil {
 		return nil, err
 	}
