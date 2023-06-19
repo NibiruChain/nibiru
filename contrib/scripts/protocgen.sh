7b8f5c3e5916c949a743dd8a82bf385c4c40f03a
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-06-19 13:43:17 -0400

    Update protocgen.sh

--- contrib/scripts/protocgen.sh
+++ contrib/scripts/protocgen.sh
@@ -5,10 +5,12 @@ set -eo pipefail
 echo "Generating gogo proto code"
 cd proto
 echo "$(pwd)"
-proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
+  echo "Generating gogo proto code for $dir"
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
+      echo "Generating gogo proto code for $file"
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done
