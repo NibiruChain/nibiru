9da9f962c61528eca60f096bf2cb04ce11e09934
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-06-19 13:52:31 -0400

    remove protogen gha workflow

--- contrib/scripts/protocgen.sh
+++ contrib/scripts/protocgen.sh
@@ -4,10 +4,8 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-echo "$(pwd)"
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  echo "Generating gogo proto code for $dir"
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
       echo "Generating gogo proto code for $file"
@@ -17,8 +15,6 @@ for dir in $proto_dirs; do
 done
 
 cd ..
-echo "$(pwd)"
-ls -al
 # move proto files to the right places
 cp -r github.com/NibiruChain/nibiru/* ./
 rm -rf github.com
