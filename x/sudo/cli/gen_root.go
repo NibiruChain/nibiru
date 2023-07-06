bb75ebb6a6fe47d58f69ae0ec697995898cfacf9
Jonathan Gimeno <jgimeno@gmail.com>
2023-07-06 15:04:05 +0200

    add sudo root account cmd

---
+++ x/sudo/cli/gen_root.go
@@ -0,0 +1,64 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/NibiruChain/nibiru/x/sudo/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	"github.com/spf13/cobra"
+)
+
+func AddSudoRootAccountCmd(defaultNodeHome string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add-sudo-root-account",
+		Short: "Add sudo module root account to genesis.json.",
+		Long:  `Add sudo module root account to genesis.json.`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
+
+			config.SetRoot(clientCtx.HomeDir)
+
+			genFile := config.GenesisFile()
+			appState, genDoc, err := genutiltypes.GenesisStateFromGenFile(genFile)
+			if err != nil {
+				return err
+			}
+
+			rootAccount := args[0]
+			addr, err := sdk.AccAddressFromBech32(rootAccount)
+			if err != nil {
+				return fmt.Errorf("failed to parse address: %w", err)
+			}
+
+			sudoGenState := types.GetGenesisStateFromAppState(clientCtx.Codec, appState)
+			sudoGenState.Sudoers.Root = addr.String()
+
+			sudoGenStateBz, err := clientCtx.Codec.MarshalJSON(sudoGenState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal market genesis state: %w", err)
+			}
+
+			appState[types.ModuleName] = sudoGenStateBz
+
+			appStateJSON, err := json.Marshal(appState)
+			if err != nil {
+				return fmt.Errorf("failed to marshal application genesis state: %w", err)
+			}
+
+			genDoc.AppState = appStateJSON
+			return genutil.ExportGenesisFile(genDoc, genFile)
+		},
+	}
+
+	cmd.Flags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
+
+	return cmd
+}
---
+++ x/sudo/types/genesis.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+func (gen *GenesisState) Validate() error {
+	if gen.Sudoers.Contracts == nil {
+		return fmt.Errorf("nil contract state must be []string")
+	} else if err := gen.Sudoers.Validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetGenesisStateFromAppState(cdc codec.JSONCodec, appState map[string]json.RawMessage) *GenesisState {
+	var genesisState GenesisState
+
+	if appState[ModuleName] != nil {
+		cdc.MustUnmarshalJSON(appState[ModuleName], &genesisState)
+	}
+
+	return &genesisState
+}
