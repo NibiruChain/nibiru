a51704c2cb71f56b38ae5de5f03d714a113d0505
Helder M <helder.moreira@voluzi.com>
2023-07-19 23:46:16 +0100

    chore(build): update wasmvm static lib version to download

--- contrib/make/build.mk
+++ contrib/make/build.mk
@@ -26,7 +26,7 @@ endif
 SDK_PACK := $(shell go list -m github.com/cosmos/cosmos-sdk | sed  's/ /\@/g')
 TM_VERSION := $(shell go list -m github.com/cometbft/cometbft | sed 's:.* ::') # grab everything after the space in "github.com/tendermint/tendermint v0.34.7"
 ROCKSDB_VERSION := 8.1.1
-WASMVM_VERSION := 1.2.4
+WASMVM_VERSION := 1.3.0
 DOCKER := $(shell which docker)
 BUILDDIR ?= $(CURDIR)/build
 TEMPDIR ?= $(CURDIR)/temp
