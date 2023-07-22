62efcc79a793fe13b092edfd88149255b8ab3156
Matthias <97468149+matthiasmatt@users.noreply.github.com>
2023-07-22 11:39:39 +0200

    test: increase test coverage for x/hooks (#1517)

--- x/epochs/module.go
+++ x/epochs/module.go
@@ -120,7 +120,7 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.Ra
 	// Initialize global index to index in genesis state
 	cdc.MustUnmarshalJSON(gs, &genState)
 
-	InitGenesis(ctx, am.keeper, genState)
+	_ = InitGenesis(ctx, am.keeper, genState)
 
 	return []abci.ValidatorUpdate{}
 }
