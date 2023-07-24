520ae0f7d6af2a7385611e7328f8741b625af8c3
Matthias <md2022@matrixsystems.co>
2023-07-24 18:35:59 +0200

    test: add more position test for validate

--- x/perp/v2/types/position_test.go
+++ x/perp/v2/types/position_test.go
@@ -66,6 +66,10 @@ func TestPositionsAreEqual(t *testing.T) {
 			modifier:      func(p *Position) { p.WithLastUpdatedBlockNumber(42) },
 			requiredError: "expected position block number",
 		},
+		{
+			modifier:      func(p *Position) { p.WithLatestCumulativePremiumFraction(sdk.NewDec(42)) },
+			requiredError: "expected position latest cumulative premium fraction",
+		},
 	}
 	for _, tc := range testCases {
 		tc := tc
@@ -75,6 +79,62 @@ func TestPositionsAreEqual(t *testing.T) {
 			tc.modifier(newPosition)
 
 			err := PositionsAreEqual(&expected, newPosition)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.requiredError)
+		})
+	}
+}
+
+func TestPositionValidate(t *testing.T) {
+	accAddress := "cosmos1zaavvzxez0elundtn32qnk9lkm8kmcszzsv80v"
+
+	validPosition := Position{
+		TraderAddress:                   accAddress,
+		Pair:                            "ubtc:unusd",
+		Size_:                           sdk.OneDec(),
+		Margin:                          sdk.OneDec(),
+		OpenNotional:                    sdk.OneDec(),
+		LatestCumulativePremiumFraction: sdk.OneDec(),
+		LastUpdatedBlockNumber:          0,
+	}
+
+	testCases := []struct {
+		modifier      func(*Position)
+		requiredError string
+	}{
+		{
+			modifier:      func(p *Position) { p.WithTraderAddress("invalid") },
+			requiredError: "decoding bech32 failed",
+		},
+		{
+			modifier:      func(p *Position) { p.WithPair("invalid") },
+			requiredError: "invalid: invalid token pair",
+		},
+		{
+			modifier:      func(p *Position) { p.WithSize_(sdk.ZeroDec()) },
+			requiredError: "zero size",
+		},
+		{
+			modifier:      func(p *Position) { p.WithMargin(sdk.ZeroDec()) },
+			requiredError: "margin <= 0",
+		},
+		{
+			modifier:      func(p *Position) { p.WithOpenNotional(sdk.ZeroDec()) },
+			requiredError: "open notional <= 0",
+		},
+		{
+			modifier:      func(p *Position) { p.WithLastUpdatedBlockNumber(-1) },
+			requiredError: "invalid block number",
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.requiredError, func(t *testing.T) {
+			newPosition := validPosition.copy()
+
+			tc.modifier(newPosition)
+
+			err := newPosition.Validate()
 			require.Error(t, err)
 			require.Contains(t, err.Error(), tc.requiredError)
 		})
