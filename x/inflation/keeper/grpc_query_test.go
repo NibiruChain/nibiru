e7938514242c013959b07cbc5520373a81e0ccf2
Unique-Divine <realuniquedivine@gmail.com>
2023-07-25 20:10:32 -0500

    test(inflation): test getters in params.go

--- x/inflation/keeper/grpc_query_test.go
+++ x/inflation/keeper/grpc_query_test.go
@@ -1,42 +1,79 @@
 package keeper_test
 
 import (
-	"testing"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
+	"github.com/NibiruChain/nibiru/app"
 	"github.com/NibiruChain/nibiru/x/common/testutil/testapp"
-	"github.com/NibiruChain/nibiru/x/inflation/types"
+
+	inflationtypes "github.com/NibiruChain/nibiru/x/inflation/types"
 )
 
-func TestPeriod(t *testing.T) {
+type QueryServerSuite struct {
+	suite.Suite
+
+	nibiruApp *app.NibiruApp
+	ctx       sdk.Context
+}
+
+func (s *QueryServerSuite) SetupSuite() {
 	nibiruApp, ctx := testapp.NewNibiruTestAppAndContext(true)
+	s.nibiruApp = nibiruApp
+	s.ctx = ctx
+}
 
-	resp, err := nibiruApp.InflationKeeper.Period(sdk.WrapSDKContext(ctx), &types.QueryPeriodRequest{})
+func (s *QueryServerSuite) TestQueryPeriod() {
+	nibiruApp, ctx := s.nibiruApp, s.ctx
 
-	require.NoError(t, err)
-	assert.Equal(t, uint64(0), resp.Period)
+	resp, err := nibiruApp.InflationKeeper.Period(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QueryPeriodRequest{},
+	)
+
+	s.NoError(err)
+	s.Assert().Equal(uint64(0), resp.Period)
 
 	nibiruApp.InflationKeeper.CurrentPeriod.Next(ctx)
 
-	resp, err = nibiruApp.InflationKeeper.Period(sdk.WrapSDKContext(ctx), &types.QueryPeriodRequest{})
-	require.NoError(t, err)
-	assert.Equal(t, uint64(1), resp.Period)
+	resp, err = nibiruApp.InflationKeeper.Period(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QueryPeriodRequest{},
+	)
+	s.NoError(err)
+	s.Assert().Equal(uint64(1), resp.Period)
 }
 
-func SkippedEpochs(t *testing.T) {
-	nibiruApp, ctx := testapp.NewNibiruTestAppAndContext(true)
+func (s *QueryServerSuite) TestQuerySkippedEpochs() {
+	nibiruApp, ctx := s.nibiruApp, s.ctx
+	resp, err := nibiruApp.InflationKeeper.SkippedEpochs(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QuerySkippedEpochsRequest{},
+	)
 
-	resp, err := nibiruApp.InflationKeeper.SkippedEpochs(sdk.WrapSDKContext(ctx), &types.QuerySkippedEpochsRequest{})
-
-	require.NoError(t, err)
-	assert.Equal(t, uint64(0), resp.SkippedEpochs)
+	s.Require().NoError(err)
+	s.Assert().Equal(uint64(0), resp.SkippedEpochs)
 
 	nibiruApp.InflationKeeper.NumSkippedEpochs.Next(ctx)
 
-	resp, err = nibiruApp.InflationKeeper.SkippedEpochs(sdk.WrapSDKContext(ctx), &types.QuerySkippedEpochsRequest{})
-	require.NoError(t, err)
-	assert.Equal(t, uint64(1), resp.SkippedEpochs)
+	resp, err = nibiruApp.InflationKeeper.SkippedEpochs(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QuerySkippedEpochsRequest{},
+	)
+	s.NoError(err)
+	s.Assert().Equal(uint64(1), resp.SkippedEpochs)
+}
+
+func (s *QueryServerSuite) TestQueryEpochMintProvision() {
+	nibiruApp, ctx := s.nibiruApp, s.ctx
+	resp, err := nibiruApp.InflationKeeper.EpochMintProvision(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QueryEpochMintProvisionRequest{},
+	)
+	s.NoError(err)
+	s.NotNil(resp)
+}
+
+func (s *QueryServerSuite) TestQueryInflationRate() {
+	nibiruApp, ctx := s.nibiruApp, s.ctx
+	resp, err := nibiruApp.InflationKeeper.InflationRate(
+		sdk.WrapSDKContext(ctx), &inflationtypes.QueryInflationRateRequest{},
+	)
+	s.NoError(err)
+	s.NotNil(resp)
 }
