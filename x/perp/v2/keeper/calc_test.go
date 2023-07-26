62bc06fe0a2b03e1db34e845dcbec369410cc744
Matthias <md2022@matrixsystems.co>
2023-07-26 16:52:22 +0200

    fix: fix margin test

--- x/perp/v2/keeper/calc_test.go
+++ x/perp/v2/keeper/calc_test.go
@@ -387,8 +387,8 @@ func TestMarginRatioFail(t *testing.T) {
 	val := keeper.MarginRatio(types.Position{Size_: sdk.ZeroDec()}, sdk.ZeroDec(), sdk.ZeroDec())
 	require.Equal(t, sdk.ZeroDec(), val)
 
-	_, err := keeper.PositionNotionalSpot(types.AMM{}, types.Position{Size_: sdk.ZeroDec()})
-	require.ErrorIs(t, err, types.ErrInputBaseAmtNegative)
+	_, err := keeper.PositionNotionalSpot(types.AMM{PriceMultiplier: sdk.OneDec()}, types.Position{Size_: sdk.ZeroDec()})
+	require.NoError(t, err)
 
 	_, err = keeper.PositionNotionalSpot(types.AMM{}, types.Position{})
 	require.ErrorContains(t, err, "input base amt is nil")
