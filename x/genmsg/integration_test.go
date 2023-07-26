074e2f3f3b814c92dfd5eabdc478b8dafd55f3f5
Jonathan Gimeno <jgimeno@gmail.com>
2023-07-26 12:19:19 +0200

    add happy path for module genmsg

--- x/genmsg/integration_test.go
+++ x/genmsg/integration_test.go
@@ -1,27 +1,57 @@
 package genmsg_test
 
 import (
+	"github.com/NibiruChain/nibiru/app"
+	"github.com/NibiruChain/nibiru/x/common/testutil/testapp"
+	"github.com/NibiruChain/nibiru/x/genmsg"
+	v1 "github.com/NibiruChain/nibiru/x/genmsg/v1"
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestIntegration(t *testing.T) {
-	//recvAddr := sdk.AccAddress("recv")
-	//chain := e2eTesting.NewTestChain(t, 1, e2eTesting.WithDummyTestAddress(), e2eTesting.WithGenDefaultCoinBalance("100000000000000000000000000000000000"), e2eTesting.TestChainGenesisOption(func(cdc codec.Codec, genesis app.GenesisState) {
-	//	testMsg := &banktypes.MsgSend{
-	//		FromAddress: e2eTesting.TestAccountAddr.String(),
-	//		ToAddress:   recvAddr.String(),
-	//		Amount:      sdk.NewCoins(sdk.NewInt64Coin("stake", 1000)),
-	//	}
-	//	anyMsg, err := codectypes.NewAnyWithValue(testMsg)
-	//	require.NoError(t, err)
-	//	genesis[genmsg.ModuleName] = cdc.MustMarshalJSON(&v1.GenesisState{Messages: []*codectypes.Any{anyMsg}})
-	//}))
-	//bankQuery := banktypes.NewQueryClient(chain.Client())
-	//resp, err := bankQuery.Balance(sdk.WrapSDKContext(chain.GetContext()), &banktypes.QueryBalanceRequest{
-	//	Address: recvAddr.String(),
-	//	Denom:   "stake",
-	//})
-	//
-	//require.NoError(t, err)
-	//t.Log(resp)
+	senderAddr := sdk.AccAddress("sender")
+	recvAddr := sdk.AccAddress("recv")
+
+	encoding := app.MakeEncodingConfig()
+	appGenesis := app.NewDefaultGenesisState(encoding.Marshaler)
+
+	appGenesis[banktypes.ModuleName] = encoding.Marshaler.MustMarshalJSON(&banktypes.GenesisState{
+		Balances: []banktypes.Balance{
+			{
+				Address: senderAddr.String(),
+				Coins:   sdk.NewCoins(sdk.NewInt64Coin("unibi", 100000)),
+			},
+		},
+	})
+
+	testMsg := &banktypes.MsgSend{
+		FromAddress: senderAddr.String(),
+		ToAddress:   recvAddr.String(),
+		Amount:      sdk.NewCoins(sdk.NewInt64Coin("unibi", 1000)),
+	}
+
+	anyMsg, err := codectypes.NewAnyWithValue(testMsg)
+	require.NoError(t, err)
+
+	appGenesis[genmsg.ModuleName] = encoding.Marshaler.MustMarshalJSON(
+		&v1.GenesisState{
+			Messages: []*codectypes.Any{anyMsg},
+		},
+	)
+
+	app := testapp.NewNibiruTestApp(appGenesis)
+	ctx := app.NewContext(false, tmproto.Header{
+		Height: 1,
+	})
+
+	balance, err := app.BankKeeper.Balance(ctx, &banktypes.QueryBalanceRequest{
+		Address: recvAddr.String(),
+		Denom:   "unibi",
+	})
+	require.True(t, balance.Balance.Equal(sdk.NewInt64Coin("unibi", 1000)))
 }
