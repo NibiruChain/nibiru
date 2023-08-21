998f3f73dfb69a1bcc6638ed0b6e79da4a8781bc
Jonathan Gimeno <jgimeno@gmail.com>
2023-08-21 20:33:35 +0200

    refactor ante handler

--- x/devgas/v1/ante/ante.go
+++ x/devgas/v1/ante/ante.go
@@ -33,7 +33,7 @@ func NewDevGasPayoutDecorator(
 	}
 }
 
-func (anteDec DevGasPayoutDecorator) AnteHandle(
+func (a DevGasPayoutDecorator) AnteHandle(
 	ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler,
 ) (newCtx sdk.Context, err error) {
 	feeTx, ok := tx.(sdk.FeeTx)
@@ -41,8 +41,8 @@ func (anteDec DevGasPayoutDecorator) AnteHandle(
 		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
 	}
 
-	err = DevGasPayout(
-		ctx, anteDec.bankKeeper, feeTx.GetFee(), anteDec.devgasKeeper, tx.GetMsgs(),
+	err = a.devGasPayout(
+		ctx, feeTx,
 	)
 	if err != nil {
 		return ctx, errorsmod.Wrapf(sdkerrors.ErrInsufficientFunds, err.Error())
@@ -51,57 +51,20 @@ func (anteDec DevGasPayoutDecorator) AnteHandle(
 	return next(ctx, tx, simulate)
 }
 
-// FeePayLogic takes the total fees and splits them based on the governance
-// params and the number of contracts we are executing on. This returns the
-// amount of fees each contract developer should get. tested in ante_test.go
-func FeePayLogic(fees sdk.Coins, govPercent sdk.Dec, numPairs int) sdk.Coins {
-	var splitFees sdk.Coins
-	for _, c := range fees.Sort() {
-		rewardAmount := govPercent.MulInt(c.Amount).QuoInt64(int64(numPairs)).RoundInt()
-		if !rewardAmount.IsZero() {
-			splitFees = splitFees.Add(sdk.NewCoin(c.Denom, rewardAmount))
-		}
-	}
-	return splitFees
-}
-
-type FeeSharePayoutEventOutput struct {
-	WithdrawAddress sdk.AccAddress `json:"withdraw_address"`
-	FeesPaid        sdk.Coins      `json:"fees_paid"`
-}
-
-// DevGasPayout takes the total fees and redistributes 50% (or param set) to
+// devGasPayout takes the total fees and redistributes 50% (or param set) to
 // the contract developers provided they opted-in to payments.
-func DevGasPayout(
+func (a DevGasPayoutDecorator) devGasPayout(
 	ctx sdk.Context,
-	bankKeeper BankKeeper,
-	totalFees sdk.Coins,
-	devgasKeeper IDevGasKeeper,
-	msgs []sdk.Msg,
+	tx sdk.FeeTx,
 ) error {
-	params := devgasKeeper.GetParams(ctx)
+	params := a.devgasKeeper.GetParams(ctx)
 	if !params.EnableFeeShare {
 		return nil
 	}
 
-	// Get valid withdraw addresses from contracts
-	toPay := make([]sdk.AccAddress, 0)
-	for _, msg := range msgs {
-		if _, ok := msg.(*wasmtypes.MsgExecuteContract); ok {
-			contractAddr, err := sdk.AccAddressFromBech32(
-				msg.(*wasmtypes.MsgExecuteContract).Contract,
-			)
-			if err != nil {
-				return err
-			}
-
-			shareData, _ := devgasKeeper.GetFeeShare(ctx, contractAddr)
-
-			withdrawAddr := shareData.GetWithdrawerAddr()
-			if withdrawAddr != nil && !withdrawAddr.Empty() {
-				toPay = append(toPay, withdrawAddr)
-			}
-		}
+	toPay, err := a.getWithdrawAddressesFromMsgs(ctx, tx.GetMsgs())
+	if err != nil {
+		return err
 	}
 
 	// Do nothing if no one needs payment
@@ -109,38 +72,24 @@ func DevGasPayout(
 		return nil
 	}
 
-	// Get only allowed governance fees to be paid (helps for taxes)
-	var fees sdk.Coins
-	if len(params.AllowedDenoms) == 0 {
-		// If empty, we allow all denoms to be used as payment
-		fees = totalFees
-	} else {
-		for _, fee := range totalFees.Sort() {
-			for _, allowed := range params.AllowedDenoms {
-				if fee.Denom == allowed {
-					fees = fees.Add(fee)
-				}
-			}
-		}
-	}
+	allowedFees := getAllowedFees(params, tx.GetFee())
 
 	numPairs := len(toPay)
-
 	feesPaidOutput := make([]FeeSharePayoutEventOutput, numPairs)
 	if numPairs > 0 {
 		govPercent := params.DeveloperShares
-		splitFees := FeePayLogic(fees, govPercent, numPairs)
+		splitFees := FeePayLogic(allowedFees, govPercent, numPairs)
 
 		// pay fees evenly between all withdraw addresses
 		for i, withdrawAddr := range toPay {
-			err := bankKeeper.SendCoinsFromModuleToAccount(ctx, authtypes.FeeCollectorName, withdrawAddr, splitFees)
+			err := a.bankKeeper.SendCoinsFromModuleToAccount(ctx, authtypes.FeeCollectorName, withdrawAddr, splitFees)
 			feesPaidOutput[i] = FeeSharePayoutEventOutput{
 				WithdrawAddress: withdrawAddr,
 				FeesPaid:        splitFees,
 			}
 
 			if err != nil {
-				return errorsmod.Wrapf(feeshare.ErrFeeSharePayment, "failed to pay fees to contract developer: %s", err.Error())
+				return errorsmod.Wrapf(feeshare.ErrFeeSharePayment, "failed to pay allowedFees to contract developer: %s", err.Error())
 			}
 		}
 	}
@@ -157,4 +106,68 @@ func DevGasPayout(
 	)
 
 	return nil
+}
+
+// getAllowedFees gets the allowed fees to be paid based on the governance params
+func getAllowedFees(params feeshare.ModuleParams, totalFees sdk.Coins) sdk.Coins {
+	// Get only allowed governance fees to be paid (helps for taxes)
+	var allowedFees sdk.Coins
+	if len(params.AllowedDenoms) == 0 {
+		// If empty, we allow all denoms to be used as payment
+		allowedFees = totalFees
+	} else {
+		for _, fee := range totalFees.Sort() {
+			for _, allowed := range params.AllowedDenoms {
+				if fee.Denom == allowed {
+					allowedFees = allowedFees.Add(fee)
+				}
+			}
+		}
+	}
+
+	return allowedFees
+}
+
+// getWithdrawAddressesFromMsgs returns a list of all contract addresses that have opted-in to receiving payments
+func (a DevGasPayoutDecorator) getWithdrawAddressesFromMsgs(ctx sdk.Context, msgs []sdk.Msg) ([]sdk.AccAddress, error) {
+	toPay := make([]sdk.AccAddress, 0)
+	for _, msg := range msgs {
+		if _, ok := msg.(*wasmtypes.MsgExecuteContract); ok {
+			contractAddr, err := sdk.AccAddressFromBech32(
+				msg.(*wasmtypes.MsgExecuteContract).Contract,
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			shareData, _ := a.devgasKeeper.GetFeeShare(ctx, contractAddr)
+
+			withdrawAddr := shareData.GetWithdrawerAddr()
+			if withdrawAddr != nil && !withdrawAddr.Empty() {
+				toPay = append(toPay, withdrawAddr)
+			}
+		}
+	}
+
+	return toPay, nil
+}
+
+// FeePayLogic takes the total fees and splits them based on the governance
+// params and the number of contracts we are executing on. This returns the
+// amount of fees each contract developer should get. tested in ante_test.go
+func FeePayLogic(fees sdk.Coins, govPercent sdk.Dec, numPairs int) sdk.Coins {
+	var splitFees sdk.Coins
+	for _, c := range fees.Sort() {
+		rewardAmount := govPercent.MulInt(c.Amount).QuoInt64(int64(numPairs)).RoundInt()
+		if !rewardAmount.IsZero() {
+			splitFees = splitFees.Add(sdk.NewCoin(c.Denom, rewardAmount))
+		}
+	}
+
+	return splitFees
+}
+
+type FeeSharePayoutEventOutput struct {
+	WithdrawAddress sdk.AccAddress `json:"withdraw_address"`
+	FeesPaid        sdk.Coins      `json:"fees_paid"`
 }
