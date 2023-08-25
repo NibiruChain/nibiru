7e3fa90ef5de2b3b8c7c37e67862b08c621afa8f
Unique-Divine <realuniquedivine@gmail.com>
2023-08-25 04:16:35 -0500

    feat(devgas): Convert legacy ABCI events to typed proto events

--- x/devgas/v1/types/events.go
+++ x/devgas/v1/types/events.go
@@ -5,8 +5,7 @@ const (
 	EventTypeRegisterFeeShare = "register_feeshare"
 	EventTypeCancelFeeShare   = "cancel_feeshare"
 	EventTypeUpdateFeeShare   = "update_feeshare"
-
-	EventTypePayoutFeeShare = "payout_feeshare"
+	EventTypePayoutFeeShare   = "payout_feeshare"
 
 	AttributeKeyContract          = "contract"
 	AttributeKeyWithdrawerAddress = "withdrawer_address"
