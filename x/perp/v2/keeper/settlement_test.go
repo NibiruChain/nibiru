be275b4dabf42dbd7472ab16fb19315570f93931
Jonathan Gimeno <jgimeno@gmail.com>
2023-09-25 18:24:42 +0200

    open position on disabled market

--- x/perp/v2/keeper/settlement_test.go
+++ x/perp/v2/keeper/settlement_test.go
@@ -3,9 +3,12 @@ package keeper_test
 import (
 	"github.com/NibiruChain/nibiru/x/common/asset"
 	"github.com/NibiruChain/nibiru/x/common/denoms"
+	"github.com/NibiruChain/nibiru/x/common/testutil"
 	. "github.com/NibiruChain/nibiru/x/common/testutil/action"
 	. "github.com/NibiruChain/nibiru/x/perp/v2/integration/action"
 	. "github.com/NibiruChain/nibiru/x/perp/v2/integration/assertion"
+	"github.com/NibiruChain/nibiru/x/perp/v2/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 	"time"
 )
@@ -13,6 +16,7 @@ import (
 func TestDisableMarket(t *testing.T) {
 	pairBtcUsdc := asset.Registry.Pair(denoms.BTC, denoms.USDC)
 	startTime := time.Now()
+	alice := testutil.AccAddress()
 
 	tc := TestCases{
 		TC("market can be disabled").
@@ -33,8 +37,32 @@ func TestDisableMarket(t *testing.T) {
 					Market_EnableShouldBeEqualTo(false),
 				),
 			),
+		TC("cannot open position on disabled market").
+			Given(
+				CreateCustomMarket(
+					pairBtcUsdc,
+					WithPricePeg(sdk.OneDec()),
+					WithSqrtDepth(sdk.NewDec(100_000)),
+				),
+				SetBlockNumber(1),
+				SetBlockTime(startTime),
 
-		TC("market can be disabled and enabled again").When(),
+				FundAccount(alice, sdk.NewCoins(sdk.NewCoin(denoms.NUSD, sdk.NewInt(10_200)))),
+			).
+			When(
+				CloseMarket(pairBtcUsdc),
+			).
+			Then(
+				MarketOrderFails(
+					alice,
+					pairBtcUsdc,
+					types.Direction_LONG,
+					sdk.NewInt(10_000),
+					sdk.OneDec(),
+					sdk.ZeroDec(),
+					types.ErrMarketNotEnabled,
+				),
+			),
 	}
 
 	NewTestSuite(t).WithTestCases(tc...).Run()
