ef307eb3a73ad887c3a8660c55092b0f7c41153f
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-10-09 15:50:56 -0500

    refactor(wasm): wasmbinding folder structure (#1630)
    * refactor(wasm): clean up wasmbindings
    
    * chore: rename some files
    
    * Update CHANGELOG.md

--- wasmbinding/wasmbin/contracts_test.go
+++ wasmbinding/wasmbin/contracts_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/NibiruChain/nibiru/x/wasm/binding/wasmbin"
+	"github.com/NibiruChain/nibiru/wasmbinding/wasmbin"
 )
 
 func TestExpectedBytecodeExists(t *testing.T) {
