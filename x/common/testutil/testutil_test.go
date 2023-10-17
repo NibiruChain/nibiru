e67c67bdefbb7449d56263979807189a07b1f8eb
Unique-Divine <realuniquedivine@gmail.com>
2023-10-17 12:16:15 -0500

    feat: add all features from wasmd to app + more tests

--- x/common/testutil/testutil_test.go
+++ x/common/testutil/testutil_test.go
@@ -1,11 +1,15 @@
 package testutil_test
 
 import (
+	"context"
 	"path"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/NibiruChain/nibiru/x/common/set"
 	"github.com/NibiruChain/nibiru/x/common/testutil"
 )
 
@@ -22,4 +26,33 @@ func (s *TestSuite) TestGetPackageDir() {
 	s.NoError(err)
 	s.Equal("testutil", path.Base(pkgDir))
 	s.Equal("common", path.Base(path.Dir(pkgDir)))
+}
+
+// TestSampleFns: Tests functions that generate test data from sample.go
+func (s *TestSuite) TestSampleFns() {
+	s.T().Log("consecutive calls give different addrs")
+	addrs := set.New[string]()
+	for times := 0; times < 16; times++ {
+		newAddr := testutil.AccAddress().String()
+		s.False(addrs.Has(newAddr))
+		addrs.Add(newAddr)
+	}
+}
+
+func (s *TestSuite) TestPrivKeyAddressPairs() {
+	s.T().Log("calls should be deterministic")
+	keysA, addrsA := testutil.PrivKeyAddressPairs(4)
+	keysB, addrsB := testutil.PrivKeyAddressPairs(4)
+	s.Equal(keysA, keysB)
+	s.Equal(addrsA, addrsB)
+}
+
+func (s *TestSuite) TestBlankContext() {
+	ctx := testutil.BlankContext("new-kv-store-key")
+	goCtx := sdk.WrapSDKContext(ctx)
+
+	freshGoCtx := context.Background()
+	s.Require().Panics(func() { sdk.UnwrapSDKContext(freshGoCtx) })
+
+	s.Require().NotPanics(func() { sdk.UnwrapSDKContext(goCtx) })
 }
