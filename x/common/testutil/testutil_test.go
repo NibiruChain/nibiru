811122b4f883c5e8eec4a99b5ddb16aff3de1ee3
Unique-Divine <realuniquedivine@gmail.com>
2023-10-18 00:57:07 -0500

    more tests

--- x/common/testutil/testutil_test.go
+++ x/common/testutil/testutil_test.go
@@ -2,9 +2,11 @@ package testutil_test
 
 import (
 	"context"
+	"os/exec"
 	"path"
 	"testing"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/suite"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -55,4 +57,70 @@ func (s *TestSuite) TestBlankContext() {
 	s.Require().Panics(func() { sdk.UnwrapSDKContext(freshGoCtx) })
 
 	s.Require().NotPanics(func() { sdk.UnwrapSDKContext(goCtx) })
+}
+
+func (s *TestSuite) TestNullifyFill() {
+	for _, tc := range []struct {
+		name  string
+		input any
+		want  any
+	}{
+		{
+			name:  "nullify fill slice",
+			input: []string{},
+			want:  make([]string, 0),
+		},
+		{
+			name: "nullify fill struct with coins",
+			input: struct {
+				Coins   sdk.Coins
+				Strings []string
+			}{},
+			want: struct {
+				Coins   sdk.Coins
+				Strings []string
+			}{
+				Coins:   sdk.Coins(nil),
+				Strings: []string(nil),
+			},
+		},
+		{
+			name: "nullify fill sdk.Coin struct",
+			input: struct {
+				Coin sdk.Coin
+				Ints []int
+			}{},
+			want: struct {
+				Coin sdk.Coin
+				Ints []int
+			}{
+				Coin: sdk.Coin{},
+				Ints: []int(nil),
+			},
+		},
+		{
+			name:  "nullify fill pointer to null concrete",
+			input: new(sdk.Coin),
+			want:  sdk.Coin{},
+		},
+	} {
+		s.Run(tc.name, func() {
+			got := testutil.Fill(tc.input)
+			s.EqualValues(tc.want, got)
+		})
+	}
+}
+
+func (s *TestSuite) TestSetupClientCtx() {
+	goCtx := testutil.SetupClientCtx(s.T())
+	trivialCobraCommand := &cobra.Command{
+		Use:   "run-true",
+		Short: "Runs the Unix command, 'true'",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return exec.Command("true").Run()
+		},
+	}
+
+	err := trivialCobraCommand.ExecuteContext(goCtx)
+	s.NoError(err)
 }
