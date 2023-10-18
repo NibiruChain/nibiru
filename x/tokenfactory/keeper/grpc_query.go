d0f529074f3adc0d05ad611860b06fd4fd01d935
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2023-10-18 15:00:58 -0500

    test(tokenfactory)!: integration test core logic with a real smart contract using `nibiru-std` (#1638)
    * test(tokenfactory): [epic] messy but working first version #wip
    
    - Add cosmwasm_1_2 feature to app/keepers.go
    - Add smart contract as test fixture
    - Test CosmosMsg::Stargate happy paths using raw json inputs
    
    * changelog + linter
    
    * linter
    
    * test: add another serde test case and remove DEBUG statements
    
    * feat: add all features from wasmd to app + more tests
    
    * more tests
    
    * test: patch coverage

--- x/tokenfactory/keeper/grpc_query.go
+++ x/tokenfactory/keeper/grpc_query.go
@@ -31,6 +31,12 @@ func (q Querier) Params(
 	return &types.QueryParamsResponse{Params: params}, nil
 }
 
+// QueryDenoms: Returns all registered denoms for a given creator.
+func (k Keeper) QueryDenoms(ctx sdk.Context, creator string) []string {
+	iter := k.Store.Denoms.Indexes.Creator.ExactMatch(ctx, creator)
+	return iter.PrimaryKeys()
+}
+
 // Denoms: Returns all registered denoms for a given creator.
 func (q Querier) Denoms(
 	goCtx context.Context,
@@ -44,13 +50,28 @@ func (q Querier) Denoms(
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	iter := q.Keeper.Store.Denoms.Indexes.Creator.ExactMatch(ctx, req.Creator)
 	return &types.QueryDenomsResponse{
-		Denoms: iter.PrimaryKeys(),
+		Denoms: q.Keeper.QueryDenoms(ctx, req.Creator),
 	}, err
 }
 
-// DenomInfo: Returns all registered denoms for a given creator.
+// QueryDenomInfo: Returns bank and tokenfactory metadata for a registered denom.
+func (k Keeper) QueryDenomInfo(
+	ctx sdk.Context, denom string,
+) (resp *types.QueryDenomInfoResponse, err error) {
+	tfMetadata, err := k.Store.denomAdmins.Get(ctx, denom)
+	if err != nil {
+		return resp, err
+	}
+
+	bankMetadata, _ := k.bankKeeper.GetDenomMetaData(ctx, denom)
+	return &types.QueryDenomInfoResponse{
+		Admin:    tfMetadata.Admin,
+		Metadata: bankMetadata,
+	}, err
+}
+
+// DenomInfo: Returns bank and tokenfactory metadata for a registered denom.
 func (q Querier) DenomInfo(
 	goCtx context.Context,
 	req *types.QueryDenomInfoRequest,
@@ -63,14 +84,5 @@ func (q Querier) DenomInfo(
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	tfMetadata, err := q.Keeper.Store.denomAdmins.Get(ctx, req.Denom)
-	if err != nil {
-		return resp, err
-	}
-
-	bankMetadata, _ := q.Keeper.bankKeeper.GetDenomMetaData(ctx, req.Denom)
-	return &types.QueryDenomInfoResponse{
-		Admin:    tfMetadata.Admin,
-		Metadata: bankMetadata,
-	}, err
+	return q.Keeper.QueryDenomInfo(ctx, req.Denom)
 }
