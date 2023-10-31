042e3699835d4193185aea7fafe8580b7e134b73
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-10-31 17:21:46 -0500

    fix(keeper): inflation keeper instantiation

--- app/keepers.go
+++ app/keepers.go
@@ -345,11 +345,6 @@ func (app *NibiruApp) InitKeepers(
 		appCodec, keys[epochstypes.StoreKey],
 	)
 
-	app.InflationKeeper = inflationkeeper.NewKeeper(
-		appCodec, keys[inflationtypes.StoreKey], app.GetSubspace(inflationtypes.ModuleName),
-		app.AccountKeeper, app.BankKeeper, app.DistrKeeper, app.stakingKeeper, authtypes.FeeCollectorName,
-	)
-
 	app.SudoKeeper = keeper.NewKeeper(
 		appCodec, keys[sudotypes.StoreKey],
 	)
