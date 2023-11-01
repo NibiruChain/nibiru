162fd05ffb8b631f32b65bc322d92c38cdbe8740
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2023-11-01 12:43:21 -0500

    test: refactors cli.network suites with 'Integration' to use common function (#1652)
    * test: refactors cli.network suites with 'Integration' to use common function
    
    * change log
    
    * linter
    
    * test fix to use correct bech32 prefix
    
    * test: make it so that simulations don't need to be fitered for explicitly
    
    * test(simapp): flag retrieval must be inside the  fn
    
    * typo: Update cases.go
    
    * test(cases.go)

--- x/common/testutil/cases.go
+++ x/common/testutil/cases.go
@@ -18,3 +18,19 @@ func RunFunctionTests(t *testing.T, testCases []FunctionTestCase) {
 		})
 	}
 }
+
+/*
+BeforeIntegrationSuite: Skips a test if the `-short` flag is used:
+
+All tests: `go test ./...`
+Unit tests only: `go test ./... -short`
+Integration tests only: `go test ./... -run Integration`
+
+See: https://stackoverflow.com/a/41407042/13305627
+*/
+func BeforeIntegrationSuite(suiteT *testing.T) {
+	if testing.Short() {
+		suiteT.Skip("skipping integration test suite")
+	}
+	suiteT.Log("setting up integration test suite")
+}
