674cace1492f324255575af1d4dd1f65317d14d6
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2023-12-03 12:41:45 -0600

    feat(perp): MsgShiftPegMultiplier, MsgShiftSwapInvariant (#1680)
    * chore(deps): Bump cosmossdk.io/math from 1.1.2 to 1.2.0
    
    Bumps [cosmossdk.io/math](https://github.com/cosmos/cosmos-sdk) from 1.1.2 to 1.2.0.
    - [Release notes](https://github.com/cosmos/cosmos-sdk/releases)
    - [Changelog](https://github.com/cosmos/cosmos-sdk/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/cosmos/cosmos-sdk/compare/math/v1.1.2...log/v1.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: cosmossdk.io/math
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat(perp): msg server methods for shift peg and shift swap invariant
    
    * (wasmbinding): remove pegshitft and depth shift
    
    * test(perp): msg tests
    
    * change log
    
    * Update x/perp/v2/types/msgs.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * PR comments changes
    
    * add events for logging purposes
    
    * refactor: consistent error name usage PR comment
    
    * refactor: more PR comments
    
    * Address Gimeno PR comments + more tests
    
    * fix strange address name conflicts
    
    * test: fix all tests
    
    * test: more calls of EnsureNibiruPrefix
    
    * Updated changelog - dependabot
    
    * test: fix no error cases in tests
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Unique-Divine <Unique-Divine@users.noreply.github.com>

--- x/perp/v2/keeper/admin_test.go
+++ x/perp/v2/keeper/admin_test.go
@@ -1,21 +1,24 @@
 package keeper_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/NibiruChain/nibiru/app"
 	"github.com/NibiruChain/nibiru/x/common/asset"
 	"github.com/NibiruChain/nibiru/x/common/denoms"
 	"github.com/NibiruChain/nibiru/x/common/testutil"
+	"github.com/NibiruChain/nibiru/x/common/testutil/genesis"
 	"github.com/NibiruChain/nibiru/x/common/testutil/mock"
 	"github.com/NibiruChain/nibiru/x/common/testutil/testapp"
 	"github.com/NibiruChain/nibiru/x/perp/v2/keeper"
-	"github.com/NibiruChain/nibiru/x/perp/v2/types"
+	perptypes "github.com/NibiruChain/nibiru/x/perp/v2/types"
 	sudotypes "github.com/NibiruChain/nibiru/x/sudo/types"
 	tftypes "github.com/NibiruChain/nibiru/x/tokenfactory/types"
 
@@ -28,9 +31,9 @@ func TestAdmin_WithdrawFromInsuranceFund(t *testing.T) {
 	expectBalance := func(
 		want sdkmath.Int, t *testing.T, nibiru *app.NibiruApp, ctx sdk.Context,
 	) {
-		insuranceFund := nibiru.AccountKeeper.GetModuleAddress(types.PerpEFModuleAccount)
+		insuranceFund := nibiru.AccountKeeper.GetModuleAddress(perptypes.PerpEFModuleAccount)
 		balances := nibiru.BankKeeper.GetAllBalances(ctx, insuranceFund)
-		got := balances.AmountOf(types.TestingCollateralDenomNUSD)
+		got := balances.AmountOf(perptypes.TestingCollateralDenomNUSD)
 		require.EqualValues(t, want.String(), got.String())
 	}
 
@@ -38,14 +41,14 @@ func TestAdmin_WithdrawFromInsuranceFund(t *testing.T) {
 		testapp.EnsureNibiruPrefix()
 		nibiru, ctx = testapp.NewNibiruTestAppAndContext()
 		expectBalance(sdk.ZeroInt(), t, nibiru, ctx)
-		nibiru.PerpKeeperV2.Collateral.Set(ctx, types.TestingCollateralDenomNUSD)
+		nibiru.PerpKeeperV2.Collateral.Set(ctx, perptypes.TestingCollateralDenomNUSD)
 		return nibiru, ctx
 	}
 
 	fundModule := func(t *testing.T, amount sdkmath.Int, ctx sdk.Context, nibiru *app.NibiruApp) {
-		coins := sdk.NewCoins(sdk.NewCoin(types.TestingCollateralDenomNUSD, amount))
+		coins := sdk.NewCoins(sdk.NewCoin(perptypes.TestingCollateralDenomNUSD, amount))
 		err := testapp.FundModuleAccount(
-			nibiru.BankKeeper, ctx, types.PerpEFModuleAccount,
+			nibiru.BankKeeper, ctx, perptypes.PerpEFModuleAccount,
 			coins,
 		)
 		require.NoError(t, err)
@@ -67,7 +70,7 @@ func TestAdmin_WithdrawFromInsuranceFund(t *testing.T) {
 
 				require.EqualValues(t,
 					amountToFund.String(),
-					nibiru.BankKeeper.GetBalance(ctx, admin, types.TestingCollateralDenomNUSD).Amount.String(),
+					nibiru.BankKeeper.GetBalance(ctx, admin, perptypes.TestingCollateralDenomNUSD).Amount.String(),
 				)
 				expectBalance(sdk.ZeroInt(), t, nibiru, ctx)
 			},
@@ -98,7 +101,7 @@ func TestCreateMarket(t *testing.T) {
 	admin := app.PerpKeeperV2.Admin
 
 	// Error because of invalid market
-	market := types.DefaultMarket(pair).WithMaintenanceMarginRatio(sdk.NewDec(2))
+	market := perptypes.DefaultMarket(pair).WithMaintenanceMarginRatio(sdk.NewDec(2))
 	err := admin.CreateMarket(ctx, keeper.ArgsCreateMarket{
 		Pair:            pair,
 		PriceMultiplier: amm.PriceMultiplier,
@@ -205,7 +208,7 @@ func TestCloseMarket(t *testing.T) {
 				SetBlockNumber(1),
 				SetBlockTime(startTime),
 
-				FundAccount(alice, sdk.NewCoins(sdk.NewCoin(types.TestingCollateralDenomNUSD, sdk.NewInt(1e6)))),
+				FundAccount(alice, sdk.NewCoins(sdk.NewCoin(perptypes.TestingCollateralDenomNUSD, sdk.NewInt(1e6)))),
 			).
 			When(
 				CloseMarket(pairBtcUsdc),
@@ -214,11 +217,11 @@ func TestCloseMarket(t *testing.T) {
 				MarketOrderFails(
 					alice,
 					pairBtcUsdc,
-					types.Direction_LONG,
+					perptypes.Direction_LONG,
 					sdk.NewInt(10_000),
 					sdk.OneDec(),
 					sdk.ZeroDec(),
-					types.ErrMarketNotEnabled,
+					perptypes.ErrMarketNotEnabled,
 				),
 			),
 		TC("cannot close position on disabled market").When(
@@ -230,11 +233,11 @@ func TestCloseMarket(t *testing.T) {
 			),
 			SetBlockNumber(1),
 			SetBlockTime(startTime),
-			FundAccount(alice, sdk.NewCoins(sdk.NewCoin(types.TestingCollateralDenomNUSD, sdk.NewInt(10_200)))),
+			FundAccount(alice, sdk.NewCoins(sdk.NewCoin(perptypes.TestingCollateralDenomNUSD, sdk.NewInt(10_200)))),
 			MarketOrder(
 				alice,
 				pairBtcUsdc,
-				types.Direction_LONG,
+				perptypes.Direction_LONG,
 				sdk.NewInt(10_000),
 				sdk.OneDec(),
 				sdk.ZeroDec(),
@@ -244,7 +247,7 @@ func TestCloseMarket(t *testing.T) {
 			CloseMarketShouldFail(pairBtcUsdc),
 			CloseMarketShouldFail("random:pair"),
 		).Then(
-			ClosePositionFails(alice, pairBtcUsdc, types.ErrMarketNotEnabled),
+			ClosePositionFails(alice, pairBtcUsdc, perptypes.ErrMarketNotEnabled),
 		),
 		TC("cannot partial close position on disabled market").When(
 			CreateCustomMarket(
@@ -255,11 +258,11 @@ func TestCloseMarket(t *testing.T) {
 			),
 			SetBlockNumber(1),
 			SetBlockTime(startTime),
-			FundAccount(alice, sdk.NewCoins(sdk.NewCoin(types.TestingCollateralDenomNUSD, sdk.NewInt(10_200)))),
+			FundAccount(alice, sdk.NewCoins(sdk.NewCoin(perptypes.TestingCollateralDenomNUSD, sdk.NewInt(10_200)))),
 			MarketOrder(
 				alice,
 				pairBtcUsdc,
-				types.Direction_LONG,
+				perptypes.Direction_LONG,
 				sdk.NewInt(10_000),
 				sdk.OneDec(),
 				sdk.ZeroDec(),
@@ -268,7 +271,7 @@ func TestCloseMarket(t *testing.T) {
 			CloseMarket(pairBtcUsdc),
 			AMMShouldBeEqual(pairBtcUsdc, AMM_SettlementPriceShoulBeEqual(sdk.MustNewDecFromStr("1.1"))),
 		).Then(
-			PartialCloseFails(alice, pairBtcUsdc, sdk.NewDec(5_000), types.ErrMarketNotEnabled),
+			PartialCloseFails(alice, pairBtcUsdc, sdk.NewDec(5_000), perptypes.ErrMarketNotEnabled),
 		),
 	}
 
@@ -320,7 +323,7 @@ func TestAdmin_ChangeCollateralDenom(t *testing.T) {
 
 		{
 			name:     "sad: invalid denom",
-			newDenom: "", sender: adminSender, wantErr: types.ErrInvalidCollateral.Error(),
+			newDenom: "", sender: adminSender, wantErr: perptypes.ErrInvalidCollateral.Error(),
 		},
 		{
 			name:     "sad: sender not in sudoers",
@@ -343,5 +346,137 @@ func TestAdmin_ChangeCollateralDenom(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, tc.newDenom, newDenom)
 		})
+	}
+}
+
+type TestSuiteAdmin struct {
+	suite.Suite
+
+	nibiru        *app.NibiruApp
+	ctx           sdk.Context
+	addrAdmin     sdk.AccAddress
+	perpKeeper    keeper.Keeper
+	perpMsgServer perptypes.MsgServer
+}
+
+var _ suite.SetupTestSuite = (*TestSuiteAdmin)(nil)
+
+func TestSuite_Admin_RunAll(t *testing.T) {
+	suite.Run(t, new(TestSuiteAdmin))
+}
+
+// SetupTest: Runs before every test in the suite.
+func (s *TestSuiteAdmin) SetupTest() {
+	s.addrAdmin = testutil.AccAddress()
+	genesisState := genesis.NewTestGenesisState(app.MakeEncodingConfig())
+	genesisState = genesis.AddPerpV2Genesis(genesisState)
+	nibiru := testapp.NewNibiruTestApp(genesisState)
+	ctx := testapp.NewContext(nibiru)
+	nibiru.SudoKeeper.Sudoers.Set(ctx, sudotypes.Sudoers{
+		Root:      "mock-root", // unused
+		Contracts: []string{s.addrAdmin.String()},
+	})
+	s.nibiru = nibiru
+	s.ctx = ctx
+	s.perpKeeper = s.nibiru.PerpKeeperV2
+	s.perpMsgServer = keeper.NewMsgServerImpl(s.perpKeeper)
+	s.nibiru.PerpKeeperV2.Collateral.Set(s.ctx, perptypes.TestingCollateralDenomNUSD)
+
+	sender := s.addrAdmin
+	coins := sdk.NewCoins(
+		sdk.NewCoin(denoms.NIBI, sdk.NewInt(1_000_000)),
+		sdk.NewCoin(perptypes.TestingCollateralDenomNUSD, sdk.NewInt(420_000*69)),
+		sdk.NewCoin(denoms.USDT, sdk.NewInt(420_000*69)),
+	)
+	s.NoError(testapp.FundAccount(nibiru.BankKeeper, ctx, sender, coins))
+}
+
+// HandleMsg: Convenience Executes a tx msg from the MsgServer
+func (s *TestSuiteAdmin) HandleMsg(txMsg sdk.Msg) (err error) {
+	ctx := s.ctx
+	switch msg := txMsg.(type) {
+	case *perptypes.MsgShiftPegMultiplier:
+		_, err = s.perpMsgServer.ShiftPegMultiplier(ctx, msg)
+	case *perptypes.MsgShiftSwapInvariant:
+		_, err = s.perpMsgServer.ShiftSwapInvariant(ctx, msg)
+	case *perptypes.MsgChangeCollateralDenom:
+		_, err = s.perpMsgServer.ChangeCollateralDenom(ctx, msg)
+	default:
+		return fmt.Errorf("unexpected message of type %T encountered", msg)
+	}
+	return err
+}
+
+// TestCheckPermissions: Verify that all of the expected admin functions fail
+// when called without sudo permissions.
+func (s *TestSuiteAdmin) TestCheckPermissions() {
+	// Sender should not be equal to the admin.
+	senderAddr := testutil.AccAddress()
+	if senderAddr.Equals(s.addrAdmin) {
+		senderAddr = testutil.AccAddress()
+	}
+	sender := senderAddr.String()
+
+	for _, testCaseMsg := range []sdk.Msg{
+		&perptypes.MsgShiftPegMultiplier{
+			Sender: sender, Pair: asset.Pair("valid:pair"), NewPegMult: sdk.NewDec(420),
+		},
+		&perptypes.MsgShiftSwapInvariant{
+			Sender: sender, Pair: asset.Pair("valid:pair"), NewSwapInvariant: sdk.NewInt(420),
+		},
+		&perptypes.MsgChangeCollateralDenom{
+			Sender: sender, NewDenom: "newdenom",
+		},
+	} {
+		s.Run(fmt.Sprintf("%T", testCaseMsg), func() {
+			err := s.HandleMsg(testCaseMsg)
+			s.ErrorContains(err, sudotypes.ErrUnauthorized.Error())
+		})
+	}
+}
+
+func (s *TestSuiteAdmin) DoShiftPegTest(pair asset.Pair) error {
+	_, err := s.perpMsgServer.ShiftPegMultiplier(
+		sdk.WrapSDKContext(s.ctx), &perptypes.MsgShiftPegMultiplier{
+			Sender:     s.addrAdmin.String(),
+			Pair:       pair,
+			NewPegMult: sdk.NewDec(420),
+		},
+	)
+	return err
+}
+
+func (s *TestSuiteAdmin) DoShiftSwapInvariantTest(pair asset.Pair) error {
+	_, err := s.perpMsgServer.ShiftSwapInvariant(
+		sdk.WrapSDKContext(s.ctx), &perptypes.MsgShiftSwapInvariant{
+			Sender:           s.addrAdmin.String(),
+			Pair:             pair,
+			NewSwapInvariant: sdk.NewInt(420),
+		},
+	)
+	return err
+}
+
+// TestAdmin_DoHappy: Happy path test cases
+func (s *TestSuiteAdmin) TestAdmin_DoHappy() {
+	pair := asset.Registry.Pair(denoms.ATOM, denoms.NUSD)
+
+	for _, err := range []error{
+		s.DoShiftPegTest(pair),
+		s.DoShiftSwapInvariantTest(pair),
+	} {
+		s.NoError(err)
+	}
+}
+
+// TestAdmin_SadPathsInvalidPair: Test scenarios that fail due to the use of a
+// market that doesn't exist.
+func (s *TestSuiteAdmin) TestAdmin_SadPathsInvalidPair() {
+	pair := asset.Pair("ftt:ust:doge")
+	for _, err := range []error{
+		s.DoShiftPegTest(pair),
+		s.DoShiftSwapInvariantTest(pair),
+	} {
+		s.Error(err)
 	}
 }
--- x/perp/v2/keeper/msg_server.go
+++ x/perp/v2/keeper/msg_server.go
@@ -157,15 +157,15 @@ func (m msgServer) ChangeCollateralDenom(
 	return &types.MsgChangeCollateralDenomResponse{}, err
 }
 
-func (m msgServer) AllocateEpochRebates(ctx context.Context, msg *types.MsgAllocateEpochRebates) (*types.MsgAllocateEpochRebatesResponse, error) {
+func (m msgServer) AllocateEpochRebates(
+	ctx context.Context, msg *types.MsgAllocateEpochRebates,
+) (*types.MsgAllocateEpochRebatesResponse, error) {
 	if msg == nil {
 		return nil, common.ErrNilMsg()
 	}
 
-	sender, err := sdk.AccAddressFromBech32(msg.Sender)
-	if err != nil {
-		return nil, err
-	}
+	// Sender is checked in `msg.ValidateBasic` before reaching this fn call.
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
 	total, err := m.k.AllocateEpochRebates(sdk.UnwrapSDKContext(ctx), sender, msg.Rebates)
 	if err != nil {
 		return nil, err
@@ -178,11 +178,8 @@ func (m msgServer) WithdrawEpochRebates(ctx context.Context, msg *types.MsgWithd
 	if msg == nil {
 		return nil, common.ErrNilMsg()
 	}
-
-	sender, err := sdk.AccAddressFromBech32(msg.Sender)
-	if err != nil {
-		return nil, err
-	}
+	// Sender is checked in `msg.ValidateBasic` before reaching this fn call.
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	totalWithdrawn := sdk.NewCoins()
 	for _, epoch := range msg.Epochs {
@@ -195,4 +192,28 @@ func (m msgServer) WithdrawEpochRebates(ctx context.Context, msg *types.MsgWithd
 	return &types.MsgWithdrawEpochRebatesResponse{
 		WithdrawnRebates: totalWithdrawn,
 	}, nil
+}
+
+// ShiftPegMultiplier: gRPC tx msg for changing a market's peg multiplier.
+// [Admin] Only callable by sudoers.
+func (m msgServer) ShiftPegMultiplier(
+	goCtx context.Context, msg *types.MsgShiftPegMultiplier,
+) (*types.MsgShiftPegMultiplierResponse, error) {
+	// Sender is checked in `msg.ValidateBasic` before reaching this fn call.
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	err := m.k.Admin.ShiftPegMultiplier(ctx, msg.Pair, msg.NewPegMult, sender)
+	return &types.MsgShiftPegMultiplierResponse{}, err
+}
+
+// ShiftSwapInvariant: gRPC tx msg for changing a market's swap invariant.
+// [Admin] Only callable by sudoers.
+func (m msgServer) ShiftSwapInvariant(
+	goCtx context.Context, msg *types.MsgShiftSwapInvariant,
+) (*types.MsgShiftSwapInvariantResponse, error) {
+	// Sender is checked in `msg.ValidateBasic` before reaching this fn call.
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	err := m.k.Admin.ShiftSwapInvariant(ctx, msg.Pair, msg.NewSwapInvariant, sender)
+	return &types.MsgShiftSwapInvariantResponse{}, err
 }
