e6fb0e2895e384c4d91c431baeee4cbf83ac9bac
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-12-08 11:56:35 -0600

    ci: update goreleaser workflow

--- contrib/scripts/release_pre_darwin.sh
+++ contrib/scripts/release_pre_darwin.sh
@@ -10,9 +10,13 @@
 # ```
 set -e
 
+# libwasmvm
 WASMVM_VERSION=$(go list -m github.com/CosmWasm/wasmvm | awk '{sub(/^v/, "", $2); print $2}')
 wget https://github.com/CosmWasm/wasmvm/releases/download/v${WASMVM_VERSION}/libwasmvmstatic_darwin.a -O /usr/local/osxcross/SDK/MacOSX12.0.sdk/usr/lib/libwasmvmstatic_darwin.a
-wget https://github.com/NibiruChain/gorocksdb/releases/download/v8.5.3/include.8.5.3.tar.gz -O /root/include.8.5.3.tar.gz
-tar -xf /root/include.8.5.3.tar.gz -C /usr/local/osxcross/SDK/MacOSX12.0.sdk/usr/include/
-wget https://github.com/NibiruChain/gorocksdb/releases/download/v8.5.3/librocksdb_8.5.3_darwin_all.tar.gz -O /root/librocksdb_8.5.3_darwin_all.tar.gz
-tar -xf /root/librocksdb_8.5.3_darwin_all.tar.gz -C /usr/local/osxcross/SDK/MacOSX12.0.sdk/usr/lib/
+
+# librocksdb
+ROCKSDB_VERSION=8.8.1
+wget https://github.com/NibiruChain/gorocksdb/releases/download/v${ROCKSDB_VERSION}/include.${ROCKSDB_VERSION}.tar.gz -O /root/include.${ROCKSDB_VERSION}.tar.gz
+tar -xf /root/include.${ROCKSDB_VERSION}.tar.gz -C /usr/local/osxcross/SDK/MacOSX12.0.sdk/usr/include/
+wget https://github.com/NibiruChain/gorocksdb/releases/download/v${ROCKSDB_VERSION}/librocksdb_${ROCKSDB_VERSION}_darwin_all.tar.gz -O /root/librocksdb_${ROCKSDB_VERSION}_darwin_all.tar.gz
+tar -xf /root/librocksdb_${ROCKSDB_VERSION}_darwin_all.tar.gz -C /usr/local/osxcross/SDK/MacOSX12.0.sdk/usr/lib/
--- contrib/scripts/release_pre_linux.sh
+++ contrib/scripts/release_pre_linux.sh
@@ -10,12 +10,23 @@
 # ```
 set -e
 
+# libwasmvm_muslc
 WASMVM_VERSION=$(go list -m github.com/CosmWasm/wasmvm | awk '{sub(/^v/, "", $2); print $2}')
+
 wget https://github.com/CosmWasm/wasmvm/releases/download/v${WASMVM_VERSION}/libwasmvm_muslc.x86_64.a -O /usr/lib/x86_64-linux-gnu/libwasmvm_muslc.a
 wget https://github.com/CosmWasm/wasmvm/releases/download/v${WASMVM_VERSION}/libwasmvm_muslc.aarch64.a -O /usr/lib/aarch64-linux-gnu/libwasmvm_muslc.a
-wget https://github.com/NibiruChain/gorocksdb/releases/download/v8.5.3/include.8.5.3.tar.gz -O /root/include.8.5.3.tar.gz
-tar -xvf /root/include.8.5.3.tar.gz -C /usr/include/
-wget https://github.com/NibiruChain/gorocksdb/releases/download/v8.5.3/librocksdb_8.5.3_linux_amd64.tar.gz -O /root/librocksdb_8.5.3_linux_amd64.tar.gz
-tar -xvf /root/librocksdb_8.5.3_linux_amd64.tar.gz -C /usr/lib/x86_64-linux-gnu/
-wget https://github.com/NibiruChain/gorocksdb/releases/download/v8.5.3/librocksdb_8.5.3_linux_arm64.tar.gz -O /root/librocksdb_8.5.3_linux_arm64.tar.gz
-tar -xvf /root/librocksdb_8.5.3_linux_arm64.tar.gz -C /usr/lib/aarch64-linux-gnu/
+
+# librocksdb
+ROCKSDB_VERSION=8.8.1
+wget https://github.com/NibiruChain/gorocksdb/releases/download/v${ROCKSDB_VERSION}/include.${ROCKSDB_VERSION}.tar.gz -O /root/include.${ROCKSDB_VERSION}.tar.gz
+tar -xvf /root/include.${ROCKSDB_VERSION}.tar.gz -C /usr/include/
+
+wget https://github.com/NibiruChain/gorocksdb/releases/download/v${ROCKSDB_VERSION}/librocksdb_${ROCKSDB_VERSION}_linux_amd64.tar.gz -O /root/librocksdb_${ROCKSDB_VERSION}_linux_amd64.tar.gz
+tar -xvf /root/librocksdb_${ROCKSDB_VERSION}_linux_amd64.tar.gz -C /usr/lib/x86_64-linux-gnu/
+
+wget https://github.com/NibiruChain/gorocksdb/releases/download/v${ROCKSDB_VERSION}/librocksdb_${ROCKSDB_VERSION}_linux_arm64.tar.gz -O /root/librocksdb_${ROCKSDB_VERSION}_linux_arm64.tar.gz
+tar -xvf /root/librocksdb_${ROCKSDB_VERSION}_linux_arm64.tar.gz -C /usr/lib/aarch64-linux-gnu/
+
+# compression libraries
+apt-get update
+apt-get install -y git make build-essential libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
