776a4af5a9653971f1d4eebbca6d40375ce840a4
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-12-13 17:02:09 -0500

    fix: upgrade handler for v1.1.0 (#1706)
    * fix: upgrade handler for v1.1.0
    
    * chore: update changelog

--- app/upgrades/v1_1_0/constants.go
+++ app/upgrades/v1_1_0/constants.go
@@ -2,6 +2,8 @@ package v1_1_0
 
 import (
 	"github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/NibiruChain/nibiru/app/upgrades"
@@ -12,8 +14,10 @@ const UpgradeName = "v1.1.0"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: UpgradeName,
-	CreateUpgradeHandler: func() upgradetypes.UpgradeHandler {
-		return nil
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
+		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return mm.RunMigrations(ctx, cfg, fromVM)
+		}
 	},
 	StoreUpgrades: types.StoreUpgrades{
 		Added: []string{inflationtypes.ModuleName},
