39396608bee4374fb77ab994875a260c2eac4d0a
matthiasmatt <matthias@nibiru.org>
2023-12-13 10:50:15 +0100

    fix: bind oracle pair for wasmbinding

--- wasmbinding/exec_perp.go
+++ wasmbinding/exec_perp.go
@@ -86,7 +86,7 @@ func (exec *ExecutorPerp) CreateMarket(
 			MaxFundingRate:                  mp.MaxFundingRate,
 			TwapLookbackWindow:              time.Duration(mp.TwapLookbackWindow.Int64()),
 			PrepaidBadDebt:                  sdk.NewCoin(pair.QuoteDenom(), sdk.ZeroInt()),
-			OraclePair:                      pair,
+			OraclePair:                      asset.MustNewPair(mp.OraclePair),
 		}
 	}
 
