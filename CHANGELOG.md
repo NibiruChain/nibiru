dd33d2a6584f875aa013344491f03952e0fc37d3
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-12-14 08:06:07 -0500

    [merge] ci: upgrade librocksdb to v8.8.1 (#1699)
    * feat: bump librocksdb to v8.5.3
    * Update CHANGELOG.md
    * ci: add goreleaser-latest workflow
    * ci: update goreleaser workflow
    * fix: goreleaser config
    * Update CHANGELOG.md
    * goreleaser: fix builds and optimize pre hook scripts
    
    ---------
    
    Co-authored-by: Helder M <helder.moreira@voluzi.com>
    Co-authored-by: Helder Moreira <helder-moreira@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -82,6 +82,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Bump `actions/setup-go` from 4 to 5 ([#1696](https://github.com/NibiruChain/nibiru/pull/1696))
 * Bump `golang` from 1.19 to 1.21 ([#1698](https://github.com/NibiruChain/nibiru/pull/1698))
 * [#1678](https://github.com/NibiruChain/nibiru/pull/1678) - chore(deps): collections to v0.4.0 for math.Int value encoder
+* Bump `rocksdb` from 8.1.1 to 8.8.1
+
 
 ## [v1.1.0] - 2023-11-20
 
--- contrib/make/build.mk
+++ contrib/make/build.mk
@@ -29,7 +29,7 @@ SDK_PACK := $(shell go list -m github.com/cosmos/cosmos-sdk | sed  's/ /\@/g')
 # TM_VERSION: Tendermint Core version (CometBFT)
 # grab everything after the space in "github.com/tendermint/tendermint v0.34.7"
 TM_VERSION := $(shell go list -m github.com/cometbft/cometbft | sed 's:.* ::') 
-ROCKSDB_VERSION := 8.1.1
+ROCKSDB_VERSION := 8.8.1
 WASMVM_VERSION := $(shell go list -m github.com/CosmWasm/wasmvm | awk '{sub(/^v/, "", $$2); print $$2}')
 DOCKER := $(shell which docker)
 BUILDDIR ?= $(CURDIR)/build
