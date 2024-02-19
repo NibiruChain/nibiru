1143611d4858948b090e9f7142622bc03f126a07
GnaD13 <cpt.meoz@gmail.com>
2024-02-19 11:56:05 +0700

    nit

--- x/tokenfactory/simulation/operations.go
+++ x/tokenfactory/simulation/operations.go
@@ -18,9 +18,6 @@ import (
 
 // Simulation parameter constants
 const (
-	StakePerAccount           = "stake_per_account"
-	InitiallyBondedValidators = "initially_bonded_validators"
-
 	DefaultWeightMsgCreateDenom      int = 100
 	DefaultWeightMsgMint             int = 100
 	DefaultWeightMsgBurn             int = 100
