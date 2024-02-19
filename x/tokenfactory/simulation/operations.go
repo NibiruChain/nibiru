f1b8b43d645ba1baeb49def2a79fb7f03d0ff7c4
GnaD13 <cpt.meoz@gmail.com>
2024-02-19 09:51:52 +0700

    add sims msg create denom

---
+++ x/tokenfactory/simulation/operations.go
@@ -0,0 +1,103 @@
+package simulation
+
+import (
+	"math/rand"
+
+	"github.com/NibiruChain/nibiru/x/tokenfactory/keeper"
+	"github.com/NibiruChain/nibiru/x/tokenfactory/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+)
+
+// Simulation parameter constants
+const (
+	StakePerAccount           = "stake_per_account"
+	InitiallyBondedValidators = "initially_bonded_validators"
+
+	DefaultWeightMsgCreateDenom      int = 100
+	DefaultWeightMsgMint             int = 100
+	DefaultWeightMsgBurn             int = 100
+	DefaultWeightMsgChangeAdmin      int = 100
+	DefaultWeightMsgSetDenomMetadata int = 100
+)
+
+// Simulation operation weights constants
+//
+//nolint:gosec
+const (
+	OpWeightMsgCreateDenom      = "op_weight_msg_create_denom"
+	OpWeightMsgChangeAdmin      = "op_weight_msg_change_admin"
+	OpWeightMsgMint             = "op_weight_msg_mint"
+	OpWeightMsgBurn             = "op_weight_msg_burn"
+	OpWeightMsgSetDenomMetadata = "op_weight_msg_set_denom_metadata"
+)
+
+type BankKeeper interface {
+	simulation.BankKeeper
+	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+}
+
+func WeightedOperations(
+	simstate *module.SimulationState,
+	tfKeeper keeper.Keeper,
+	ak types.AccountKeeper,
+	bk BankKeeper,
+) simulation.WeightedOperations {
+
+	var (
+		weightMsgCreateDenom int
+	)
+
+	simstate.AppParams.GetOrGenerate(simstate.Cdc, OpWeightMsgCreateDenom, &weightMsgCreateDenom, nil,
+		func(_ *rand.Rand) {
+			weightMsgCreateDenom = DefaultWeightMsgCreateDenom
+		},
+	)
+
+	return simulation.WeightedOperations{
+		simulation.NewWeightedOperation(
+			weightMsgCreateDenom,
+			SimulateMsgCreateDenom(
+				tfKeeper,
+				ak,
+				bk,
+			),
+		),
+	}
+}
+
+// Simulate msg create denom
+func SimulateMsgCreateDenom(tfKeeper keeper.Keeper, ak types.AccountKeeper, bk BankKeeper) simtypes.Operation {
+	return func(
+		r *rand.Rand,
+		app *baseapp.BaseApp,
+		ctx sdk.Context,
+		accs []simtypes.Account,
+		chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		// Get sims account
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+
+		// Check if sims account enough create fee
+		moduleParams, _ := tfKeeper.Store.ModuleParams.Get(ctx)
+		createFee := moduleParams.DenomCreationGasConsume
+		balances := bk.GetAllBalances(ctx, simAccount.Address)
+		_, hasNeg := balances.SafeSub(createFee)
+		if hasNeg {
+			return simtypes.NoOpMsg(types.ModuleName, types.MsgCreateDenom{}.Type(), "Creator not enough creation fee"), nil, nil
+		}
+
+		// Create msg create denom
+		msg := types.MsgCreateDenom{
+			Sender:   simAccount.Address.String(),
+			Subdenom: simtypes.RandStringOfLength(r, 10),
+		}
+
+		txCtx := BuildOperationInput(r, app, ctx, &msg, simAccount, ak, bk, createFee)
+		return simulation.GenAndDeliverTxWithRandFees(txCtx)
+	}
+}
