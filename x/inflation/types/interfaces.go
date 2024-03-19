4a13af2f9ac6ab50195644b2a7fbcaf6f0152ade
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-03-20 04:35:08 +0900

    feat(inflation): add burn method (#1823)
    * feat(inflation): add burn method
    
    * Update CHANGELOG.md

--- x/inflation/types/interfaces.go
+++ x/inflation/types/interfaces.go
@@ -21,6 +21,9 @@ type BankKeeper interface {
 	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(
+		ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins,
+	) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 	BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 	HasSupply(ctx sdk.Context, denom string) bool
