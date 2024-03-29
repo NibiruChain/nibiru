c7e901b011a08472b593c88543be201a3cd1d7c1
matthiasmatt <matthias@nibiru.org>
2024-03-29 13:18:54 +0100

    fix: fix justfile and deplyo-wasm

--- contrib/scripts/e2e/deploy-wasm.sh
+++ contrib/scripts/e2e/deploy-wasm.sh
@@ -4,19 +4,22 @@ set -e
 BINARY="nibid"
 DENOM="unibi"
 CHAIN_ID="nibiru-localnet-0"
-TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID"
+TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b async --chain-id $CHAIN_ID"
 
 # validator addr
 VALIDATOR_ADDR=$($BINARY keys show validator --address)
 echo "Validator address:"
 echo "$VALIDATOR_ADDR"
 
-BALANCE_1=$($BINARY q bank balances $VALIDATOR_ADDR)
+BALANCE_1=$($BINARY q bank balances "$VALIDATOR_ADDR")
 echo "Pre-store balance:"
 echo "$BALANCE_1"
 echo "TX Flags: $TXFLAG"
 
-CONTRACT_CODE=$($BINARY tx wasm store "./contrib/scripts/e2e/contracts/cw_nameservice.wasm" --from validator $TXFLAG --output json | jq -r '.logs[0].events[-1].attributes[-1].value')
+TX_HASH=$($BINARY tx wasm store "./contrib/scripts/e2e/contracts/cw_nameservice.wasm" --from validator $TXFLAG --output json | jq -r '.txhash' )
+sleep 3
+
+CONTRACT_CODE=$($BINARY q tx $TX_HASH --output json | jq -r '.logs[0].events[-1].attributes[-1].value')
 echo "Stored: $CONTRACT_CODE"
 
 BALANCE_2=$($BINARY q bank balances $VALIDATOR_ADDR)
@@ -25,6 +28,7 @@ echo "$BALANCE_2"
 
 INIT='{"purchase_price":{"amount":"100","denom":"unibi"},"transfer_price":{"amount":"999","denom":"unibi"}}'
 $BINARY tx wasm instantiate $CONTRACT_CODE "$INIT" --from validator $TXFLAG --label "awesome name service" --no-admin
+sleep 3
 
 CONTRACT_ADDRESS=$($BINARY query wasm list-contract-by-code $CONTRACT_CODE --output json | jq -r '.contracts[-1]')
 echo "Contract Address: $CONTRACT_ADDRESS"
@@ -33,6 +37,7 @@ $BINARY query wasm contract $CONTRACT_ADDRESS
 
 # purchase a domain name
 $BINARY tx wasm execute $CONTRACT_ADDRESS '{"register":{"name":"uniques-domain"}}' --amount 100$DENOM --from validator $TXFLAG -y
+sleep 3
 
 # query registered name
 NAME_QUERY='{"resolve_record": {"name": "uniques-domain"}}'
