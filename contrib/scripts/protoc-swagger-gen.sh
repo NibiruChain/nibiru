6aff6ad041cc79e4f63a2c5433097e1d44348e59
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-04-23 15:06:06 -0500

    fix: remove unnecessary file and import

--- contrib/scripts/protoc-swagger-gen.sh
+++ contrib/scripts/protoc-swagger-gen.sh
@@ -4,11 +4,12 @@ set -eo pipefail
 
 mkdir -p ./tmp-swagger-gen
 cd proto
-proto_dirs=$(find ./nibiru -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
+    # echo $query_file
     buf generate --template buf.gen.swagger.yaml $query_file
   fi
 done
