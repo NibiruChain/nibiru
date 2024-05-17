d955d1d5fbe8d6724e69d9a3be00cbf7fce19cf4
Unique-Divine <realuniquedivine@gmail.com>
2024-05-17 00:03:31 -0500

    feat(evm): MsgServer fns for MsgEthereumTx

---
+++ eth/gas_limit.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2023-2024 Nibi, Inc.
+package eth
+
+import (
+	math "math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// BlockGasLimit: Gas (limit) as defined by the block gas meter. Gas limit is
+// derived from the consensus params if the block gas meter is nil.
+func BlockGasLimit(ctx sdk.Context) (gasLimit uint64) {
+	blockGasMeter := ctx.BlockGasMeter()
+
+	// Get the limit from the gas meter only if its not null and not an InfiniteGasMeter
+	if blockGasMeter != nil && blockGasMeter.Limit() != 0 {
+		return blockGasMeter.Limit()
+	}
+
+	// Otherwise get from the consensus parameters
+	cp := ctx.ConsensusParams()
+	if cp == nil || cp.Block == nil {
+		return 0
+	}
+
+	maxGas := cp.Block.MaxGas
+
+	// Setting max_gas to -1 in Tendermint means there is no limit on the maximum gas consumption for transactions
+	// https://github.com/cometbft/cometbft/blob/v0.37.2/proto/tendermint/types/params.proto#L25-L27
+	if maxGas == -1 {
+		return math.MaxUint64
+	}
+
+	if maxGas > 0 {
+		return uint64(maxGas) // #nosec G701 -- maxGas is int64 type. It can never be greater than math.MaxUint64
+	}
+
+	return 0
+}
