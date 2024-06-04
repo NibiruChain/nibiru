9ff225a9859731e9547966dbc7c41f23e00d6b36
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-06-04 13:08:43 -0500

    chore: add v1.4.0 upgrade handler

---
+++ app/upgrades/v1_4_0/constants.go
@@ -0,0 +1,24 @@
+package v1_4_0
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/NibiruChain/nibiru/app/upgrades"
+)
+
+const UpgradeName = "v1.4.0"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName: UpgradeName,
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
+		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return mm.RunMigrations(ctx, cfg, fromVM)
+		}
+	},
+	StoreUpgrades: types.StoreUpgrades{
+		Added: []string{},
+	},
+}
