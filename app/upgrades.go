b6a2a4406494469509c0d1f198d1189ea7084604
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-06-04 13:08:43 -0500

    chore: add v1.4.0 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -13,6 +13,7 @@ import (
 	"github.com/NibiruChain/nibiru/app/upgrades/v1_1_0"
 	"github.com/NibiruChain/nibiru/app/upgrades/v1_2_0"
 	"github.com/NibiruChain/nibiru/app/upgrades/v1_3_0"
+	"github.com/NibiruChain/nibiru/app/upgrades/v1_4_0"
 )
 
 var Upgrades = []upgrades.Upgrade{
@@ -22,6 +23,7 @@ var Upgrades = []upgrades.Upgrade{
 	v1_1_0.Upgrade,
 	v1_2_0.Upgrade,
 	v1_3_0.Upgrade,
+	v1_4_0.Upgrade,
 }
 
 func (app *NibiruApp) setupUpgrades() {
