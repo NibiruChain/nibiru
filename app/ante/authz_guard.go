36745fde9d0228252e961f64d728ad8e51ff1ca6
Unique-Divine <realuniquedivine@gmail.com>
2024-06-07 16:07:20 -0500

    chore: add issue number for TODO comment

--- app/ante/authz_guard.go
+++ app/ante/authz_guard.go
@@ -1,6 +1,9 @@
 package ante
 
-// TODO: IsAuthzMessage fn. Use authz import with module name
-
-// TODO: Disabled msg set
-// TODO: Disabled msg set while nested
+// TODO: https://github.com/NibiruChain/nibiru/issues/1915
+// feat(ante): Add an authz guard to disable authz Ethereum txs and provide
+// additional security around the default functionality exposed by the module.
+//
+// Implemenetation Notes
+// UD-NOTE - IsAuthzMessage fn. Use authz import with module name
+// UD-NOTE - Define set of disabled txMsgs
