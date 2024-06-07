a862309f79b28965ec4b76a5f767222c8f1cc5c1
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2024-06-07 18:09:49 +0400

    test(evm): ante handler setup ctx coverage

---
+++ app/evmante_emit_event.go
@@ -0,0 +1,49 @@
+// Copyright (c) 2023-2024 Nibi, Inc.
+package app
+
+import (
+	"strconv"
+
+	errorsmod "cosmossdk.io/errors"
+	"github.com/NibiruChain/nibiru/x/evm"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// EthEmitEventDecorator emit events in ante handler in case of tx execution failed (out of block gas limit).
+type EthEmitEventDecorator struct {
+	AppKeepers
+}
+
+// NewEthEmitEventDecorator creates a new EthEmitEventDecorator
+func NewEthEmitEventDecorator(k AppKeepers) EthEmitEventDecorator {
+	return EthEmitEventDecorator{AppKeepers: k}
+}
+
+// AnteHandle emits some basic events for the eth messages
+func (eeed EthEmitEventDecorator) AnteHandle(
+	ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler,
+) (newCtx sdk.Context, err error) {
+	// After eth tx passed ante handler, the fee is deducted and nonce increased,
+	// it shouldn't be ignored by json-rpc. We need to emit some events at the
+	// very end of ante handler to be indexed by the consensus engine.
+	txIndex := eeed.EvmKeeper.EVMState().BlockTxIndex.GetOr(ctx, 0)
+
+	for i, msg := range tx.GetMsgs() {
+		msgEthTx, ok := msg.(*evm.MsgEthereumTx)
+		if !ok {
+			return ctx, errorsmod.Wrapf(errortypes.ErrUnknownRequest, "invalid message type %T, expected %T", msg, (*evm.MsgEthereumTx)(nil))
+		}
+
+		// emit ethereum tx hash as an event so that it can be indexed by
+		// Tendermint for query purposes it's emitted in ante handler, so we can
+		// query failed transaction (out of block gas limit).
+		ctx.EventManager().EmitEvent(sdk.NewEvent(
+			evm.EventTypeEthereumTx,
+			sdk.NewAttribute(evm.AttributeKeyEthereumTxHash, msgEthTx.Hash),
+			sdk.NewAttribute(evm.AttributeKeyTxIndex, strconv.FormatUint(txIndex+uint64(i), 10)), // #nosec G701
+		))
+	}
+
+	return next(ctx, tx, simulate)
+}
---
+++ app/evmante_setup_ctx_test.go
@@ -0,0 +1,42 @@
+package app_test
+
+import (
+	"math"
+
+	"github.com/NibiruChain/nibiru/app"
+	"github.com/NibiruChain/nibiru/x/evm/evmtest"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (s *TestSuite) TestEthSetupContextDecorator() {
+	deps := evmtest.NewTestDeps()
+	stateDB := deps.StateDB()
+	anteDec := app.NewEthSetUpContextDecorator(deps.Chain.AppKeepers)
+
+	s.Require().NoError(stateDB.Commit())
+	tx := happyCreateContractTx(&deps)
+
+	// Set block gas used to non 0 to check that handler resets it
+	anteDec.EvmKeeper.EvmState.BlockGasUsed.Set(deps.Ctx, 1000)
+
+	// Ante handler returns new context
+	newCtx, err := anteDec.AnteHandle(
+		deps.Ctx, tx, false, NextNoOpAnteHandler,
+	)
+	s.Require().NoError(err)
+
+	// Check that ctx gas meter is set up to infinite
+	ctxGasMeter := newCtx.GasMeter()
+	s.Require().Equal(sdk.Gas(math.MaxUint64), ctxGasMeter.GasRemaining())
+
+	// Check that gas configs are reset to default
+	defaultGasConfig := storetypes.GasConfig{}
+	s.Require().Equal(defaultGasConfig, newCtx.KVGasConfig())
+	s.Require().Equal(defaultGasConfig, newCtx.TransientKVGasConfig())
+
+	// Check that block gas used is reset to 0
+	gas, err := anteDec.EvmKeeper.EvmState.BlockGasUsed.Get(newCtx)
+	s.Require().NoError(err)
+	s.Require().Equal(gas, uint64(0))
+}
