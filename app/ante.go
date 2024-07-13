d807ad1cd824435211f2f4d0630523c05192154e
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-07-13 00:48:06 +0000

    refactor: remove ExtensionOptionDynamicFeeTx tx extension type

--- app/ante.go
+++ app/ante.go
@@ -10,9 +10,7 @@ import (
 	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
 
 	"github.com/NibiruChain/nibiru/app/ante"
-	"github.com/NibiruChain/nibiru/eth"
 	devgasante "github.com/NibiruChain/nibiru/x/devgas/v1/ante"
-	"github.com/NibiruChain/nibiru/x/evm"
 )
 
 // NewAnteHandler returns and AnteHandler that checks and increments sequence
@@ -20,52 +18,41 @@ import (
 // first signer.
 func NewAnteHandler(
 	keepers AppKeepers,
-	opts ante.AnteHandlerOptions,
+	options ante.AnteHandlerOptions,
 ) sdk.AnteHandler {
 	return func(
 		ctx sdk.Context, tx sdk.Tx, sim bool,
 	) (newCtx sdk.Context, err error) {
-		if err := opts.ValidateAndClean(); err != nil {
+		if err := options.ValidateAndClean(); err != nil {
 			return ctx, err
 		}
 
 		var anteHandler sdk.AnteHandler
-		hasExt, typeUrl := TxHasExtensions(tx)
-		if hasExt && typeUrl != "" {
-			anteHandler = AnteHandlerExtendedTx(typeUrl, keepers, opts)
-			return anteHandler(ctx, tx, sim)
+		txWithExtensions, ok := tx.(authante.HasExtensionOptionsTx)
+		if ok {
+			opts := txWithExtensions.GetExtensionOptions()
+			if len(opts) > 0 {
+				switch typeURL := opts[0].GetTypeUrl(); typeURL {
+				case "/ethermint.evm.v1.ExtensionOptionsEthereumTx":
+					// handle as *evmtypes.MsgEthereumTx
+					anteHandler = NewAnteHandlerEVM(keepers, options)
+				default:
+					return ctx, fmt.Errorf(
+						"rejecting tx with unsupported extension option: %s", typeURL)
+				}
+
+				return anteHandler(ctx, tx, sim)
+			}
 		}
 
 		switch tx.(type) {
 		case sdk.Tx:
-			anteHandler = NewAnteHandlerNonEVM(opts)
+			anteHandler = NewAnteHandlerNonEVM(options)
 		default:
 			return ctx, fmt.Errorf("invalid tx type (%T) in AnteHandler", tx)
 		}
 		return anteHandler(ctx, tx, sim)
 	}
-}
-
-func AnteHandlerExtendedTx(
-	typeUrl string,
-	keepers AppKeepers,
-	opts ante.AnteHandlerOptions,
-) (anteHandler sdk.AnteHandler) {
-	switch typeUrl {
-	case evm.TYPE_URL_ETHEREUM_TX:
-		anteHandler = NewAnteHandlerEVM(keepers, opts)
-	case eth.TYPE_URL_DYNAMIC_FEE_TX:
-		anteHandler = NewAnteHandlerNonEVM(opts)
-	default:
-		errUnsupported := fmt.Errorf(
-			`encountered tx with unsupported extension option, "%s"`, typeUrl)
-		return func(
-			ctx sdk.Context, tx sdk.Tx, simulate bool,
-		) (newCtx sdk.Context, err error) {
-			return ctx, errUnsupported
-		}
-	}
-	return anteHandler
 }
 
 // NewAnteHandlerNonEVM: Default ante handler for non-EVM transactions.
@@ -90,11 +77,9 @@ func NewAnteHandlerNonEVM(
 		authante.NewConsumeGasForTxSizeDecorator(opts.AccountKeeper),
 		// TODO: spike(security): Does minimum gas price of 0 pose a risk?
 		// ticket: https://github.com/NibiruChain/nibiru/issues/1916
-		authante.NewDeductFeeDecorator(
-			opts.AccountKeeper, opts.BankKeeper, opts.FeegrantKeeper, opts.TxFeeChecker),
+		authante.NewDeductFeeDecorator(opts.AccountKeeper, opts.BankKeeper, opts.FeegrantKeeper, opts.TxFeeChecker),
 		// ----------- Ante Handlers:  devgas
-		devgasante.NewDevGasPayoutDecorator(
-			opts.DevGasBankKeeper, opts.DevGasKeeper),
+		devgasante.NewDevGasPayoutDecorator(opts.DevGasBankKeeper, opts.DevGasKeeper),
 		// ----------- Ante Handlers:  Keys and signatures
 		// NOTE: SetPubKeyDecorator must be called before all signature verification decorators
 		authante.NewSetPubKeyDecorator(opts.AccountKeeper),
@@ -105,18 +90,4 @@ func NewAnteHandlerNonEVM(
 		ibcante.NewRedundantRelayDecorator(opts.IBCKeeper),
 		AnteDecoratorGasWanted{},
 	)
-}
-
-func TxHasExtensions(tx sdk.Tx) (hasExt bool, typeUrl string) {
-	extensionTx, ok := tx.(authante.HasExtensionOptionsTx)
-	if !ok {
-		return false, ""
-	}
-
-	extOpts := extensionTx.GetExtensionOptions()
-	if len(extOpts) == 0 {
-		return false, ""
-	}
-
-	return true, extOpts[0].GetTypeUrl()
 }
