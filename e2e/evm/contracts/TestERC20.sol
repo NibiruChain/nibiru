4c988ce6d991eee4bd0b5120263651c064b297c7
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-07-30 05:06:16 -0700

    refactor: update ERC20 contract name

--- e2e/evm/contracts/TestERC20.sol
+++ e2e/evm/contracts/TestERC20.sol
@@ -1,16 +1,16 @@
-// contracts/FunToken.sol
+// contracts/TestERC20.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FunToken is ERC20 {
+contract TestERC20 is ERC20 {
 
-    // Define the supply of FunToken: 1,000,000
+    // Define the supply of TestERC20: 1,000,000
     uint256 constant initialSupply = 1000000 * (10**18);
 
     // Constructor will be called on contract creation
-    constructor() ERC20("FunToken", "FUN") {
+    constructor() ERC20("TestERC20", "FOO") {
         _mint(msg.sender, initialSupply);
     }
 }
