6d8b822210fdc941ba4412e75f59b54df66cbbaa
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-07-30 05:09:46 -0700

    refactor: clean up tests

--- e2e/evm/test/basic_queries.test.ts
+++ e2e/evm/test/basic_queries.test.ts
@@ -1,36 +1,34 @@
-import { ethers } from "ethers"
-import { describe, it, expect } from "bun:test" // eslint-disable-line import/no-unresolved
-import { account, provider } from "./setup"
+import { describe, expect, it } from "bun:test"; // eslint-disable-line import/no-unresolved
+import { toBigInt, Wallet } from "ethers";
+import { account, provider } from "./setup";
 
 describe("Basic Queries", () => {
   it("Simple transfer, balance check", async () => {
-    const randomAddress = ethers.Wallet.createRandom().address
-    const amountToSend = BigInt(1000) // unibi
-    const gasLimit = BigInt(100_000) // unibi
+    const alice = Wallet.createRandom()
+    const amountToSend = toBigInt(1e3) // unibi
 
-    const senderBalanceBefore = await provider.getBalance(account.address)
-    const recipientBalanceBefore = await provider.getBalance(randomAddress)
-
+    const senderBalanceBefore = await provider.getBalance(account)
+    const recipientBalanceBefore = await provider.getBalance(alice)
     expect(senderBalanceBefore).toBeGreaterThan(0)
     expect(recipientBalanceBefore).toEqual(BigInt(0))
 
     // Execute EVM transfer
     const transaction = {
-      gasLimit: gasLimit,
-      to: randomAddress,
+      gasLimit: toBigInt(100e3),
+      to: alice,
       value: amountToSend,
     }
     const txResponse = await account.sendTransaction(transaction)
     await txResponse.wait()
     expect(txResponse).toHaveProperty("blockHash")
 
-    const senderBalanceAfter = await provider.getBalance(account.address)
-    const recipientBalanceAfter = await provider.getBalance(randomAddress)
+    const senderBalanceAfter = await provider.getBalance(account)
+    const recipientBalanceAfter = await provider.getBalance(alice)
 
     // TODO: https://github.com/NibiruChain/nibiru/issues/1902
     // gas is not deducted regardless the gas limit, check this
     const expectedSenderBalance = senderBalanceBefore - amountToSend
-    expect(senderBalanceAfter).toBeLessThanOrEqual(expectedSenderBalance)
+    expect(senderBalanceAfter).toEqual(expectedSenderBalance)
     expect(recipientBalanceAfter).toEqual(amountToSend)
-  }, 20_000)
+  }, 20e3)
 })
