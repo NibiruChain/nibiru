b90bd33e61d4efa36572e95d1cc2e7f922e70b5a
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-08-01 14:46:58 -0700

    refactor: remove embeds FixtureType

--- x/evm/embeds/embeds.go
+++ x/evm/embeds/embeds.go
@@ -10,10 +10,6 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
-	"os"
-	"path"
-	"path/filepath"
-	"runtime"
 	"strings"
 
 	gethabi "github.com/ethereum/go-ethereum/accounts/abi"
@@ -31,18 +27,16 @@ var (
 	// Contract_ERC20Minter: The default ERC20 contract deployed during the
 	// creation of a `FunToken` mapping from a bank coin.
 	SmartContract_ERC20Minter = CompiledEvmContract{
-		Name:        "ERC20Minter.sol",
-		FixtureType: FixtueType_Prod,
-		EmbedJSON:   erc20MinterContractJSON,
+		Name:      "ERC20Minter.sol",
+		EmbedJSON: erc20MinterContractJSON,
 	}
 
 	// SmartContract_Funtoken: Precompile contract interface for
 	// "IFunToken.sol". This precompile enables transfers of ERC20 tokens
 	// to non-EVM accounts. Only the ABI is used.
 	SmartContract_FunToken = CompiledEvmContract{
-		Name:        "FunToken.sol",
-		FixtureType: FixtueType_Prod,
-		EmbedJSON:   funtokenContractJSON,
+		Name:      "FunToken.sol",
+		EmbedJSON: funtokenContractJSON,
 	}
 )
 
@@ -52,23 +46,13 @@ func init() {
 }
 
 type CompiledEvmContract struct {
-	Name        string
-	FixtureType ContractFixtureType
-	EmbedJSON   []byte
+	Name      string
+	EmbedJSON []byte
 
 	// filled in post-load
 	ABI      *gethabi.ABI `json:"abi"`
 	Bytecode []byte       `json:"bytecode"`
 }
-
-// ContractFixtureType: Enum type for embedded smart contracts. This type
-// expresses whether a contract is used in production or only for testing.
-type ContractFixtureType string
-
-const (
-	FixtueType_Prod = "prod"
-	FixtueType_Test = "test"
-)
 
 // HexString: Hexadecimal-encoded string
 type HexString string
@@ -111,32 +95,11 @@ func (sc *CompiledEvmContract) MustLoad() {
 }
 
 func (sc CompiledEvmContract) Load() error {
-	var jsonBz []byte
-
-	// Locate the contracts directory.
-	switch sc.FixtureType {
-	case FixtueType_Prod:
-		if sc.EmbedJSON == nil {
-			return fmt.Errorf("missing compiled contract embed")
-		}
-		jsonBz = sc.EmbedJSON
-	case FixtueType_Test:
-		contractsDirPath, err := pathToE2EContracts()
-		if err != nil {
-			return err
-		}
-		baseName := strings.TrimSuffix(sc.Name, ".sol")
-		compiledPath := fmt.Sprintf("%s/%sCompiled.json", contractsDirPath, baseName)
-
-		jsonBz, err = os.ReadFile(compiledPath)
-		if err != nil {
-			return err
-		}
-	default:
-		panic(fmt.Errorf("unexpected case type \"%s\"", sc.FixtureType))
+	if sc.EmbedJSON == nil {
+		return fmt.Errorf("missing compiled contract embed")
 	}
 
-	abi, bytecode, err := parseCompiledJson(jsonBz)
+	abi, bytecode, err := parseCompiledJson(sc.EmbedJSON)
 	if err != nil {
 		return err
 	}
@@ -145,28 +108,4 @@ func (sc CompiledEvmContract) Load() error {
 	sc.Bytecode = bytecode
 
 	return nil
-}
-
-// pathToE2EContracts: Returns the absolute path to the E2E test contract
-// directory located at path, "NibiruChain/nibiru/e2e/evm/contracts".
-func pathToE2EContracts() (thePath string, err error) {
-	dirEvmTest, _ := GetPackageDir()
-	dirOfRepo := path.Dir(path.Dir(path.Dir(dirEvmTest)))
-	dirEvmE2e := path.Join(dirOfRepo, "e2e/evm")
-	if path.Base(dirEvmE2e) != "evm" {
-		return thePath, fmt.Errorf("failed to locate the e2e/evm directory")
-	}
-	return dirEvmE2e + "/contracts", nil
-}
-
-// GetPackageDir: Returns the absolute path of the Golang package that
-// calls this function.
-func GetPackageDir() (string, error) {
-	// Get the import path of the current package
-	_, filename, _, _ := runtime.Caller(0)
-	pkgDir := path.Dir(filename)
-	pkgPath := path.Join(path.Base(pkgDir), "..")
-
-	// Get the directory path of the package
-	return filepath.Abs(pkgPath)
 }
