b5c1c7d444d86844d9f1ede52b50ae01e97c9a19
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2024-08-15 12:31:18 +0400

    fix: release.mk go version upgrade to 1.21

--- contrib/make/release.mk
+++ contrib/make/release.mk
@@ -3,8 +3,12 @@
 ###############################################################################
 
 PACKAGE_NAME		  := github.com/NibiruChain/nibiru
-GOLANG_CROSS_VERSION  ?= v1.19.4
+GOLANG_CROSS_VERSION  ?= v1.21.5
 
+# The `make release` command is running a Docker container with the image
+# `gorelease/goreleaser-cross:${GOLANG_CROSS_VERSION}`. This command:
+# `-v "$(CURDIR)":/go/src/$(PACKAGE_NAME)`: mounts the current directory
+# `release --clean`: executes the release inside the directory
 release:
 	docker run \
 		--rm \
@@ -14,15 +18,15 @@ release:
 		-e CGO_ENABLED=1 \
 		-e GITHUB_TOKEN=${GITHUB_TOKEN} \
 		goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
-		release --rm-dist
+		release --clean
 
 release-snapshot:
 	docker run \
 		--rm \
 		--platform linux/amd64 \
+		-v /tmp:/tmp \
 		-v "$(CURDIR)":/go/src/$(PACKAGE_NAME) \
 		-w /go/src/$(PACKAGE_NAME) \
 		-e CGO_ENABLED=1 \
-		-e GITHUB_TOKEN=${GITHUB_TOKEN} \
 		goreleaser/goreleaser-cross:${GOLANG_CROSS_VERSION} \
-		release --rm-dist --snapshot
+		release --clean --snapshot
