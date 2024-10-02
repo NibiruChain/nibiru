6752ed589c72eff0d4d48bd7309d0e44d524e640
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-10-02 18:51:08 -0500

    Update x/evm/embeds/contracts/IOracle.sol
    Co-authored-by: Unique Divine <51418232+Unique-Divine@users.noreply.github.com>

--- x/evm/embeds/contracts/IOracle.sol
+++ x/evm/embeds/contracts/IOracle.sol
@@ -8,7 +8,7 @@ interface IOracle {
   /// USD price of BTC and "unibi:uusd" is the USD price of NIBI.
   /// @return The exchange rate (a decimal value) as a string.
   /// @dev This function is view-only and does not modify state.
-  function queryExchangeRate(string memory pair) external returns (string memory);
+  function queryExchangeRate(string memory pair) external view returns (string memory);
 }
 
 address constant ORACLE_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000801;
