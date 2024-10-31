ecf3e476cf393384df582141fcedd0411ba6f470
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2024-10-31 14:15:01 -0500

    fix(evm): Use a NibiruBankKeeper to automatically record ether balance changes on the StateDB journal (#2095)
    * statedb: add cacheing for multistore before precompile runs
    
    * messy, working first version that allows for precompile reversion
    
    * wip!: Save checkpoint.
    1. Created NibiruBankKeeper with safety around NIBI transfers inside of
       EthereumTx.
    2. The "PrecompileCalled" JournalChange now has a propery implementation
       and a strong test case to show that reverting the precompile calls
       works as intended.
    3. Remove unneeded functions created for testing with low-level struct
       fields.
    
    * chore: changelog
    
    * finalize bank keeper changes
    
    * chore changelog
    
    * reset cache
    
    * finalize bank keeper changes
    
    * revert to previous commit 7f904a07ac4d5555d8c088411024fc50ff65d085
    
    * fix strange ignored file issue
    
    * fix strange ignored file issue
    
    * remove new bank keeper
    
    * chore: comments from self-review
    
    * chore: changelo g
    
    * fix(deps): update bank module to remove concrete type assertion on AppModule.RegisterServices
    
    This makes it possible to plug in an alternative bankkeeper.Keeper
    implementation instead of bankkeeper.BaseKeeper.
    
    * chore: merge conflicts
    
    * revert experiment
    
    * finishing touch
    
    * fix incorrect arugmetn
    
    * feat(evmtest-erc20: Add optional descriptions to the assertions

--- app/evmante/evmante_sigverify_test.go
+++ app/evmante/evmante_sigverify_test.go
@@ -66,8 +66,8 @@ func (s *TestSuite) TestEthSigVerificationDecorator() {
 	for _, tc := range testCases {
 		s.Run(tc.name, func() {
 			deps := evmtest.NewTestDeps()
-			stateDB := deps.StateDB()
-			anteDec := evmante.NewEthSigVerificationDecorator(&deps.App.AppKeepers.EvmKeeper)
+			stateDB := deps.NewStateDB()
+			anteDec := evmante.NewEthSigVerificationDecorator(deps.App.AppKeepers.EvmKeeper)
 
 			tx := tc.txSetup(&deps)
 			s.Require().NoError(stateDB.Commit())
