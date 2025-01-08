d67b40fd8118d12cc64862d12b1b5f97bb43b940
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2025-01-08 21:26:08 +0400

    test: checked that random changes with block time

--- x/evm/keeper/random_test.go
+++ x/evm/keeper/random_test.go
@@ -2,6 +2,8 @@
 package keeper_test
 
 import (
+	"time"
+
 	"github.com/NibiruChain/nibiru/v2/x/evm/embeds"
 	"github.com/NibiruChain/nibiru/v2/x/evm/evmtest"
 )
@@ -14,9 +16,20 @@ func (s *Suite) TestRandom() {
 	randomContractAddr := deployResp.ContractAddr
 
 	// highjacked LoadERC20BigInt method as it perfectly fits the need of this test
-	resp, err := deps.EvmKeeper.LoadERC20BigInt(
+	random1, err := deps.EvmKeeper.LoadERC20BigInt(
 		deps.Ctx, embeds.SmartContract_TestRandom.ABI, randomContractAddr, "getRandom",
 	)
 	s.Require().NoError(err)
-	s.Require().Greater(resp.Int64(), int64(0))
+	s.Require().NotNil(random1)
+	s.Require().NotZero(random1.Int64())
+
+	// Update block time to check that random changes
+	deps.Ctx = deps.Ctx.WithBlockTime(deps.Ctx.BlockTime().Add(1 * time.Second))
+	random2, err := deps.EvmKeeper.LoadERC20BigInt(
+		deps.Ctx, embeds.SmartContract_TestRandom.ABI, randomContractAddr, "getRandom",
+	)
+	s.Require().NoError(err)
+	s.Require().NotNil(random1)
+	s.Require().NotZero(random2.Int64())
+	s.Require().NotEqual(random1, random2)
 }
