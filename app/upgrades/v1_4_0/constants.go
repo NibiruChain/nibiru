91338dd35a79fe0c92030adefadc53ea82c26f81
expertdicer <expertdicer@gmail.com>
2025-02-20 10:38:46 +0700

    fix: using right context in upgrades

--- app/upgrades/v1_4_0/constants.go
+++ app/upgrades/v1_4_0/constants.go
@@ -1,10 +1,12 @@
 package v1_4_0
 
 import (
+	"context"
+
 	"cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/NibiruChain/nibiru/app/upgrades"
 )
@@ -14,7 +16,8 @@ const UpgradeName = "v1.4.0"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: UpgradeName,
 	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
-		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return func(c context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx := sdk.UnwrapSDKContext(c)
 			return mm.RunMigrations(ctx, cfg, fromVM)
 		}
 	},
