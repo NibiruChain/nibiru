cdbe3431320c355137c77e262ccbb4d09a961ec9
Skylar Ray <137945430+sky-coderay@users.noreply.github.com>
2025-02-28 18:32:54 +0200

    Update reject_ethereum_tx_msgs_test.go

--- app/ante/reject_ethereum_tx_msgs_test.go
+++ app/ante/reject_ethereum_tx_msgs_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/NibiruChain/nibiru/v2/x/evm/evmtest"
 )
 
-func (s *AnteTestSuite) TestAnteDecoratorPreventEtheruemTxMsgs() {
+func (s *AnteTestSuite) TestAnteDecoratorPreventEthereumTxMsgs() {
 	testCases := []struct {
 		name    string
 		txSetup func(deps *evmtest.TestDeps) sdk.Tx
@@ -30,7 +30,7 @@ func (s *AnteTestSuite) TestAnteDecoratorPreventEtheruemTxMsgs() {
 	for _, tc := range testCases {
 		s.Run(tc.name, func() {
 			deps := evmtest.NewTestDeps()
-			anteDec := ante.AnteDecoratorPreventEtheruemTxMsgs{}
+			anteDec := ante.AnteDecoratorPreventEthereumTxMsgs{}
 			tx := tc.txSetup(&deps)
 
 			_, err := anteDec.AnteHandle(
