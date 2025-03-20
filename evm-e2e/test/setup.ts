12f223ea386ea64b88838d2623bab399f1e82c81
Unique-Divine <realuniquedivine@gmail.com>
2025-03-20 03:17:14 -0500

    test(e2e): add WNIBI tests, and use single formatter

--- evm-e2e/test/setup.ts
+++ evm-e2e/test/setup.ts
@@ -1,11 +1,11 @@
-import { config } from 'dotenv';
-import { ethers, getDefaultProvider, Wallet } from 'ethers';
+import { config } from "dotenv"
+import { ethers, getDefaultProvider, Wallet } from "ethers"
 
-config();
+config()
 
-const provider = new ethers.JsonRpcProvider(process.env.JSON_RPC_ENDPOINT);
-const account = Wallet.fromPhrase(process.env.MNEMONIC, provider);
-const TEST_TIMEOUT = Number(process.env.TEST_TIMEOUT) || 15000;
-const TX_WAIT_TIMEOUT = Number(process.env.TX_WAIT_TIMEOUT) || 5000;
+const provider = new ethers.JsonRpcProvider(process.env.JSON_RPC_ENDPOINT)
+const account = Wallet.fromPhrase(process.env.MNEMONIC, provider)
+const TEST_TIMEOUT = Number(process.env.TEST_TIMEOUT) || 15000
+const TX_WAIT_TIMEOUT = Number(process.env.TX_WAIT_TIMEOUT) || 5000
 
-export { account, provider, TEST_TIMEOUT, TX_WAIT_TIMEOUT };
+export { account, provider, TEST_TIMEOUT, TX_WAIT_TIMEOUT }
--- evm-e2e/test/wrapped_nibiru_wnibi.test.ts
+++ evm-e2e/test/wrapped_nibiru_wnibi.test.ts
@@ -1,59 +1,60 @@
-import { expect, test } from '@jest/globals';
-import { deployContractWNIBI } from './utils';
-import { TEST_TIMEOUT, account, provider } from './setup';
-import { parseUnits, toBigInt } from 'ethers';
+import { expect, test } from "@jest/globals"
+import { parseUnits, toBigInt } from "ethers"
+
+import { account, provider, TEST_TIMEOUT } from "./setup"
+import { deployContractWNIBI } from "./utils"
 
 test(
-  'WNIBI.sol deposits via ether transfer',
+  "WNIBI.sol deposits via ether transfer",
   async () => {
-    const { contract } = await deployContractWNIBI();
-    const decimals = await contract.decimals();
-    expect(decimals).toEqual(BigInt(18));
-    const contractAddr = await contract.getAddress();
+    const { contract } = await deployContractWNIBI()
+    const decimals = await contract.decimals()
+    expect(decimals).toEqual(BigInt(18))
+    const contractAddr = await contract.getAddress()
 
     // Check balances before any actions
-    const walletBalWei = await provider.getBalance(account.address);
-    expect(walletBalWei).toBeGreaterThan(parseUnits('999', 18));
+    const walletBalWei = await provider.getBalance(account.address)
+    expect(walletBalWei).toBeGreaterThan(parseUnits("999", 18))
     {
-      const balanceOf = await contract.balanceOf(account.address);
-      expect(balanceOf).toEqual(BigInt(0));
+      const balanceOf = await contract.balanceOf(account.address)
+      expect(balanceOf).toEqual(BigInt(0))
     }
 
     // Deposit via transfer of wei
     {
-      const amountWei = parseUnits('420', 12);
+      const amountWei = parseUnits("420", 12)
       const txResp = await account.sendTransaction({
         to: contractAddr,
         value: amountWei,
-      });
-      await txResp.wait();
+      })
+      await txResp.wait()
 
       // Check balances after deposit
-      const contractBalWei = await provider.getBalance(contractAddr);
-      const contractBalWNIBI = await contract.balanceOf(contractAddr);
-      const walletBalWNIBI = await contract.balanceOf(account.address);
-      expect(contractBalWei).toEqual(amountWei);
-      expect(contractBalWNIBI).toEqual(0n);
-      expect(walletBalWNIBI).toEqual(amountWei);
+      const contractBalWei = await provider.getBalance(contractAddr)
+      const contractBalWNIBI = await contract.balanceOf(contractAddr)
+      const walletBalWNIBI = await contract.balanceOf(account.address)
+      expect(contractBalWei).toEqual(amountWei)
+      expect(contractBalWNIBI).toEqual(0n)
+      expect(walletBalWNIBI).toEqual(amountWei)
     }
 
     // Withdraw
     {
-      const amountWei = parseUnits('351', 12);
-      const txResp = await contract.withdraw(amountWei);
-      await txResp.wait();
+      const amountWei = parseUnits("351", 12)
+      const txResp = await contract.withdraw(amountWei)
+      await txResp.wait()
 
       // Check balanaces after withdraw
-      const contractBalWei = await provider.getBalance(contractAddr);
-      const contractBalWNIBI = await contract.balanceOf(contractAddr);
-      const walletBalWNIBI = await contract.balanceOf(account.address);
-      expect(contractBalWei).toEqual(parseUnits('69', 12));
-      expect(contractBalWNIBI).toEqual(0n);
-      expect(walletBalWNIBI).toEqual(parseUnits('69', 12));
+      const contractBalWei = await provider.getBalance(contractAddr)
+      const contractBalWNIBI = await contract.balanceOf(contractAddr)
+      const walletBalWNIBI = await contract.balanceOf(account.address)
+      expect(contractBalWei).toEqual(parseUnits("69", 12))
+      expect(contractBalWNIBI).toEqual(0n)
+      expect(walletBalWNIBI).toEqual(parseUnits("69", 12))
     }
 
     // TODO: test: Deposit via method call
     // TODO: test: totalSupply and transfer tests
   },
   TEST_TIMEOUT,
-);
+)
