60eaa00e07818702ca88aa23807d36e9575aa3ea
expertdicer <expertdicer@gmail.com>
2025-05-27 16:36:01 +0700

    update v2.4.0 upgrade

--- app/upgrades/v2_4_0/constants.go
+++ app/upgrades/v2_4_0/constants.go
@@ -1,12 +1,7 @@
 package v2_4_0
 
 import (
-	"context"
-
 	"cosmossdk.io/store/types"
-	upgradetypes "cosmossdk.io/x/upgrade/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
-	clientkeeper "github.com/cosmos/ibc-go/v8/modules/core/02-client/keeper"
 
 	"github.com/NibiruChain/nibiru/v2/app/upgrades"
 )
