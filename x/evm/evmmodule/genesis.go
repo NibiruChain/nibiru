ca28877f6331ba340b7dc3775d12e532201bf92d
Unique Divine <realuniquedivine@gmail.com>
2025-09-07 17:38:29 -0500

    impl first draft

--- x/evm/evmmodule/genesis.go
+++ x/evm/evmmodule/genesis.go
@@ -2,14 +2,12 @@
 package evmmodule
 
 import (
-	"bytes"
 	"fmt"
 
 	"github.com/NibiruChain/collections"
 	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	gethcommon "github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/NibiruChain/nibiru/v2/eth"
 	"github.com/NibiruChain/nibiru/v2/x/evm"
@@ -37,35 +35,9 @@ func InitGenesis(
 
 	// Create evm contracts from genstate.Accounts
 	for _, account := range genState.Accounts {
-		address := gethcommon.HexToAddress(account.Address)
-		accAddress := sdk.AccAddress(address.Bytes())
-		// check that the EVM balance the matches the account balance
-		acc := accountKeeper.GetAccount(ctx, accAddress)
-		if acc == nil {
-			panic(fmt.Errorf("account not found for address %s", account.Address))
-		}
-
-		ethAcct, ok := acc.(eth.EthAccountI)
-		if !ok {
-			panic(
-				fmt.Errorf("account %s must be an EthAccount interface, got %T",
-					account.Address, acc,
-				),
-			)
-		}
-		code := gethcommon.Hex2Bytes(account.Code)
-		codeHash := crypto.Keccak256Hash(code)
-
-		// we ignore the empty Code hash checking, see ethermint PR#1234
-		if len(account.Code) != 0 && !bytes.Equal(ethAcct.GetCodeHash().Bytes(), codeHash.Bytes()) {
-			s := "the evm state code doesn't match with the codehash\n"
-			panic(fmt.Sprintf("%s account: %s , evm state codehash: %v, ethAccount codehash: %v, evm state code: %s\n",
-				s, account.Address, codeHash, ethAcct.GetCodeHash(), account.Code))
-		}
-		k.SetCode(ctx, codeHash.Bytes(), code)
-
-		for _, storage := range account.Storage {
-			k.SetState(ctx, address, gethcommon.HexToHash(storage.Key), gethcommon.HexToHash(storage.Value).Bytes())
+		err := k.ImportGenesisAccount(ctx, account)
+		if err != nil {
+			panic(err)
 		}
 	}
 
