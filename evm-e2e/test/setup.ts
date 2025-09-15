785a8f041be4b4c90f3a55617541c4f2dbae9a61
expertdicer <expertdicer@gmail.com>
2025-09-15 12:52:47 +0700

    fix(evm): update account creation to use HDNodeWallet for improved mnemonic handling

--- evm-e2e/test/setup.ts
+++ evm-e2e/test/setup.ts
@@ -1,16 +1,17 @@
 import { config } from "dotenv"
-import { ethers, getDefaultProvider, Wallet, Mnemonic } from "ethers"
+import { ethers, Mnemonic } from "ethers"
 import { HDNodeWallet } from "ethers/wallet"
 
 config()
 
 const provider = new ethers.JsonRpcProvider(process.env.JSON_RPC_ENDPOINT)
-const account = Wallet.fromPhrase(process.env.MNEMONIC, provider)
+const mnemonic = Mnemonic.fromPhrase(process.env.MNEMONIC!)
+const account = HDNodeWallet.fromMnemonic(mnemonic, "m/44'/60'/0'/0/0").connect(provider)
+const account2 = HDNodeWallet.fromMnemonic(mnemonic, "m/44'/60'/0'/0/1").connect(provider)
 const TEST_TIMEOUT = Number(process.env.TEST_TIMEOUT) || 15000
 const TX_WAIT_TIMEOUT = Number(process.env.TX_WAIT_TIMEOUT) || 5000
 
-const mnemonic = Mnemonic.fromPhrase(process.env.MNEMONIC!)
-const account2 = HDNodeWallet.fromMnemonic(mnemonic, "m/44'/60'/0'/0/1").connect(provider)
+
 
 export { account, account2, provider, TEST_TIMEOUT, TX_WAIT_TIMEOUT }
 
