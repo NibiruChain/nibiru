f26c402548439bd11b309f8da8fd2196ea791454
expertdicer <expertdicer@gmail.com>
2026-01-20 20:58:45 +0700

    remove unused Cosmos address handling and trade size calculation logic

--- sai-trading/services/evmtrader/hd_derive.go
+++ sai-trading/services/evmtrader/hd_derive.go
@@ -5,26 +5,19 @@ import (
 
 	"github.com/NibiruChain/nibiru/v2/eth"
 	nibiruhd "github.com/NibiruChain/nibiru/v2/eth/crypto/hd"
-	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 type DerivedAccounts struct {
-	CosmosAddrBech32 string
-	CosmosAddrHex    common.Address
-
 	EthAddrHex    common.Address
 	EthAddrBech32 string
 
-	EthPrivateKeyHex    string
-	CosmosPrivateKeyHex string
+	EthPrivateKeyHex string
 }
 
-// DeriveAccountsFromMnemonic derives both Ethereum and Cosmos accounts from a mnemonic
+// DeriveAccountsFromMnemonic derives Ethereum account from a mnemonic (using BIP44 path m/44'/60'/0'/0/0)
 func DeriveAccountsFromMnemonic(mnemonic string, bech32Prefix string) (*DerivedAccounts, error) {
 	if bech32Prefix == "" {
 		bech32Prefix = "nibi" // Default to Nibiru prefix
@@ -45,25 +38,9 @@ func DeriveAccountsFromMnemonic(mnemonic string, bech32Prefix string) (*DerivedA
 
 	ethAddrBech32 := eth.EthAddrToNibiruAddr(ethAddrHex).String()
 
-	cosmosHDPath := sdk.GetConfig().GetFullBIP44Path()
-	cosmosPrivKeyBytes, err := hd.Secp256k1.Derive()(mnemonic, keyring.DefaultBIP39Passphrase, cosmosHDPath)
-	if err != nil {
-		return nil, fmt.Errorf("derive Cosmos private key: %w", err)
-	}
-
-	cosmosPrivKey := &secp256k1.PrivKey{Key: cosmosPrivKeyBytes}
-	cosmosPrivKeyHex := fmt.Sprintf("%x", cosmosPrivKeyBytes)
-
-	cosmosPubKey := cosmosPrivKey.PubKey()
-	cosmosAddrBech32 := sdk.AccAddress(cosmosPubKey.Address()).String()
-	cosmosAddrHex := common.BytesToAddress(cosmosPubKey.Address().Bytes())
-
 	return &DerivedAccounts{
-		CosmosAddrBech32:    cosmosAddrBech32,
-		CosmosAddrHex:       cosmosAddrHex,
-		EthAddrHex:          ethAddrHex,
-		EthAddrBech32:       ethAddrBech32,
-		EthPrivateKeyHex:    ethPrivKeyHex,
-		CosmosPrivateKeyHex: cosmosPrivKeyHex,
+		EthAddrHex:       ethAddrHex,
+		EthAddrBech32:    ethAddrBech32,
+		EthPrivateKeyHex: ethPrivKeyHex,
 	}, nil
 }
