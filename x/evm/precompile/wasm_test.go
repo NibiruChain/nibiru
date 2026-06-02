46161f0bc76b1db04ce203882766c19319d3a9dc
Unique Divine <realuniquedivine@gmail.com>
2026-06-02 15:40:38 -0500

    test(evm): Fix test assertion for FunToken token creation

--- x/evm/precompile/wasm_test.go
+++ x/evm/precompile/wasm_test.go
@@ -83,6 +83,10 @@ func (s *WasmSuite) TestExecute() {
 
 	wasmContracts := test.SetupWasmContracts(&deps, &s.Suite)
 	wasmContract := wasmContracts[0] // nibi_stargate.wasm
+	sudoers, err := deps.App.SudoKeeper.Sudoers.Get(deps.Ctx())
+	s.Require().NoError(err)
+	sudoers.Contracts = append(sudoers.Contracts, wasmContract.String())
+	deps.App.SudoKeeper.Sudoers.Set(deps.Ctx(), sudoers)
 
 	s.Run("create denom", func() {
 		msgArgsBz := []byte(`
